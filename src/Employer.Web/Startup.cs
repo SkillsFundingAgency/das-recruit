db2c413f52d227ef9852543f92cabcd521e68db8
Lee Wadhams <lee.wadhams@valtech.co.uk>
2018-01-26 14:44:54 +0000

    Allow authentication to be switched off

--- src/Employer.Web/Startup.cs
+++ src/Employer.Web/Startup.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,20 +13,28 @@ namespace Esfa.Recruit.Employer.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration config)
+        public Startup(IConfiguration config, IHostingEnvironment env)
         {
             Configuration = config;
+            HostingEnvironment = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment HostingEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-
             var authConfig = Configuration.GetSection("Authentication").Get<AuthenticationConfiguration>();
-
+            
+            services.AddMvc(opts =>
+            {
+                if (!authConfig.IsEnabled)
+                {
+                    opts.Filters.Add(new AllowAnonymousFilter());
+                }
+            });
+              
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
             services.AddAuthentication(options =>
@@ -75,6 +84,7 @@ namespace Esfa.Recruit.Employer.Web
 
         private class AuthenticationConfiguration
         {
+            public bool IsEnabled { get; set; } = true;
             public string Authority { get; set; }
             public string MetaDataAddress { get; set; }
             public string ClientId { get; set; }
