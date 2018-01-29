2ac06b003a94f91ff80752b1e75d4d3e3f084fc5
Lee Wadhams <lee.wadhams@valtech.co.uk>
2018-01-29 17:33:24 +0000

    Add NLog Nuget packages

--- src/Employer.Web/Controllers/HomeController.cs
+++ src/Employer.Web/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Employer.Domain.Configuration;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Esfa.Recruit.Employer.Web.Controllers
@@ -14,15 +15,18 @@ namespace Esfa.Recruit.Employer.Web.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        private ExternalLinksConfiguration _externalLinks;
+        private readonly ExternalLinksConfiguration _externalLinks;
+        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(IOptions<ExternalLinksConfiguration> externalLinksOptions)
+        public HomeController(IOptions<ExternalLinksConfiguration> externalLinksOptions, ILogger<HomeController> logger)
         {
             _externalLinks = externalLinksOptions.Value;
+            _logger = logger;
         }
         
         public IActionResult Index()
         {
+            _logger.LogInformation("Showing Index page.");
             return View();
         }
 
--- src/Employer.Web/Program.cs
+++ src/Employer.Web/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace Esfa.Recruit.Employer.Web
 {
@@ -14,7 +15,19 @@ namespace Esfa.Recruit.Employer.Web
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            // NLog: setup the logger first to catch all errors
+            var logger = NLogBuilder.ConfigureNLog("NLog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("About to Configure Host and Start.");
+                BuildWebHost(args).Run();
+            }
+            catch (Exception e)
+            {
+                //NLog: catch setup errors
+                logger.Error(e, "Stopped program because of exception");
+                throw;
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
@@ -29,6 +42,7 @@ namespace Esfa.Recruit.Employer.Web
                             config.AddUserSecrets<Startup>();
                         }
                     })
+                .UseNLog()
                 .Build();
     }
 }
