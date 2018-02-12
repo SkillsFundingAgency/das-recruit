30471811e1a63e8f98b6d255ee95fc40d0cb801d
Lee Wadhams <lee.wadhams@valtech.co.uk>
2018-02-12 12:36:33 +0000

    Create ErrorController

---
+++ src/Employer.Web/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Esfa.Recruit.Employer.Web.Configuration;
+using Esfa.Recruit.Employer.Web.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Esfa.Recruit.Employer.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ErrorController()
+        {
+        }
+
+        public IActionResult Error(int id)
+        {
+            return View(new ErrorViewModel { StatusCode = id, RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
