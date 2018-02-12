a45cc16ffdaf68c6476eed819139347c03f0eba7
Lee Wadhams <lee.wadhams@valtech.co.uk>
2018-02-12 12:36:33 +0000

    Create ErrorController

--- src/Employer.Web/Controllers/HomeController.cs
+++ src/Employer.Web/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Threading.Tasks;
 using Esfa.Recruit.Employer.Web.Configuration;
-using Esfa.Recruit.Employer.Web.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -32,11 +31,6 @@ namespace Esfa.Recruit.Employer.Web.Controllers
             await HttpContext.SignOutAsync("oidc");
 
             return Redirect(_externalLinks.ManageApprenticeshipSiteUrl);
-        }
-
-        public IActionResult Error(int id)
-        {
-            return View(new ErrorViewModel { StatusCode = id, RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
