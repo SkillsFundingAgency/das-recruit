682ed6b0aba1534202b60920e9dc8160c230db6f
Chris Jensen <chrisjensenuk@gmail.com>
2018-02-13 15:26:15 +0000

    ER-240 - Added messaging, command and Mediatr to Core

---
+++ src/Recruit.Storage.Client/Core/Messaging/MediatrMessaging.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esfa.Recruit.Storage.Client.Core.Messaging
+{
+    public class MediatrMessaging : IMessaging
+    {
+        private readonly IMediator _mediator;
+
+        public MediatrMessaging(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task SendCommand(ICommand command)
+        {
+            var request = command as IRequest;
+
+            await _mediator.Send(request);
+        }
+    }
+}
