3e593934b60e9663281a4be0b7beae8ff24ebe37
sachin.patel <sachin.patel@valtech.co.uk>
2018-02-14 13:12:07 +0000

    ER-217 :hammer: - Moving some of the configuration service body into separate methods in partial class files.

--- src/Employer.Web/Startup.ConfigureAuthentication.cs
+++ src/Employer.Web/Startup.ConfigureAuthentication.cs
@@ -7,10 +7,64 @@ namespace Employer.Web
 {
     public partial class Startup
     {
-        public void ConfigureAuthenticationServices(IServiceCollection services)
+        public void ConfigureAuthentication(IServiceCollection services)
         {
-			
-            //_hostingEnvironemnt
+            if (_isAuthEnabled)
+            {
+                JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+                services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = "Cookies";
+                    options.DefaultChallengeScheme = "oidc";
+                })
+                .AddCookie("Cookies", options =>
+                {
+                    options.AccessDeniedPath = "/Error/403";
+                })
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.SignInScheme = "Cookies";
+
+                    options.Authority = _authConfig.Authority;
+                    options.MetadataAddress = _authConfig.MetaDataAddress;
+                    options.RequireHttpsMetadata = false;
+                    options.ResponseType = "code";
+                    options.ClientId = _authConfig.ClientId;
+                    options.ClientSecret = _authConfig.ClientSecret;
+                    options.Scope.Add("profile");
+
+                    options.Events.OnTokenValidated = PopulateAccountsClaim;
+                });
+            }
+        }
+
+        private Task PopulateAccountsClaim(TokenValidatedContext ctx)
+        {
+            var userId = ctx.Principal.Claims.First(c => c.Type.Equals(EmployerRecruitClaims.IdamsUserIdClaimTypeIdentifier)).Value;
+            var accounts = _accountsSvc.GetAssociatedAccounts(userId);
+
+            var accountsConcatenated = string.Join(",", accounts);
+            var associatedAccountClaim = new Claim(EmployerRecruitClaims.AccountsClaimsTypeIdentifier, accountsConcatenated, ClaimValueTypes.String);
+
+            ctx.Principal.Identities.First().AddClaim(associatedAccountClaim);
+
+            return Task.CompletedTask;
+        }
+
+        private void ConfigureAuthorization(IServiceCollection services)
+        {
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("HasEmployerAccount", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim(EmployerRecruitClaims.AccountsClaimsTypeIdentifier);
+                    policy.Requirements.Add(new EmployerAccountRequirement());
+                });
+            });
+
+            services.AddSingleton<IAuthorizationHandler, EmployerAccountHandler>();
         }
     }
 }
