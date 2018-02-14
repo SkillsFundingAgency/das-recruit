a75b2b4dbb73bdd1efe23875ecc60b9be0f8fad7
sachin.patel <sachin.patel@valtech.co.uk>
2018-02-14 13:12:07 +0000

    ER-217 :hammer: - Moving some of the configuration service body into separate methods in partial class files.

---
+++ src/Employer.Web/Startup.ConfigureMvc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Esfa.Recruit.Employer.Web
+{
+    public partial class Startup
+    {
+        private void ConfigureMvc(IServiceCollection services)
+        {
+            services.AddMvc(opts =>
+            {
+                //opts.Conventions.Insert(0, new EmployerAccountRoutePrefixConvention());
+
+                if (!_hostingEnvironment.IsDevelopment())
+                {
+                    opts.Filters.Add(new RequireHttpsAttribute());
+                }
+
+                if (!_isAuthEnabled)
+                {
+                    opts.Filters.Add(new AllowAnonymousFilter());
+                }
+
+                var policy = new AuthorizationPolicyBuilder()
+                         .RequireAuthenticatedUser()
+                         .Build();
+
+                opts.Filters.Add(new AuthorizeFilter(policy));
+
+                opts.Filters.Add(new AuthorizeFilter("HasEmployerAccount"));
+
+                var jsonInputFormatters = opts.InputFormatters.OfType<JsonInputFormatter>();
+                foreach (var formatter in jsonInputFormatters)
+                {
+                    formatter.SupportedMediaTypes
+                        .Add(MediaTypeHeaderValue.Parse("application/csp-report"));
+                }
+
+                opts.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+            });
+        }
+    }
+}
--- src/Employer.Web/Startup.ConfigureServices.cs
+++ src/Employer.Web/Startup.ConfigureServices.cs
@@ -1,20 +1,8 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Employer.Web.Configuration;
-using Employer.Web.Middleware;
-using Employer.Web.Services;
+﻿using Employer.Web.Services;
 using Esfa.Recruit.Employer.Web.Configuration;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Authorization;
-using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Net.Http.Headers;
 
 namespace Esfa.Recruit.Employer.Web
 {
@@ -24,9 +12,12 @@ namespace Esfa.Recruit.Employer.Web
         private IConfiguration _configuration { get; }
         private IHostingEnvironment _hostingEnvironment { get; }
         private AuthenticationConfiguration _authConfig { get; }
-
         private readonly IGetAssociatedEmployerAccountsService _accountsSvc;
 
+        partial void ConfigureMvc(IServiceCollection services);
+        partial void ConfigureAuthentication(IServiceCollection services);
+        partial void ConfigureAuthorization(IServiceCollection services);
+        
         public Startup(IConfiguration config, IHostingEnvironment env)
         {
             _configuration = config;
@@ -53,103 +44,12 @@ namespace Esfa.Recruit.Employer.Web
                 opt.AppendTrailingSlash = true;
             });
 
-            services.AddMvc(opts =>
-            {
-                //opts.Conventions.Insert(0, new EmployerAccountRoutePrefixConvention());
-
-                if (!_hostingEnvironment.IsDevelopment())
-                {
-                    opts.Filters.Add(new RequireHttpsAttribute());
-                }
-
-                if (!_isAuthEnabled)
-                {
-                    opts.Filters.Add(new AllowAnonymousFilter());
-                }
-
-                var policy = new AuthorizationPolicyBuilder()
-                         .RequireAuthenticatedUser()
-                         .Build();
-
-                opts.Filters.Add(new AuthorizeFilter(policy));
-
-                opts.Filters.Add(new AuthorizeFilter("HasEmployerAccount"));
-
-                var jsonInputFormatters = opts.InputFormatters.OfType<JsonInputFormatter>();
-                    foreach (var formatter in jsonInputFormatters)
-                {
-                    formatter.SupportedMediaTypes
-                        .Add(MediaTypeHeaderValue.Parse("application/csp-report"));
-                }
-
-                opts.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-            });
+            ConfigureMvc(services);
 
             services.AddApplicationInsightsTelemetry(_configuration);
             
             ConfigureAuthentication(services);
             ConfigureAuthorization(services);
-
-        }
-
-        private void ConfigureAuthentication(IServiceCollection services)
-        {
-            if (_isAuthEnabled)
-            {
-                JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-              
-                services.AddAuthentication(options =>
-                {
-                    options.DefaultScheme = "Cookies";
-                    options.DefaultChallengeScheme = "oidc";
-                })
-                .AddCookie("Cookies", options =>
-                {
-                    options.AccessDeniedPath = "/Error/403";
-                })
-                .AddOpenIdConnect("oidc", options =>
-                {
-                    options.SignInScheme = "Cookies";
-
-                    options.Authority = _authConfig.Authority;
-                    options.MetadataAddress = _authConfig.MetaDataAddress;
-                    options.RequireHttpsMetadata = false;
-                    options.ResponseType = "code";
-                    options.ClientId = _authConfig.ClientId;
-                    options.ClientSecret = _authConfig.ClientSecret;
-                    options.Scope.Add("profile");                    
-                  
-                    options.Events.OnTokenValidated = PopulateAccountsClaim;
-                });
-            }
-        }
-
-        private Task PopulateAccountsClaim(TokenValidatedContext ctx)
-        {
-            var userId = ctx.Principal.Claims.First(c => c.Type.Equals(EmployerRecruitClaims.IdamsUserIdClaimTypeIdentifier)).Value;
-            var accounts = _accountsSvc.GetAssociatedAccounts(userId);
-
-            var accountsConcatenated = string.Join(",", accounts);
-            var associatedAccountClaim = new Claim(EmployerRecruitClaims.AccountsClaimsTypeIdentifier, accountsConcatenated, ClaimValueTypes.String);
-
-            ctx.Principal.Identities.First().AddClaim(associatedAccountClaim);
-
-            return Task.CompletedTask;
-        }
-
-        private void ConfigureAuthorization(IServiceCollection services)
-        {
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("HasEmployerAccount", policy => 
-                {
-                    policy.RequireAuthenticatedUser();
-                    policy.RequireClaim(EmployerRecruitClaims.AccountsClaimsTypeIdentifier);
-                    policy.Requirements.Add(new EmployerAccountRequirement());
-                });
-            });
-
-            services.AddSingleton<IAuthorizationHandler, EmployerAccountHandler>();
         }
     }
 }
