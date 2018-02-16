1aec0bf20c10816ca61d11f1a4a5b168c0f12e9b
Chris Jensen <chrisjensenuk@gmail.com>
2018-02-16 11:52:56 +0000

    ER-240 - Using mongo ConnectionString for configuration

--- src/Recruit.Storage.Client/Core/Mongo/MongoDbCollectionBase.cs
+++ src/Recruit.Storage.Client/Core/Mongo/MongoDbCollectionBase.cs
@@ -11,30 +11,19 @@ namespace Esfa.Recruit.Storage.Client.Core.Mongo
     {
         private readonly string _dbName;
         private readonly string _collectionName;
-        private readonly MongoDbConnectionDetails _details;
-
-        private const string MongoCredentialMechanism = "SCRAM-SHA-1";
-
-        public MongoDbCollectionBase(string dbName, string collectionName, IOptions<MongoDbConnectionDetails> details)
+        private readonly MongoDbConnectionDetails _config;
+        
+        public MongoDbCollectionBase(string dbName, string collectionName, IOptions<MongoDbConnectionDetails> config)
         {
             _dbName = dbName;
             _collectionName = collectionName;
-            _details = details.Value;
+            _config = config.Value;
         }
 
         protected IMongoCollection<T> GetCollection<T>()
         {
-            var settings = new MongoClientSettings
-            {
-                Server = new MongoServerAddress(_details.Host, _details.Port),
-                UseSsl = true,
-                SslSettings = new SslSettings { EnabledSslProtocols = SslProtocols.Tls12 }
-            };
-
-            MongoIdentity identity = new MongoInternalIdentity(_dbName, _details.User);
-            MongoIdentityEvidence evidence = new PasswordEvidence(_details.Password);
-
-            settings.Credential = new MongoCredential(MongoCredentialMechanism, identity, evidence);
+            var settings = MongoClientSettings.FromUrl(new MongoUrl(_config.ConnectionString));
+            settings.SslSettings = new SslSettings { EnabledSslProtocols = SslProtocols.Tls12 };
 
             var client = new MongoClient(settings);
             var database = client.GetDatabase(_dbName);
--- src/Recruit.Storage.Client/Core/Mongo/MongoDbConnectionDetails.cs
+++ src/Recruit.Storage.Client/Core/Mongo/MongoDbConnectionDetails.cs
@@ -2,9 +2,6 @@
 {
     public class MongoDbConnectionDetails
     {
-        public string User { get; set; }
-        public string Host { get; set; }
-        public int Port { get; set; }
-        public string Password { get; set; }
+        public string ConnectionString { get; set; }
     }
 }
