40b19939df19e0e7a5181bb19e677fc4e8a10da1
Chris Jensen <chrisjensenuk@gmail.com>
2018-02-16 11:57:00 +0000

    ER-240 - Added IoC Registry to Recruit.Storage.Client

--- src/Employer.Web/Configuration/IoC.cs
+++ src/Employer.Web/Configuration/IoC.cs
@@ -1,16 +1,9 @@
 ﻿using Employer.Web.Services;
 using Esfa.Recruit.Employer.Web.Orchestrators;
-using Esfa.Recruit.Storage.Client.Core.Entities;
-using Esfa.Recruit.Storage.Client.Core.Handlers;
-using Esfa.Recruit.Storage.Client.Core.Messaging;
-using Esfa.Recruit.Storage.Client.Core.Mongo;
-using Esfa.Recruit.Storage.Client.Core.Repositories;
-using Esfa.Recruit.Storage.Client.Core.Services;
-using MediatR;
+using Esfa.Recruit.Storage.Client.Core.Ioc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using MongoDB.Bson.Serialization;
 
 namespace Esfa.Recruit.Employer.Web.Configuration
 {
@@ -18,37 +11,18 @@ namespace Esfa.Recruit.Employer.Web.Configuration
     {
         public static void AddIoC(this IServiceCollection services, IConfiguration configuration)
         {
+            //Register Recruit.Storage.Client IoC
+            services.RegisterRecruitStorageClient(configuration);
+
             //Configuration
             services.Configure<ExternalLinksConfiguration>(configuration.GetSection("ExternalLinks"));
             
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>(); // Used by NLog to log out traceidentifier value.
 
             services.AddTransient<IGetAssociatedEmployerAccountsService, GetAssociatedEmployerAccountsService>();
-
-            //Mediatr
-            services.AddMediatR(typeof(CreateVacancyCommandHandler).Assembly);
-            services.AddTransient<IMessaging, MediatrMessaging>();
-
+            
             //Orchestrators
             services.AddTransient<NewVacancyOrchestrator, NewVacancyOrchestrator>();
-
-            //Core Services
-            services.AddTransient<IdGenerator, IdGenerator>();
-
-            //Repositories
-            var mongoConfig = configuration.GetSection("MongoDbConnectionDetails");
-            services.Configure<MongoDbConnectionDetails>(mongoConfig);
-
-            if (mongoConfig.Get<MongoDbConnectionDetails>() == null)
-            {
-                services.AddSingleton<IVacancyRepository, StubVacancyRepository>();
-            }
-            else
-            {
-                MongoDbVacancyRepository.RegisterMongoMappings();
-                services.AddTransient<IVacancyRepository, MongoDbVacancyRepository>();
-            }
-            
         }
     }
 }
---
+++ src/Recruit.Storage.Client/Core/Ioc/Registry.cs
@@ -0,0 +1,40 @@
+﻿using Esfa.Recruit.Storage.Client.Core.Handlers;
+using Esfa.Recruit.Storage.Client.Core.Messaging;
+using Esfa.Recruit.Storage.Client.Core.Mongo;
+using Esfa.Recruit.Storage.Client.Core.Repositories;
+using Esfa.Recruit.Storage.Client.Core.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+
+namespace Esfa.Recruit.Storage.Client.Core.Ioc
+{
+    public static class Registry
+    {
+        public static void RegisterRecruitStorageClient(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(typeof(CreateVacancyCommandHandler).Assembly);
+            services.AddTransient<IMessaging, MediatrMessaging>();
+            
+            services.AddTransient<IdGenerator, IdGenerator>();
+
+            services.AddRepositories(configuration);
+        }
+
+        private static void AddRepositories(this IServiceCollection services, IConfiguration configuration)
+        {
+            IConfigurationSection mongoConfig = configuration.GetSection("MongoDbConnectionDetails");
+            services.Configure<MongoDbConnectionDetails>(mongoConfig);
+
+            if (mongoConfig.Get<MongoDbConnectionDetails>() == null)
+            {
+                services.AddSingleton<IVacancyRepository, StubVacancyRepository>();
+            }
+            else
+            {
+                MongoDbVacancyRepository.RegisterMongoMappings();
+                services.AddTransient<IVacancyRepository, MongoDbVacancyRepository>();
+            }
+        }
+    }
+}
