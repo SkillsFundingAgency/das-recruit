f5f55cd21420e41ff7a8cf5dbd49ca4b31eac936
sachin.patel <sachin.patel@valtech.co.uk>
2018-02-21 15:32:07 +0000

    ER-219 :construction: - Adding method to vacancy repo for reading vacancies by employer account id.

--- src/Recruit.Storage.Client/Core/Repositories/IQueryVacancyRepository.cs
+++ src/Recruit.Storage.Client/Core/Repositories/IQueryVacancyRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Esfa.Recruit.Storage.Client.Core.Entities;
 
@@ -7,5 +8,6 @@ namespace Esfa.Recruit.Storage.Client.Core.Repositories
     public interface IQueryVacancyRepository
     {
         Task<Vacancy> GetVacancyAsync(Guid vacancyId);
+        Task<IEnumerable<Vacancy>> GetVacanciesAsync(string employerAccountId);
     }
 }
