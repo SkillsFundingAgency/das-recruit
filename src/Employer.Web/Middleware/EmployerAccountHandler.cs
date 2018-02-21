67192a2f6cbd52d0f681671ec41cfbeec986eb35
sachin.patel <sachin.patel@valtech.co.uk>
2018-02-21 15:42:11 +0000

    ER-219 :construction: - Hardening up code for verifying employer account id for current user and making use of updated get account service which has an async call.

--- src/Employer.Web/Middleware/EmployerAccountHandler.cs
+++ src/Employer.Web/Middleware/EmployerAccountHandler.cs
@@ -2,6 +2,7 @@
 using Esfa.Recruit.Employer.Web.Configuration;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,18 +10,16 @@ namespace Employer.Web.Middleware
 {
     public class EmployerAccountHandler : AuthorizationHandler<EmployerAccountRequirement>
     {
-        private const string EmployerAccountIdRouteDataKey = "employerAccountId";
-
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, EmployerAccountRequirement requirement)
         {
-            if (context.Resource is AuthorizationFilterContext mvcContext && mvcContext.RouteData.Values.ContainsKey(EmployerAccountIdRouteDataKey))
+            if (context.Resource is AuthorizationFilterContext mvcContext && mvcContext.RouteData.Values.ContainsKey(RouteValues.EmployerAccountId))
             {
                 if (context.User.HasClaim(c => c.Type.Equals(EmployerRecruitClaims.AccountsClaimsTypeIdentifier)))
                 {
-                    var accountIdFromUrl = mvcContext.RouteData.Values[EmployerAccountIdRouteDataKey];
+                    string accountIdFromUrl = mvcContext.RouteData.Values[RouteValues.EmployerAccountId].ToString().ToUpper();
                     var employerAccountClaim = context.User.FindFirst(c => c.Type.Equals(EmployerRecruitClaims.AccountsClaimsTypeIdentifier));
 
-                    if (employerAccountClaim.Value.Split(',').Contains(accountIdFromUrl))
+                    if (employerAccountClaim != null && employerAccountClaim.Value.Split(',', StringSplitOptions.RemoveEmptyEntries).Contains(accountIdFromUrl))
                     {
                         context.Succeed(requirement);
                     }
