fb99c433668a98c29fe4a38ff6ea01d3404c91d4
sachin.patel <sachin.patel@valtech.co.uk>
2018-02-22 11:31:44 +0000

    ER-219 :lock: - Reducing number of args on a post action, intended to take Route level argument and bind it into the viewmodel argument.

--- src/Employer.Web/ViewModels/CreateVacancy/IndexViewModel.cs
+++ src/Employer.Web/ViewModels/CreateVacancy/IndexViewModel.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Esfa.Recruit.Employer.Web.ViewModels.CreateVacancy
 {
@@ -10,5 +7,8 @@ namespace Esfa.Recruit.Employer.Web.ViewModels.CreateVacancy
     {
         [Required]
         public string Title { get; set; }
+        [Required]
+        [FromRoute]
+        public string EmployerAccountId { get; set; }
     }
 }
