dad0dbb906cbf83236bd12dbea0fa0bd7ba84dce
sachin.patel <sachin.patel@valtech.co.uk>
2018-02-27 08:33:45 +0000

    ER-219 :construction: - DashboardOrchestrator makes use of VacancyClient for retrieving dashboard data.

--- src/Employer.Web/Orchestrators/DashboardOrchestrator.cs
+++ src/Employer.Web/Orchestrators/DashboardOrchestrator.cs
@@ -1,27 +1,27 @@
 ﻿using Esfa.Recruit.Employer.Web.Mappings;
 using Esfa.Recruit.Employer.Web.Services;
 using Esfa.Recruit.Employer.Web.ViewModels;
-using Esfa.Recruit.Vacancies.Client.Domain.QueryStore;
+using Esfa.Recruit.Vacancies.Client.Infrastructure.Client;
 using System.Threading.Tasks;
 
 namespace Esfa.Recruit.Employer.Web.Orchestrators
 {
     public class DashboardOrchestrator
     {
-        private readonly IQueryStoreReader _queryRepository;
+        private readonly IVacancyClient _vacancyClient;
         private readonly IEmployerAccountService _getAccountService;
         private readonly DashboardMapper _mapper = new DashboardMapper();
 
-        public DashboardOrchestrator(IEmployerAccountService getAccountsService, IQueryStoreReader queryRepository)
+        public DashboardOrchestrator(IEmployerAccountService getAccountsService, IVacancyClient vacancyClient)
         {
             _getAccountService = getAccountsService;
-            _queryRepository = queryRepository;
+            _vacancyClient = vacancyClient;
         }
 
         public async Task<DashboardViewModel> GetDashboardViewModelAsync(string employerAccountId)
         {
             var account = _getAccountService.GetAccountDetailAsync(employerAccountId);
-            var dashboard = _queryRepository.GetDashboardAsync(employerAccountId);
+            var dashboard = _vacancyClient.GetDashboardAsync(employerAccountId);
             await Task.WhenAll(account, dashboard);
 
             var vm = _mapper.MapFromDashboard(dashboard.Result, account.Result);
