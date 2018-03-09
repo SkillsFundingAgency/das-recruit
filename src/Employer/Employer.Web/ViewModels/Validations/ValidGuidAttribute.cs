51271da375537e104ef6a5e7f1ddaab59f74d654
Chris Jensen <chrisjensenuk@gmail.com>
2018-03-09 15:57:54 +0000

    ER-269 - Added custom validation attributes for Guid and Postcode

---
+++ src/Employer/Employer.Web/ViewModels/Validations/ValidGuidAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Esfa.Recruit.Employer.Web.ViewModels.Validations
+{
+    public class ValidGuidAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            //Ensuring guid is not 00000000-0000-0000-0000-000000000000
+            return !Equals((Guid)value, default(Guid));
+        }
+    }
+}
