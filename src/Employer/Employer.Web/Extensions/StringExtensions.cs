6a383146bb590efc5fb572b4c81e7398c9afee4f
Chris Jensen <chrisjensenuk@gmail.com>
2018-03-09 16:00:46 +0000

    ER-269 - Added String extension to format postcodes

---
+++ src/Employer/Employer.Web/Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Esfa.Recruit.Employer.Web.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string AsPostCode(this string postcode)
+        {
+            if (!string.IsNullOrEmpty(postcode) && postcode.Length > 3)
+            {
+                postcode = postcode.ToUpper().Replace(" ", "");
+                var chars = postcode.ToCharArray().ToList();
+                chars.Insert(chars.Count - 3, ' ');
+                postcode = new string(chars.ToArray());
+            }
+
+            return postcode;
+        }
+    }
+}
