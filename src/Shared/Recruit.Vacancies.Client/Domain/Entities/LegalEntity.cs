dbd4226aa4543290181987c4c5a76a9deca7abd8
sachin.patel <sachin.patel@valtech.co.uk>
2018-03-16 11:17:27 +0000

    ER-285 :construction: :umbrella: - Adding mapper for LegalEntities and LegalEntity address lines along with some tests.

---
+++ src/Shared/Recruit.Vacancies.Client/Domain/Entities/LegalEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Esfa.Recruit.Vacancies.Client.Domain.Entities
+{
+    public class LegalEntity
+    {
+        public long LegalEntityId { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Status { get; set; }
+        public Address Address { get; set; }
+    }
+}
---
+++ src/Shared/Recruit.Vacancies.Client/Domain/Mappings/LegalEntityMapper.cs
@@ -0,0 +1,57 @@
+﻿using Esfa.Recruit.Vacancies.Client.Domain.Entities;
+using SFA.DAS.EAS.Account.Api.Types;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Esfa.Recruit.Vacancies.Client.Domain.Mappings
+{
+    public static class LegalEntityMapper
+    {
+        internal static Regex PostcodeRegex => new Regex(@"(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))");
+
+        public static LegalEntity MapFromAccountApiLegalEntity(LegalEntityViewModel data)
+        {
+            return new LegalEntity
+            {
+                LegalEntityId = data.LegalEntityId,
+                Name = data.Name,
+                Code = data.Code,
+                Status = data.Status,
+                Address = MapFromAddressLine(data.Address)
+            };
+        }
+
+        internal static Address MapFromAddressLine(string address)
+        {
+            const string splitChar = ",";
+
+            if (!string.IsNullOrWhiteSpace(address) && address.Contains(splitChar) && PostcodeRegex.IsMatch(address))
+            {
+                var addressParts = address.Split(new[] { splitChar }, StringSplitOptions.RemoveEmptyEntries)
+                                            .Select(s => s.Trim());
+
+                var postcode = addressParts.FirstOrDefault(x => PostcodeRegex.IsMatch(x));
+                var nonPostcodeAddressParts = addressParts.Where(x => !PostcodeRegex.IsMatch(x));
+
+                var legalEntityAddress = new Address
+                {
+                    AddressLine1 = nonPostcodeAddressParts.First(),
+                    AddressLine2 = nonPostcodeAddressParts.ElementAtOrDefault(1),
+                    AddressLine3 = nonPostcodeAddressParts.ElementAtOrDefault(2),
+                    AddressLine4 = nonPostcodeAddressParts.ElementAtOrDefault(3),
+                    Postcode = postcode
+                };
+
+                return legalEntityAddress;
+            }
+
+            if (PostcodeRegex.IsMatch(address))
+            {
+                return new Address { Postcode = address };
+            }
+
+            return new Address { AddressLine1 = address };
+        }
+    }
+}
