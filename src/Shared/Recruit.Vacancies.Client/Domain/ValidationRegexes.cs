40d63bd59857e26f0bcb46fa9a44656c33f0a664
sachin.patel <sachin.patel@valtech.co.uk>
2018-03-16 15:18:44 +0000

    ER-285 :hammer: - Sharing postcode regex used in domain layer with the postcode validation annotation attribute.

--- src/Shared/Recruit.Vacancies.Client/Domain/ValidationRegexes.cs
+++ src/Shared/Recruit.Vacancies.Client/Domain/ValidationRegexes.cs
@@ -2,8 +2,9 @@
 
 namespace Esfa.Recruit.Vacancies.Client.Domain
 {
-    internal static class ValidationRegexes
+    public static class ValidationRegexes
     {
-        internal static Regex PostcodeRegex => new Regex(@"(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))");
+        // See http://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive
+        public static Regex PostcodeRegex => new Regex(@"(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))");
     }
 }
