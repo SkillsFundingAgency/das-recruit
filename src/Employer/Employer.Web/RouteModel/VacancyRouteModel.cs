a47eb1f8eabb51fc55c253230e6364718a62a837
sachin.patel <sachin.patel@valtech.co.uk>
2018-03-16 14:29:01 +0000

    :newspaper: - Adding class to capture RouteData values for single entity vacancy routes.

---
+++ src/Employer/Employer.Web/RouteModel/VacancyRouteModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Esfa.Recruit.Employer.Web.RouteModel
+{
+    public class VacancyRouteModel
+    {
+        [FromRoute]
+        [Required]
+        public Guid VacancyId { get; set; }
+        
+        [FromRoute]
+        [Required]
+        public string EmployerAccountId { get; set; }
+    }
+}
