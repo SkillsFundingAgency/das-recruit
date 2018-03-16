15d52eeb2faad2b2da09fba522a60ce64e5231c2
sachin.patel <sachin.patel@valtech.co.uk>
2018-03-16 15:26:10 +0000

    ER-285 :hammer: - Sharing postcode regex used in domain layer with the postcode validation annotation attribute.

--- src/Employer/Employer.Web/ViewModels/Validations/PostcodeAttribute.cs
+++ src/Employer/Employer.Web/ViewModels/Validations/PostcodeAttribute.cs
@@ -1,13 +1,11 @@
 ﻿namespace Esfa.Recruit.Employer.Web.ViewModels.Validations
 {
+    using Esfa.Recruit.Vacancies.Client.Domain;
     using System.ComponentModel.DataAnnotations;
 
     public class PostcodeAttribute : RegularExpressionAttribute
     {
-        // See http://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive
-        private const string RegexPostcode = @"^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$";
-
-        public PostcodeAttribute() : base(RegexPostcode)
+        public PostcodeAttribute() : base(ValidationRegexes.PostcodeRegex.ToString())
         {
         }
     }
