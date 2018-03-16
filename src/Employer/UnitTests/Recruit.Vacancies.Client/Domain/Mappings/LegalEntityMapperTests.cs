b413b8da92a153fd7e7cbd87bb3f4af9e2d298f8
sachin.patel <sachin.patel@valtech.co.uk>
2018-03-16 11:17:27 +0000

    ER-285 :construction: :umbrella: - Adding mapper for LegalEntities and LegalEntity address lines along with some tests.

---
+++ src/Employer/UnitTests/Recruit.Vacancies.Client/Domain/Mappings/LegalEntityMapperTests.cs
@@ -0,0 +1,66 @@
+﻿using Esfa.Recruit.Vacancies.Client.Domain.Mappings;
+using FluentAssertions;
+using Xunit;
+
+namespace Esfa.Recruit.Employer.UnitTests.Recruit.Vacancies.Client.Domain.Mappings
+{
+    public class LegalEntityMapperTests
+    {
+        [Fact]
+        public void ShouldMapFullFivePartAddress()
+        {
+            var inputAddr = "Valtech, 46 Colebrooke Row,Islington,London, N1 8AF";
+
+            var actualAddr = LegalEntityMapper.MapFromAddressLine(inputAddr);
+
+            actualAddr.AddressLine1.Should().Be("Valtech");
+            actualAddr.AddressLine2.Should().Be("46 Colebrooke Row");
+            actualAddr.AddressLine3.Should().Be("Islington");
+            actualAddr.AddressLine4.Should().Be("London");
+            actualAddr.Postcode.Should().Be("N1 8AF");
+        }
+
+        [Fact]
+        public void ShouldMapFullThreePartAddress()
+        {
+            var inputAddr = "46 Colebrooke Row,London, N1 8AF";
+
+            var actualAddr = LegalEntityMapper.MapFromAddressLine(inputAddr);
+
+            actualAddr.AddressLine1.Should().Be("46 Colebrooke Row");
+            actualAddr.AddressLine2.Should().Be("London");
+            actualAddr.AddressLine3.Should().BeNull();
+            actualAddr.AddressLine4.Should().BeNull();
+
+            actualAddr.Postcode.Should().Be("N1 8AF");
+        }
+
+        [Fact]
+        public void ShouldMapOnlyPostcodeAddress()
+        {
+            var inputAddr = "N1 8AF";
+
+            var actualAddr = LegalEntityMapper.MapFromAddressLine(inputAddr);
+
+            actualAddr.AddressLine1.Should().BeNull();
+            actualAddr.AddressLine2.Should().BeNull();
+            actualAddr.AddressLine3.Should().BeNull();
+            actualAddr.AddressLine4.Should().BeNull();
+            actualAddr.Postcode.Should().Be("N1 8AF");
+        }
+
+        [Fact]
+        public void ShouldMapOnlySinglePartNonPostcodeAddress()
+        {
+            var inputAddr = "46 Colebrooke Row";
+
+            var actualAddr = LegalEntityMapper.MapFromAddressLine(inputAddr);
+
+            actualAddr.AddressLine1.Should().Be("46 Colebrooke Row");
+            actualAddr.AddressLine2.Should().BeNull();
+            actualAddr.AddressLine3.Should().BeNull();
+            actualAddr.AddressLine4.Should().BeNull();
+            actualAddr.Postcode.Should().BeNull();
+        }
+    }
+}
