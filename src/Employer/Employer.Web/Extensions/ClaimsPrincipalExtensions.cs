7ebc8f98bb05c7f7aec27d17970481df4368dc69
sachin.patel <sachin.patel@valtech.co.uk>
2018-03-19 13:57:31 +0000

    :zap: - Getting display name of current user from an extension method to the ClaimsPrincipal.

---
+++ src/Employer/Employer.Web/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Esfa.Recruit.Employer.Web.Configuration;
+using System.Security.Claims;
+
+namespace Esfa.Recruit.Employer.Web.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetDisplayName(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(EmployerRecruitClaims.IdamsUserDisplayNameClaimTypeIdentifier);
+        }
+    }
+}
