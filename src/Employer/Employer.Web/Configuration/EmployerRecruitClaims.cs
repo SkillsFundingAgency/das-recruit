2d0bdd1cbba2ab3b47acb88ffec3e70f4bb8ee49
sachin.patel <sachin.patel@valtech.co.uk>
2018-03-19 08:14:24 +0000

    :zap: - Adding references to more of the claims coming from Idams.

--- src/Employer/Employer.Web/Configuration/EmployerRecruitClaims.cs
+++ src/Employer/Employer.Web/Configuration/EmployerRecruitClaims.cs
@@ -3,6 +3,8 @@
     public static class EmployerRecruitClaims
     {
         public static string IdamsUserIdClaimTypeIdentifier = "http://das/employer/identity/claims/id";
+        public static string IdamsUserDisplayNameClaimTypeIdentifier = "http://das/employer/identity/claims/display_name";
+        public static string IdamsUserEmailClaimTypeIdentifier = "http://das/employer/identity/claims/email_address";
         public static string AccountsClaimsTypeIdentifier = "http://das/employer/identity/claims/associatedAccounts";
     }
 }
