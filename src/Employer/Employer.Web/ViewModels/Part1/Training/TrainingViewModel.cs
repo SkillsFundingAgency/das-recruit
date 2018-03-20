23294a16c0417265df13e47c49726bb455863de6
sachin.patel <sachin.patel@valtech.co.uk>
2018-03-20 08:21:34 +0000

    ER-272 :lipstick: - Moving logic out of view for what the min year or date fields on the training selections page should be.

--- src/Employer/Employer.Web/ViewModels/Part1/Training/TrainingViewModel.cs
+++ src/Employer/Employer.Web/ViewModels/Part1/Training/TrainingViewModel.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Esfa.Recruit.Employer.Web.ViewModels.Part1.Training
 {
     public class TrainingViewModel : TrainingEditModel
     {
+        public int MinYear => DateTime.UtcNow.Year;
         public IEnumerable<ApprenticeshipProgrammeViewModel> Programmes { get; set; }
     }
 
