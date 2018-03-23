122cb1639175737e1f2724b529fdcec22af4b7ea
sachin.patel <sachin.patel@valtech.co.uk>
2018-03-23 09:59:05 +0000

    ER-296 :art: :construction: - Vacancy preview page restructure to latest prototype. Still needs fleshing out.

--- src/Shared/Recruit.Vacancies.Client/Domain/Entities/Programme.cs
+++ src/Shared/Recruit.Vacancies.Client/Domain/Entities/Programme.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Esfa.Recruit.Vacancies.Client.Domain.Projections;
+﻿using Esfa.Recruit.Vacancies.Client.Domain.Projections;
 
 namespace Esfa.Recruit.Vacancies.Client.Domain.Entities
 {
