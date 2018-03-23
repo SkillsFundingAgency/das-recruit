4e09150256dadd58e160dd5c20ca466c1cf8d0fe
Chris Jensen <chrisjensenuk@gmail.com>
2018-03-23 10:22:44 +0000

    ER-290 - Added validation spans and thoughts on how best to handle multiple invalid custom skills

--- src/Employer/Employer.Web/Controllers/Part2/SkillsController.cs
+++ src/Employer/Employer.Web/Controllers/Part2/SkillsController.cs
@@ -27,6 +27,18 @@ namespace Esfa.Recruit.Employer.Web.Controllers.Part2
         [HttpPost("skills", Name = RouteNames.Skills_Post)]
         public async Task<IActionResult> Skills(SkillsEditModel m)
         {
+            /*
+            If custom skills have been added via javascript then m.Skills could contain multiple invalid entries.
+            As we only have one custom skills textbox we can only fix one at a time. 
+            I think the best we can do is map the first erroring custom skill to the AddCustomSkillName input and 
+            add the error to the AddCustomSkillName ModelState. We then remove this skill from the CustomSkills list and the 
+            error from the Skills ModelState.
+            The next invalid custom skill will be picked up the next time we try and submit the page and we repeat.
+
+            An alternative is we also replicate the validation in javascript and validate on the client however this adds to the burden.
+            Another alternative is we ajax the validation to be done on the server. However this latency will impact on UX.
+            */
+
             if (!ModelState.IsValid)
             {
                 var vm = await _orchestrator.GetSkillsViewModelAsync(m);
