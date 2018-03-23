55cb7056de3acaf506504fee62643b3b2b4b346c
Chris Jensen <chrisjensenuk@gmail.com>
2018-03-23 15:16:43 +0000

    ER-290 - Removing reliance on SkillViewModel Id

--- src/Employer/Employer.Web/ViewModels/Part2/Skills/SkillsViewModel.cs
+++ src/Employer/Employer.Web/ViewModels/Part2/Skills/SkillsViewModel.cs
@@ -4,17 +4,15 @@ namespace Esfa.Recruit.Employer.Web.ViewModels.Part2.Skills
 {
     public class SkillsViewModel : SkillsEditModel
     {
-        public IEnumerable<SkillViewModel> Column1Checkboxes { get; set; }
-        public IEnumerable<SkillViewModel> Column2Checkboxes { get; set; }
-        public IEnumerable<string> CustomSkills { get; set; }
+        public List<SkillViewModel> Column1Checkboxes { get; set; }
+        public List<SkillViewModel> Column2Checkboxes { get; set; }
+        public List<string> CustomSkills { get; set; }
     }
 
     public class SkillViewModel
     {
         public string Name { get; set; }
         public bool Selected { get; set; }
-
-        public string Id => Name.Replace(" ", "-").ToLower();
     }
     
 }
