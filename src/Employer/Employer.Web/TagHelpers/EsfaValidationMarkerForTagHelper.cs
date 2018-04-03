9a86afd9320e42f3be4b01c492d1dbb756cf7fc4
sachin.patel <sachin.patel@valtech.co.uk>
2018-04-04 00:05:11 +0100

    ER-309 :sparkle: - Adding EsfaValidationMarkerForTagHelper to place a vertical bar around errored section on a form.

---
+++ src/Employer/Employer.Web/TagHelpers/EsfaValidationMarkerForTagHelper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace Esfa.Recruit.Employer.Web.TagHelpers
+{
+    [HtmlTargetElement("div", Attributes = ValidationForAttributeName)]
+    public class EsfaValidationMarkerForTagHelper : TagHelper
+    {
+        private const string ValidationForAttributeName = "esfa-validation-marker-for";
+        private const string ClassAttributeIdentifier = "class";
+        private const string ErrorClassSpecifier = "error";
+        
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        [HtmlAttributeName(ValidationForAttributeName)]
+        public ModelExpression For { get; set; }
+        
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (ViewContext.ModelState.TryGetValue(For.Name, out var modelStateEntry) && modelStateEntry.ValidationState == ModelValidationState.Invalid)
+            {
+                if (output.Attributes.ContainsName(ClassAttributeIdentifier))
+                {
+                    output.Attributes.TryGetAttribute(ClassAttributeIdentifier, out var classAttr);
+                    output.Attributes.SetAttribute(ClassAttributeIdentifier, $"{classAttr.Value} {ErrorClassSpecifier}");
+                }
+                else
+                {
+                    output.Attributes.SetAttribute(ClassAttributeIdentifier, ErrorClassSpecifier);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
