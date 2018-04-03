91feacff3667a4bd490fbc8654a826e991c28d59
Chris Jensen <chrisjensenuk@gmail.com>
2018-04-03 15:21:30 +0100

    ER-290 - Adding Skills validation to web project

--- src/Employer/Employer.Web/Controllers/Part2/SkillsController.cs
+++ src/Employer/Employer.Web/Controllers/Part2/SkillsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Esfa.Recruit.Employer.Web.Configuration;
 using Esfa.Recruit.Employer.Web.Configuration.Routing;
+using Esfa.Recruit.Employer.Web.Orchestrators;
 using Esfa.Recruit.Employer.Web.Orchestrators.Part2;
 using Esfa.Recruit.Employer.Web.ViewModels.Part2.Skills;
 using Microsoft.AspNetCore.Mvc;
@@ -32,25 +33,6 @@ namespace Esfa.Recruit.Employer.Web.Controllers.Part2
         [HttpPost("skills", Name = RouteNames.Skills_Post)]
         public async Task<IActionResult> Skills(SkillsEditModel m)
         {
-            /*
-            If custom skills have been added via javascript then m.Skills could contain multiple invalid entries.
-            As we only have one custom skills textbox we can only fix one at a time. 
-            I think the best we can do is map the first erroring custom skill to the AddCustomSkillName input and 
-            add the error to the AddCustomSkillName ModelState. We then remove this skill from the CustomSkills list and the 
-            error from the Skills ModelState.
-            The next invalid custom skill will be picked up the next time we try and submit the page and we repeat.
-
-            An alternative is we also replicate the validation in javascript and validate on the client however this adds to the burden.
-            Another alternative is we ajax the validation to be done on the server. However this latency will impact on UX.
-            */
-
-            if (!ModelState.IsValid)
-            {
-                var vm = await _orchestrator.GetSkillsViewModelAsync(m);
-
-                return View(vm);
-            }
-
             if (m.IsAddingCustomSkill || m.IsRemovingCustomSkill)
             {
                 HandleCustomSkillChange(m);
@@ -58,11 +40,23 @@ namespace Esfa.Recruit.Employer.Web.Controllers.Part2
                 return RedirectToRoute(RouteNames.Skills_Get);
             }
 
-            await _orchestrator.PostSkillsEditModelAsync(m);
+            var response = await _orchestrator.PostSkillsEditModelAsync(m);
 
+            if (!response.Success)
+            {
+                response.AddErrorsToModelState(ModelState);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                var vm = await _orchestrator.GetSkillsViewModelAsync(m);
+
+                return View(vm);
+            }
+            
             return RedirectToRoute(RouteNames.Preview_Index_Get);
         }
-
+        
         private void HandleCustomSkillChange(SkillsEditModel m)
         {
             var skills = m.Skills ?? new List<string>();
---
+++ src/Employer/Employer.Web/Extensions/EntityValidationErrorExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Esfa.Recruit.Vacancies.Client.Application.Validation;
+
+namespace Esfa.Recruit.Employer.Web.Extensions
+{
+    public static class EntityValidationErrorExtensions
+    {
+        private const char PositionStart = '[';
+        private const char PositionEnd = ']';
+
+        public static int? GetPosition(this EntityValidationError error)
+        {
+            var start = error.PropertyName.IndexOf(PositionStart) + 1;
+
+            if (start == 0)
+            {
+                return null;
+            }
+
+            var end = error.PropertyName.IndexOf(PositionEnd, start);
+
+            if (end == -1)
+            {
+                return null;
+            }
+
+            if (int.TryParse(error.PropertyName.Substring(start, end - start), out var position))
+            {
+                return position;
+            }
+
+            return null;
+        }
+    }
+}
+
