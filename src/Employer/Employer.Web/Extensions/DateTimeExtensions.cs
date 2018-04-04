aae2fa7348bbfbdef4931eb413cb7a75901611a2
sachin.patel <sachin.patel@valtech.co.uk>
2018-04-04 15:16:37 +0100

    :hammer: - Cleaning use of fully qualified class in Ioc. Removing redundant code in DateTimeExtensions.

--- src/Employer/Employer.Web/Extensions/DateTimeExtensions.cs
+++ src/Employer/Employer.Web/Extensions/DateTimeExtensions.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Esfa.Recruit.Employer.Web.Extensions
 {
@@ -10,7 +6,6 @@ namespace Esfa.Recruit.Employer.Web.Extensions
     public static class DateTimeExtensions
     {
         private const string DisplayDateFormat = "dd MMM yyyy";
-        const int incodeLength = 3;
         
         public static string AsDisplayDate(this DateTime date)
         {
