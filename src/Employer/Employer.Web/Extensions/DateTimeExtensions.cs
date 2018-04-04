b7b6b1e8cca2340c92c3828e7246d1545993b72c
sachin.patel <sachin.patel@valtech.co.uk>
2018-04-04 14:30:28 +0100

    :lipstick: - DateTime display formatted date extension method.

---
+++ src/Employer/Employer.Web/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Esfa.Recruit.Employer.Web.Extensions
+{
+
+    public static class DateTimeExtensions
+    {
+        private const string DisplayDateFormat = "dd MMM yyyy";
+        const int incodeLength = 3;
+        
+        public static string AsDisplayDate(this DateTime date)
+        {
+            return date.ToString(DisplayDateFormat);
+        }
+    }
+}
