c2e30d6f2bcaa23fd3b874a7b944aa34068b5f0e
Steve Holder <steve.holder@reavalleyit.com>
2018-04-12 15:04:52 +0100

    Added functionality to expand/collapse textareas

--- src/Employer/Employer.Web/wwwroot/javascripts/application.js
+++ src/Employer/Employer.Web/wwwroot/javascripts/application.js
@@ -68,7 +68,16 @@ $(window).scroll(function () {
     }
 });
 
-//Legacy menu script
 $(function() {
+    //Legacy menu script
     sfa.navigation.init();
+    // expanding /collapsing text areas
+    $('.textarea-expandable').each(function () {
+        this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
+    }).on('input', function () {
+        this.style.height = 'auto';
+        this.style.height = (this.scrollHeight) + 'px';
+    });
 });
+
+   
