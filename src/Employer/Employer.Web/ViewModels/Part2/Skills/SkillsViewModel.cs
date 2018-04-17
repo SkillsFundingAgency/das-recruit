92386297006ee1789a20263ced05a49c53fcb05c
Steve Holder <steve.holder@reavalleyit.com>
2018-04-17 13:39:30 +0100

    Amended copy on skills screen

--- src/Employer/Employer.Web/ViewModels/Part2/Skills/SkillsViewModel.cs
+++ src/Employer/Employer.Web/ViewModels/Part2/Skills/SkillsViewModel.cs
@@ -4,6 +4,7 @@ namespace Esfa.Recruit.Employer.Web.ViewModels.Part2.Skills
 {
     public class SkillsViewModel : SkillsEditModel
     {
+        public string Title { get; internal set; }
         public List<SkillViewModel> Column1Checkboxes { get; set; }
         public List<SkillViewModel> Column2Checkboxes { get; set; }
         public List<string> CustomSkills { get; set; }
