ee2b5b54ca27e98cef79b7ce6c4b6d41a365f4db
sachin.patel <sachin.patel@valtech.co.uk>
2018-04-18 13:21:13 +0100

    ER-303 :lock: - Creating AuthorisationException

---
+++ src/Shared/Recruit.Vacancies.Client/Domain/Exceptions/AuthorisationException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Esfa.Recruit.Vacancies.Client.Domain.Exceptions
+{
+    [Serializable]
+    public class AuthorisationException : Exception
+    {
+        public AuthorisationException(string message) : base(message) { }
+    }
+}
