46b5ed85072b01b26600dee10aaa7a76c4095821
sachin.patel <sachin.patel@valtech.co.uk>
2018-04-20 13:15:47 +0100

    ER-295 :hammer: - Fix for making sure that the user only lands on the confirm training provider page if they come directly from posting the select training provider form.

--- src/Employer/Employer.Web/Configuration/Routing/RouteValues.cs
+++ src/Employer/Employer.Web/Configuration/Routing/RouteValues.cs
@@ -3,5 +3,6 @@
     public static class RouteValues
     {
         public const string EmployerAccountId = "employerAccountId";
+        public const string VacancyId = "vacancyId";
     }
 }
--- src/Employer/Employer.Web/Controllers/Part2/TrainingProviderController.cs
+++ src/Employer/Employer.Web/Controllers/Part2/TrainingProviderController.cs
@@ -7,6 +7,7 @@ using Esfa.Recruit.Employer.Web.Orchestrators.Part2;
 using Esfa.Recruit.Employer.Web.Orchestrators;
 using Esfa.Recruit.Employer.Web.RouteModel;
 using Esfa.Recruit.Vacancies.Client.Infrastructure.Client;
+using Esfa.Recruit.Employer.Web.Configuration;
 
 namespace Esfa.Recruit.Employer.Web.Controllers.Part2
 {
@@ -15,6 +16,7 @@ namespace Esfa.Recruit.Employer.Web.Controllers.Part2
     {
         private readonly TrainingProviderOrchestrator _orchestrator;
         private readonly IEmployerVacancyClient _client;
+        private const string TrainingProviderJourneyTempDataKey = "FromSelectTrainingProvider";
         private const string InvalidUkprnMessageFormat = "The UKPRN {0} is not valid or the associated provider is not active.";
 
         public TrainingProviderController(TrainingProviderOrchestrator orchestrator, IEmployerVacancyClient client)
@@ -45,12 +47,17 @@ namespace Esfa.Recruit.Employer.Web.Controllers.Part2
                 return await ProviderNotFound(m);
 
             var confirmDetailsVm = await _orchestrator.GetConfirmViewModel(m);
+            TempData.Add(TrainingProviderJourneyTempDataKey, 1);
             return RedirectToRoute(RouteNames.TrainingProvider_Confirm_Get, confirmDetailsVm);
         }
 
         [HttpGet("confirm-training-provider", Name = RouteNames.TrainingProvider_Confirm_Get)]
         public async Task<IActionResult> ConfirmTrainingProvider(ConfirmTrainingProviderViewModel confirmDetailsVm)
         {
+            if (!TempData.ContainsKey(TrainingProviderJourneyTempDataKey))
+                return RedirectToRoute(RouteNames.TrainingProvider_Select_Get);
+
+            TempData.Remove(TrainingProviderJourneyTempDataKey);
             var vacancy = await _client.GetVacancyAsync(confirmDetailsVm.VacancyId);
             _orchestrator.CheckAuthorisedAccess(vacancy, confirmDetailsVm.EmployerAccountId);
             return View(confirmDetailsVm);
@@ -82,7 +89,7 @@ namespace Esfa.Recruit.Employer.Web.Controllers.Part2
             if (!ModelState.IsValid)
             {
                 var vm = await _orchestrator.GetSelectTrainingProviderViewModel(m);
-                return View("SelectTrainingProvider", vm);
+                return View(ViewNames.SelectTrainingProvider, vm);
             }
 
             return RedirectToRoute(RouteNames.Vacancy_Preview_Get);
@@ -92,7 +99,7 @@ namespace Esfa.Recruit.Employer.Web.Controllers.Part2
         {
             ModelState.AddModelError(string.Empty, string.Format(InvalidUkprnMessageFormat, m.Ukprn));
             var vm = await _orchestrator.GetSelectTrainingProviderViewModel(m);
-            return View("SelectTrainingProvider", vm);
+            return View(ViewNames.SelectTrainingProvider, vm);
         }
     }
 }
