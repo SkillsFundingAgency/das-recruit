2552c95e67244ee6d633533af0169c261fb5bee9
Lee Wadhams <lee.wadhams@valtech.co.uk>
2018-04-24 11:54:35 +0100

    Copy site assets from Employer
    Create default index view

--- src/QA/QA.Web/Controllers/HomeController.cs
+++ src/QA/QA.Web/Controllers/HomeController.cs
@@ -16,21 +16,6 @@ namespace Esfa.Recruit.Qa.Web.Controllers
             return View();
         }
 
-        [Authorize]
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
