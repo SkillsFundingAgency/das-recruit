dc542e9c78a5d4b963d5521ed8488db133272bef
sachin.patel <sachin.patel@valtech.co.uk>
2018-04-26 08:04:39 +0100

    :sparkle: - Adding a custom tag helper to output data-atuomation attribute on html elements to aid acceptance tests that cannot grab particular on page web elements by id, linktext or css class. Will only apply for html generated for non-production environments.

---
+++ src/Employer/Employer.Web/TagHelpers/EsfaAutomationTestElementTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace Esfa.Recruit.Employer.Web.TagHelpers
+{
+    [HtmlTargetElement(Attributes = TagAttributeName)]
+    public class EsfaAutomationTestElementTagHelper : TagHelper
+    {
+        private const string TagAttributeName = "esfa-automation";
+        private const string DataAutomationAttributeName = "data-automation";
+        private readonly IHostingEnvironment _env;
+
+        public EsfaAutomationTestElementTagHelper(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HtmlAttributeName(TagAttributeName)]
+        public string TargetName { get; set; }
+        
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!_env.IsProduction())
+            {
+                output.Attributes.SetAttribute(DataAutomationAttributeName, TargetName);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
