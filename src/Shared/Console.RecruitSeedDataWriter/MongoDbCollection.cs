22d9c87912f820050c4919e46948f2e69f52cfc8
sachin.patel <sachin.patel@valtech.co.uk>
2018-05-14 13:11:10 +0100

    ER-340 :sparkles: - Dotnet core console app which writes a specified json data document into a Cosmos Db/Mongo Db specified database and collection.

---
+++ src/Shared/Console.RecruitSeedDataWriter/MongoDbCollection.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Security.Authentication;
+
+namespace Console.RecruitSeedDataWriter
+{
+    internal class MongoDbCollection
+    {
+        private readonly string _collectionName;
+        private readonly MongoUrl _connString;
+
+        public MongoDbCollection(MongoUrl connString, string collectionName)
+        {
+            _connString = connString;
+            _collectionName = collectionName;
+        }
+
+        public IMongoCollection<BsonDocument> GetCollection()
+        {
+            var settings = MongoClientSettings.FromUrl(_connString);
+            settings.SslSettings = new SslSettings { EnabledSslProtocols = SslProtocols.Tls12 };
+
+            var client = new MongoClient(settings);
+            var database = client.GetDatabase(_connString.DatabaseName);
+            var collection = database.GetCollection<BsonDocument>(_collectionName);
+
+            return collection;
+        }
+    }
+}
---
+++ src/Shared/Console.RecruitSeedDataWriter/MongoWriter.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Console.RecruitSeedDataWriter
+{
+    internal class MongoWriter
+    {
+        private const string KeyFieldName = "_id";
+        private readonly WriterOptions _writerOptions;
+
+        public MongoWriter(WriterOptions writerOptions)
+        {
+            _writerOptions = writerOptions;
+        }
+
+        internal async Task Write(BsonDocument bsonDocument, bool canOverwrite = false)
+        {
+            var collection = new MongoDbCollection(_writerOptions.ConnectionString, _writerOptions.CollectionName).GetCollection();
+
+            if (canOverwrite)
+            {   
+                var bsonDocId = bsonDocument.Single(field => field.Name.Equals(KeyFieldName)).Value.AsString;
+                var filter = Builders<BsonDocument>.Filter.Eq(KeyFieldName, bsonDocId);
+                await collection.ReplaceOneAsync(filter, bsonDocument, new UpdateOptions { IsUpsert = true });
+            }
+            else
+            {
+                await collection.InsertOneAsync(bsonDocument);
+            }
+
+            await Task.CompletedTask;
+        }
+    }
+}
