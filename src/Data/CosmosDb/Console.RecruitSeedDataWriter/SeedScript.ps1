1095dca59afb5df0491bad4bf17695f31db93993
sachin.patel <sachin.patel@valtech.co.uk>
2018-05-16 15:38:50 +0100

    ER-340 :scroll: :sparkle: - Powershell script that will take in the db connection string as paaramater and run through the seed script agianst the database.

---
+++ src/Data/CosmosDb/Console.RecruitSeedDataWriter/SeedScript.ps1
@@ -0,0 +1,9 @@
+﻿
+param(
+    [string]$dbConnectionString
+)
+
+& dotnet Console.RecruitSeedDataWriter.dll $dbConnectionString -c sequences -f Sequence\Sequence_Vacancy.json
+& dotnet Console.RecruitSeedDataWriter.dll $dbConnectionString -f Data\CandidateSkills.json -o true
+& dotnet Console.RecruitSeedDataWriter.dll $dbConnectionString -f Data\QualificationTypes.json -o true
+& dotnet Console.RecruitSeedDataWriter.dll $dbConnectionString -f Data\MinimumWageRanges.json -o true
