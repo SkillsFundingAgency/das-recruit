99bf790b8e54a60340560dae99934d992bbeee34
sachin.patel <sachin.patel@valtech.co.uk>
2018-05-17 14:16:04 +0100

    :ambulance: :rocket: - Removing & at the beginnning of lines in powershell script which is causing a release error.

--- src/Data/CosmosDb/Console.RecruitSeedDataWriter/SeedScript.ps1
+++ src/Data/CosmosDb/Console.RecruitSeedDataWriter/SeedScript.ps1
@@ -3,7 +3,7 @@ param(
     [string]$dbConnectionString
 )
 
-& dotnet Esfa.Recruit.Console.RecruitSeedDataWriter.dll $dbConnectionString -c sequences -f Sequence\Sequence_Vacancy.json
-& dotnet Esfa.Recruit.Console.RecruitSeedDataWriter.dll $dbConnectionString -f Data\CandidateSkills.json -o true
-& dotnet Esfa.Recruit.Console.RecruitSeedDataWriter.dll $dbConnectionString -f Data\QualificationTypes.json -o true
-& dotnet Esfa.Recruit.Console.RecruitSeedDataWriter.dll $dbConnectionString -f Data\MinimumWageRanges.json -o true
+dotnet Esfa.Recruit.Console.RecruitSeedDataWriter.dll $dbConnectionString -c sequences -f Sequence\Sequence_Vacancy.json
+dotnet Esfa.Recruit.Console.RecruitSeedDataWriter.dll $dbConnectionString -f Data\CandidateSkills.json -o true
+dotnet Esfa.Recruit.Console.RecruitSeedDataWriter.dll $dbConnectionString -f Data\QualificationTypes.json -o true
+dotnet Esfa.Recruit.Console.RecruitSeedDataWriter.dll $dbConnectionString -f Data\MinimumWageRanges.json -o true
