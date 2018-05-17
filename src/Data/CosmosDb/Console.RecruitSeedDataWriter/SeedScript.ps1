a884ade9b430226499084da130a22ce718ad3e80
sachin.patel <sachin.patel@valtech.co.uk>
2018-05-17 16:11:18 +0100

    :ambulance: :rocket: - Trying string replace of the ampersand with one enclosed in quotes within the string.

--- src/Data/CosmosDb/Console.RecruitSeedDataWriter/SeedScript.ps1
+++ src/Data/CosmosDb/Console.RecruitSeedDataWriter/SeedScript.ps1
@@ -3,6 +3,8 @@ param(
     [string]$dbConnectionString
 )
 
+$dbConnectionString = $dbConnectionString.Replace("&", "\"&\"")
+
 Invoke-Expression "dotnet Esfa.Recruit.Console.RecruitSeedDataWriter.dll ""$dbConnectionString"" -c sequences -f Sequence\Sequence_Vacancy.json"
 Invoke-Expression "dotnet Esfa.Recruit.Console.RecruitSeedDataWriter.dll ""$dbConnectionString"" -f Data\CandidateSkills.json -o true"
 Invoke-Expression "dotnet Esfa.Recruit.Console.RecruitSeedDataWriter.dll ""$dbConnectionString"" -f Data\QualificationTypes.json -o true"
