896c5b78b16d7865243d5c41e0337b7b2d81ca4c
sachin.patel <sachin.patel@valtech.co.uk>
2018-05-18 10:25:53 +0100

    :ambulance: :rocket: - Adding more handling to powershell script.

--- src/Data/CosmosDb/Console.RecruitSeedDataWriter/SeedScript.ps1
+++ src/Data/CosmosDb/Console.RecruitSeedDataWriter/SeedScript.ps1
@@ -4,10 +4,10 @@ param(
 )
 
 try {
-    & dotnet "$PSScriptRoot\Esfa.Recruit.Console.RecruitSeedDataWriter.dll" $($ConnectionString) -c sequences -f "$PSScriptRoot\Sequence\Sequence_Vacancy.json"
-    & dotnet "$PSScriptRoot\Esfa.Recruit.Console.RecruitSeedDataWriter.dll" $($ConnectionString) -f "$PSScriptRoot\Data\CandidateSkills.json" -o true
-    & dotnet "$PSScriptRoot\Esfa.Recruit.Console.RecruitSeedDataWriter.dll" $($ConnectionString) -f "$PSScriptRoot\Data\QualificationTypes.json" -o true
-    & dotnet "$PSScriptRoot\Esfa.Recruit.Console.RecruitSeedDataWriter.dll" $($ConnectionString) -f "$PSScriptRoot\Data\MinimumWageRanges.json" -o true
+    & dotnet "$PSScriptRoot\Esfa.Recruit.Console.RecruitSeedDataWriter.dll" $($dbConnectionString) -c sequences -f "$PSScriptRoot\Sequence\Sequence_Vacancy.json"
+    & dotnet "$PSScriptRoot\Esfa.Recruit.Console.RecruitSeedDataWriter.dll" $($dbConnectionString) -f "$PSScriptRoot\Data\CandidateSkills.json" -o true
+    & dotnet "$PSScriptRoot\Esfa.Recruit.Console.RecruitSeedDataWriter.dll" $($dbConnectionString) -f "$PSScriptRoot\Data\QualificationTypes.json" -o true
+    & dotnet "$PSScriptRoot\Esfa.Recruit.Console.RecruitSeedDataWriter.dll" $($dbConnectionString) -f "$PSScriptRoot\Data\MinimumWageRanges.json" -o true
 }
 catch {
     throw $_
