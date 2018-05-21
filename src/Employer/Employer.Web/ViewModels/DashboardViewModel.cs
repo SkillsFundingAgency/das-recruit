21e58a89f01e62b0542a3325256c1a4c94f94f97
sachin.patel <sachin.patel@valtech.co.uk>
2018-05-21 07:55:55 +0100

    ER-412 :art: - Empty dashboard greeting.

--- src/Employer/Employer.Web/ViewModels/DashboardViewModel.cs
+++ src/Employer/Employer.Web/ViewModels/DashboardViewModel.cs
@@ -1,14 +1,16 @@
 using System.Collections.Generic;
+using System.Linq;
 using Esfa.Recruit.Vacancies.Client.Infrastructure.QueryStore.Projections.Dashboard;
 
 namespace Esfa.Recruit.Employer.Web.ViewModels
 {
     public class DashboardViewModel
     {
-        public string EmployerName { get; set; }
-
         public IList<VacancySummary> Vacancies { get; set; }
         public string WarningMessage { get; internal set; }
+
+        public bool ShowNoVacanciesMessage => Vacancies.Count == 0;
+        public bool HasVacancies => Vacancies.Any();
         public bool HasWarning => !string.IsNullOrEmpty(WarningMessage);
     }
 }
