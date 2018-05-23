cd9102c911b4a29590a16b65bb50a9fd608dc3da
sachin.patel <sachin.patel@valtech.co.uk>
2018-05-23 12:48:20 +0100

    :tractor: - Moving exception message constant that will only be used from Employer.Web into that project.

---
+++ src/Employer/Employer.Web/Exceptions/RecruitWebExceptionMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace Esfa.Recruit.Employer.Web.Exceptions
+{
+    public static class RecruitWebExceptionMessages
+    {
+        public const string RouteNotValidForVacancy = "Invalid route for vacancy. Requested route:{0} Redirecting to route:{1}";
+    }
+}
--- src/Shared/Recruit.Vacancies.Client/Application/Exceptions/ExceptionMessages.cs
+++ src/Shared/Recruit.Vacancies.Client/Application/Exceptions/ExceptionMessages.cs
@@ -1,8 +1,7 @@
 ﻿namespace Esfa.Recruit.Vacancies.Client.Application.Exceptions
 {
     public static class ExceptionMessages
-    {
-        public const string RouteNotValidForVacancy = "Invalid route for vacancy. Requested route:{0} Redirecting to route:{1}";
+    {        
         public const string VacancyUnauthorisedAccess = "The employer account {0} cannot access employer account {1} vacancy '{2} ({3})'.";
         public const string VacancyWithReferenceNotFound = "Unable to find vacancy with reference: {0}.";
         public const string VacancyWithIdNotFound = "Unable to find vacancy with id: {0}.";
