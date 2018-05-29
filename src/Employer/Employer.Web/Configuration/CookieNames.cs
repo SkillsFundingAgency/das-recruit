badd6a9efff43a79a0354f69a6041161af3f5947
sachin.patel <sachin.patel@valtech.co.uk>
2018-05-29 16:02:54 +0100

    ER-515 :hammer: Refactoring cookie name used for employer setup recording.

--- src/Employer/Employer.Web/Configuration/CookieNames.cs
+++ src/Employer/Employer.Web/Configuration/CookieNames.cs
@@ -6,5 +6,6 @@ namespace Esfa.Recruit.Employer.Web.Configuration
         public const string RecruitData = "recruit-data";
         public const string RecruitTempData = "recruit-temp-data";
         public const string AntiForgeryCookie = "recruit-x-csrf";
+        public const string SetupEmployer = "setup-employer-{0}";
     }
 }
