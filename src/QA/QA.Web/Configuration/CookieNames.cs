f4817fafc8fcbaf132a587058d5a4869c6a2d4df
sachin.patel <sachin.patel@valtech.co.uk>
2018-05-29 16:03:27 +0100

    ER-515 :hammer: Changing cookie names used for the qa website.

---
+++ src/QA/QA.Web/Configuration/CookieNames.cs
@@ -0,0 +1,9 @@
+﻿namespace Esfa.Recruit.Qa.Web.Configuration
+{
+    public static class CookieNames
+    {
+        public const string QaData = "qa-data";
+        public const string QaTempData = "qa-temp-data";
+        public const string AntiForgeryCookie = "qa-x-csrf";
+    }
+}
