d0a58286511123c182029a2122fb15d5d12b90db
sachin.patel <sachin.patel@valtech.co.uk>
2018-06-08 14:06:56 +0100

    ER-315 :zap: - Adding feature toggle tag helper.

---
+++ src/Shared/Recruit.Shared.Web/TagHelpers/EsfaFeatureTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Esfa.Recruit.Shared.TagHelpers
+{
+    [HtmlTargetElement(TagName)]
+    public class EsfaFeatureTagHelper : TagHelper
+    {
+        private const string TagName = "esfaFeature";
+        private readonly IFeature _feature;
+
+        [HtmlAttributeName("name")]
+        public string Name { get; set; }
+
+        public EsfaFeatureTagHelper(IFeature feature)
+        {
+            _feature = feature;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //output.TagName = "DIV";
+            if (!_feature.IsFeatureEnabled(Name))
+                output.SuppressOutput();
+        }
+    }
+}
