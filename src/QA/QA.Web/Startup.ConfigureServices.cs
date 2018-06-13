83825fe095217d2b01a869962c984096fd837e1b
sachin.patel <sachin.patel@valtech.co.uk>
2018-06-13 15:51:03 +0100

    ER-515 :pencil: - Renaming CSRF cookie name for the qa site

--- src/QA/QA.Web/Startup.ConfigureServices.cs
+++ src/QA/QA.Web/Startup.ConfigureServices.cs
@@ -41,7 +41,7 @@ namespace Esfa.Recruit.Qa.Web
             services.AddAntiforgery(
                 options =>
                 {
-                    options.Cookie.Name = "_af";
+                    options.Cookie.Name = "qa-x-csrf";
                     options.Cookie.HttpOnly = true;
                     options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
                     options.HeaderName = "X-XSRF-TOKEN";
