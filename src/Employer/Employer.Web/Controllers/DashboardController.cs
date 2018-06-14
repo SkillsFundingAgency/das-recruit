b63783087054a0ecc5481bd66543bc52c233cf2c
sachin.patel <sachin.patel@valtech.co.uk>
2018-06-14 07:16:01 +0100

    ER-320 :sparkles: - Adding lotto dashboard with filters to employer dashboard and including the vacancy reference number in the vacancies list.

--- src/Employer/Employer.Web/Controllers/DashboardController.cs
+++ src/Employer/Employer.Web/Controllers/DashboardController.cs
@@ -1,30 +1,45 @@
 using Esfa.Recruit.Employer.Web.Configuration;
 using Esfa.Recruit.Employer.Web.Configuration.Routing;
 using Esfa.Recruit.Employer.Web.Orchestrators;
+using Esfa.Recruit.Vacancies.Client.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using System;
 using System.Threading.Tasks;
+using Esfa.Recruit.Shared.Web.Extensions;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Esfa.Recruit.Employer.Web.Controllers
 {
     [Route(RoutePrefixPaths.AccountRoutePath)]
     public class DashboardController : Controller
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
         private readonly DashboardOrchestrator _orchestrator;
 
-        public DashboardController(DashboardOrchestrator orchestrator)
+        public DashboardController(IHostingEnvironment hostingEnvironment, DashboardOrchestrator orchestrator)
         {
+            _hostingEnvironment = hostingEnvironment;
             _orchestrator = orchestrator;
         }
 
         [HttpGet("", Name = RouteNames.Dashboard_Index_Get)]
-        public async Task<IActionResult> Dashboard([FromRoute]string employerAccountId)
+        public async Task<IActionResult> Dashboard([FromRoute]string employerAccountId, [FromQuery]string statusFilter)
         {
             var vm = await _orchestrator.GetDashboardViewModelAsync(employerAccountId);
             
             if (TempData.ContainsKey(TempDataKeys.DashboardErrorMessage))
             {
                 vm.WarningMessage = TempData[TempDataKeys.DashboardErrorMessage].ToString();
+            }
+            
+            if (!string.IsNullOrEmpty(statusFilter) && Enum.TryParse<VacancyStatus>(statusFilter, out var filter))
+            {
+                vm.Filter = filter;
+                Response.Cookies.Append(CookieNames.VacancyStatusFilter, filter.GetDisplayName(), EsfaCookieOptions.GetDefaultHttpCookieOption(_hostingEnvironment));
+            }
+            else
+            {
+                Response.Cookies.Delete(CookieNames.VacancyStatusFilter, EsfaCookieOptions.GetDefaultHttpCookieOption(_hostingEnvironment));
             }
 
             return View(vm);
