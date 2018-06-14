32e30e86fb949af6e7d0d19e1cfd6c8a3a13e69b
sachin.patel <sachin.patel@valtech.co.uk>
2018-06-14 07:14:28 +0100

    ER-320 :pencil: - Adding vacancyreference property to VacancySummary so it can be displayed on the employer dashboard.

--- src/Shared/Recruit.Vacancies.Client/Infrastructure/QueryStore/Projections/Dashboard/VacancySummary.cs
+++ src/Shared/Recruit.Vacancies.Client/Infrastructure/QueryStore/Projections/Dashboard/VacancySummary.cs
@@ -7,12 +7,14 @@ namespace Esfa.Recruit.Vacancies.Client.Infrastructure.QueryStore.Projections.Da
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
+        public long? VacancyReference { get; internal set; }
         public DateTime? CreatedDate { get; set; }
         public VacancyStatus Status { get; set; }
         public bool IsDeleted { get; set; }
         public DateTime? SubmittedDate { get; set; }
-        public long? VacancyReference {get;set;}
         public int AllApplicationsCount { get; set; }
         public int NewApplicationsCount { get; set; }
+
+        public bool HasVacancyReference => VacancyReference.HasValue;
     }
 }
