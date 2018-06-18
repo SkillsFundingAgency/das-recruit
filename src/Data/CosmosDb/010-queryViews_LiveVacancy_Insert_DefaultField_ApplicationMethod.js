e382100d12c25eb2d251c891bd07d1707e21205d
sachin.patel <sachin.patel@valtech.co.uk>
2018-06-18 14:54:51 +0100

    ER-583 :scroll: - applicationMethod field insertion with default value into vacancies and queryViews collections

---
+++ src/Data/CosmosDb/010-queryViews_LiveVacancy_Insert_DefaultField_ApplicationMethod.js
@@ -0,0 +1,25 @@
+var matchedDocs = db.queryViews.find(
+{
+    "type": "LiveVacancy",
+    "applicationMethod": { $exists: false }
+});
+
+print("Found " + matchedDocs.length() + " documents to operate on.");
+
+while (matchedDocs.hasNext()) {
+    var doc = matchedDocs.next();
+
+    var accept = db.queryViews.update(
+    {
+        "_id": doc._id,
+        "applicationMethod": { $exists: false }
+    },
+    {
+        $set: { "applicationMethod": "ThroughExternalApplicationSite" }
+    },
+    {
+        upsert: false
+    });
+
+    print("Updated document '" + doc._id + "' with applicationMethod: ThroughExternalApplicationSite");
+}
---
+++ src/Data/CosmosDb/020-vacancies_Insert_DefaultField_ApplicationMethod.js
@@ -0,0 +1,24 @@
+var matchedDocs = db.vacancies.find(
+{
+    "applicationMethod": { $exists: false }
+});
+
+print("Found " + matchedDocs.length() + " documents to operate on.");
+
+while (matchedDocs.hasNext()) {
+    var doc = matchedDocs.next();
+
+    var accept = db.vacancies.update(
+    {
+        "_id": doc._id,
+        "applicationMethod": { $exists: false }
+    },
+    {
+        $set: { "applicationMethod": "ThroughExternalApplicationSite" }
+    },
+    {
+        upsert: false
+    });
+
+    print("Updated document '" + doc._id + "' with applicationMethod: ThroughExternalApplicationSite");
+}
