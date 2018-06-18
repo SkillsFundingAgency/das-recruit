df87cb4bb625c0f1248464b8b45b8fcd0c594fa0
sachin.patel <sachin.patel@valtech.co.uk>
2018-06-18 14:54:51 +0100

    ER-583 :scroll: - applicationMethod field insertion with default value into vacancies and queryViews collections

---
+++ src/Data/CosmosDb/documentMigration.js
@@ -0,0 +1,9 @@
+
+db = db.getSiblingDB('recruit')
+
+load("010-queryViews_LiveVacancy_Insert_DefaultField_ApplicationMethod.js");
+
+
+load("020-vacancies_Insert_DefaultField_ApplicationMethod.js");
+
+
