92397ad9c4025dd85be40d74fe4b60e0a03217d7
sachin.patel <sachin.patel@valtech.co.uk>
2018-06-19 08:00:49 +0100

    :zap: :umbrella: - Adding helper method for listing non prod environments.

---
+++ src/Shared/UnitTests/Shared.Web/Configuration/EnvironmentNamesTests.cs
@@ -0,0 +1,22 @@
+﻿using Esfa.Recruit.Shared.Configuration;
+using FluentAssertions;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace Esfa.Recruit.Vacancies.Client.UnitTests.Shared.Web.Configuration
+{
+    public class EnvironmentNamesTests
+    {
+        [Fact]
+        public void GetNonProdEnvironmentNamesShouldNotIncludeProd()
+        {
+            Regex.IsMatch(EnvironmentNames.GetNonProdEnvironmentNamesCommaDelimited(), @"(?<!PRE)PROD\b").Should().BeFalse();
+        }
+
+        [Fact]
+        public void GetNonProdEnvironmentNamesShouldIncludeEveryNonProdEnvironment()
+        {
+            EnvironmentNames.GetNonProdEnvironmentNamesCommaDelimited().Should().Be("DEVELOPMENT,AT,TEST,DEMO,PREPROD");
+        }
+    }
+}
