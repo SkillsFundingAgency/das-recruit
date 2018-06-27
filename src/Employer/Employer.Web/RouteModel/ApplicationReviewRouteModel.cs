28d3354f1f03856434b4152f625344f43b75d372
Chris Jensen <chrisjensenuk@gmail.com>
2018-06-27 14:50:38 +0100

    ER-444 - Fixing up navigation

--- src/Employer/Employer.Web/RouteModel/ApplicationReviewRouteModel.cs
+++ src/Employer/Employer.Web/RouteModel/ApplicationReviewRouteModel.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Esfa.Recruit.Employer.Web.RouteModel
 {
-    public class ApplicationReviewRouteModel : RouteModel
+    public class ApplicationReviewRouteModel : VacancyRouteModel
     {
         [FromRoute]
         public Guid ApplicationReviewId { get; set; }
