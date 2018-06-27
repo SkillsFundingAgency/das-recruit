9ea9a0b05aed8ade405e2d547dee909748e94ab7
sachin.patel <sachin.patel@valtech.co.uk>
2018-06-27 09:31:17 +0100

    :green_heart: - Fixing broken build for a change that wasn't committed.

--- src/Employer/Employer.Web/Views/PartialNames.cs
+++ src/Employer/Employer.Web/Views/PartialNames.cs
@@ -6,6 +6,8 @@
         public const string GoogleAnalyticsSnippet = "_GoogleAnalyticsPartial";
         public const string NonEditableVacancyDisplay = "_NonEditableVacancyDisplayPartial";
 
+        public const string Part1Header = "_Part1HeaderPartial";
+
         public const string VacancyApplicationsTable = "_VacancyApplicationsTablePartial";
         public const string VacancySummaryDisplay = "_VacancySummaryDisplayPartial";
     }
