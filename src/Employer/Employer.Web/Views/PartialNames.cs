ada7d26afde9a720528fae1f9ddf8e74aa33fe87
sachin.patel <sachin.patel@valtech.co.uk>
2018-06-27 07:54:41 +0100

    :art: - Referencing partial views through a static named reference.

---
+++ src/Employer/Employer.Web/Views/PartialNames.cs
@@ -0,0 +1,12 @@
+﻿namespace Esfa.Recruit.Employer.Web.Views
+{
+    public static class PartialNames
+    {
+        public const string ExampleVacancy = "_ExampleVacancyPartial";
+        public const string GoogleAnalyticsSnippet = "_GoogleAnalyticsPartial";
+        public const string NonEditableVacancyDisplay = "_NonEditableVacancyDisplayPartial";
+
+        public const string VacancyApplicationsTable = "_VacancyApplicationsTablePartial";
+        public const string VacancySummaryDisplay = "_VacancySummaryDisplayPartial";
+    }
+}
