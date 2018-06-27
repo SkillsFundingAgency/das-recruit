178e333bb606b2b1db43c99d0ea9a6f640b2bd0d
sachin.patel <sachin.patel@valtech.co.uk>
2018-06-27 13:51:38 +0100

    ER-583 :hammer: :scroll: - Updating call to find number of documents to operate on. Using length() moves the cursor's current position to the end whereas count() doesn't.

--- src/Data/CosmosDb/documentMigration.js
+++ src/Data/CosmosDb/documentMigration.js
@@ -2,8 +2,4 @@
 db = db.getSiblingDB('recruit')
 
 load("010-queryViews_LiveVacancy_Insert_DefaultField_ApplicationMethod.js");
-
-
 load("020-vacancies_Insert_DefaultField_ApplicationMethod.js");
-
-
