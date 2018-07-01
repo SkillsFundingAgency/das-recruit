751ebde6f9b9ecfb5be6671b016103940385016a
sachin.patel <sachin.patel@valtech.co.uk>
2018-07-01 21:14:52 +0100

    ER-583 :muscle: - Strengthening update script for applicationMethod on LiveVacancy queryviews to run in a loop so that any missed on during update iteration are handled.

--- src/Data/CosmosDb/010-queryViews_LiveVacancy_Insert_DefaultField_ApplicationMethod.js
+++ src/Data/CosmosDb/010-queryViews_LiveVacancy_Insert_DefaultField_ApplicationMethod.js
@@ -1,25 +1,35 @@
-var matchedDocs = db.queryViews.find(
-{
+print("Start adding/updating LiveVacancy queryViews with default applicationMethod.");
+
+var query = {
     "type": "LiveVacancy",
     "applicationMethod": { $exists: false }
-});
+};
 
-print("Found " + matchedDocs.count() + " documents to operate on.");
+do {
+    var matchedDocs = db.queryViews.find(query);
 
-while (matchedDocs.hasNext()) {
-    var doc = matchedDocs.next();
+    print("Found " + matchedDocs.count() + " document(s) to operate on.");
 
-    var accept = db.queryViews.update(
-    {
-        "_id": doc._id,
-        "applicationMethod": { $exists: false }
-    },
-    {
-        $set: { "applicationMethod": "ThroughExternalApplicationSite" }
-    },
-    {
-        upsert: false
-    });
+    while (matchedDocs.hasNext()) {
+        var doc = matchedDocs.next();
 
-    print("Updated document '" + doc._id + "' with applicationMethod: ThroughExternalApplicationSite");
+        var writeResult = db.queryViews.update({
+            "_id": doc._id,
+            "applicationMethod": { $exists: false }
+        }, {
+            $set: { "applicationMethod": "ThroughExternalApplicationSite" }
+        }, {
+            upsert: false
+        });
+
+        if (writeResult.hasWriteConcernError()) {
+            printjson(writeResult.writeConcernError);
+            quit(14);
+        }
+
+        print("Updated document '" + doc._id + "' with applicationMethod: ThroughExternalApplicationSite.");
+    }
 }
+while (db.queryViews.count(query) > 0);
+
+print("Finished adding/updating LiveVacancy queryViews with default applicationMethod.");
