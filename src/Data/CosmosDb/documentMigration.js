40c961caa3d925b61b95d3c40f09b3e976e93cc7
sachin.patel <sachin.patel@valtech.co.uk>
2018-07-01 21:43:14 +0100

    ER-583 :hammer: - Handling pointing to the recruit db as target from Mongo shell if it hasn't been set correctly already.

--- src/Data/CosmosDb/documentMigration.js
+++ src/Data/CosmosDb/documentMigration.js
@@ -1,8 +1,10 @@
 
-db = db.getSiblingDB('recruit')
 // Scripts called by this script will raise Mongo exit codes as documented on the below link.
 // https://docs.mongodb.com/manual/reference/exit-codes/
 
+if (db !== "recruit") {
+    db = db.getSiblingDB("recruit");
+}
 
 load("010-queryViews_LiveVacancy_Insert_DefaultField_ApplicationMethod.js");
 load("020-vacancies_Insert_DefaultField_ApplicationMethod.js");
