50527276f2316f10447ee714568c898310d0f4ca
sachin.patel <sachin.patel@valtech.co.uk>
2018-07-01 21:38:46 +0100

    ER-583 :books: - Documenting exit codes that will be used by the change scripts.

--- src/Data/CosmosDb/documentMigration.js
+++ src/Data/CosmosDb/documentMigration.js
@@ -1,5 +1,8 @@
 
 db = db.getSiblingDB('recruit')
+// Scripts called by this script will raise Mongo exit codes as documented on the below link.
+// https://docs.mongodb.com/manual/reference/exit-codes/
+
 
 load("010-queryViews_LiveVacancy_Insert_DefaultField_ApplicationMethod.js");
 load("020-vacancies_Insert_DefaultField_ApplicationMethod.js");
