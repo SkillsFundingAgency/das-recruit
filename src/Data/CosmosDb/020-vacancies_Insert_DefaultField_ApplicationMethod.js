cca2c0be3a36927970e90101b29c9e1d8d70e6c4
sachin.patel <sachin.patel@valtech.co.uk>
2018-07-01 21:16:34 +0100

    ER-583 :muscle: - Strengthening update script for applicationMethod on vacancies collection to run in a loop so that any missed on during update iteration are handled.

--- src/Data/CosmosDb/020-vacancies_Insert_DefaultField_ApplicationMethod.js
+++ src/Data/CosmosDb/020-vacancies_Insert_DefaultField_ApplicationMethod.js
@@ -1,24 +1,33 @@
-var matchedDocs = db.vacancies.find(
-{
+print("Start adding/updating Vacancies with default applicationMethod.");
+
+var query = {
     "applicationMethod": { $exists: false }
-});
+};
 
-print("Found " + matchedDocs.count() + " documents to operate on.");
+do {
+    var matchedDocs = db.vacancies.find(query);
 
-while (matchedDocs.hasNext()) {
-    var doc = matchedDocs.next();
+    print("Found " + matchedDocs.count() + " document(s) to operate on.");
 
-    var accept = db.vacancies.update(
-    {
-        "_id": doc._id,
-        "applicationMethod": { $exists: false }
-    },
-    {
-        $set: { "applicationMethod": "ThroughExternalApplicationSite" }
-    },
-    {
-        upsert: false
-    });
+    while (matchedDocs.hasNext()) {
+        var doc = matchedDocs.next();
 
-    print("Updated document '" + doc._id + "' with applicationMethod: ThroughExternalApplicationSite");
-}
+        var writeResult = db.vacancies.update({
+            "_id": doc._id,
+            "applicationMethod": { $exists: false }
+        }, {
+            $set: { "applicationMethod": "ThroughExternalApplicationSite" }
+        }, {
+            upsert: false
+        });
+
+        if (writeResult.hasWriteConcernError()) {
+            printjson(writeResult.writeConcernError);
+            quit(14);
+        }
+
+        print("Updated document '" + doc._id + "' with applicationMethod: ThroughExternalApplicationSite.");
+    }
+} while (db.vacancies.count(query) > 0);
+
+print("Finished adding/updating Vacancies with default applicationMethod.");
