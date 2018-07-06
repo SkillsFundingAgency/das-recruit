2453578102c5b7e495a7bfd1f73cf1388cb04d7b
sachin.patel <sachin.patel@valtech.co.uk>
2018-07-06 22:03:08 +0100

    ER-286 :hammer: - Binding a default legal entity address if there is one legal entity

--- src/Employer/Employer.Web/Orchestrators/Part1/EmployerOrchestrator.cs
+++ src/Employer/Employer.Web/Orchestrators/Part1/EmployerOrchestrator.cs
@@ -41,16 +41,6 @@ namespace Esfa.Recruit.Employer.Web.Orchestrators.Part1
                 PageInfo = Utility.GetPartOnePageInfo(vacancy)
             };
 
-            if (employerData.LegalEntities.Count() == 1 && vacancy.EmployerLocation == null)
-            {
-                var defaultLegalEntity = employerData.LegalEntities.First();
-                vm.AddressLine1 = defaultLegalEntity.Address.AddressLine1;
-                vm.AddressLine2 = defaultLegalEntity.Address.AddressLine2;
-                vm.AddressLine3 = defaultLegalEntity.Address.AddressLine3;
-                vm.AddressLine4 = defaultLegalEntity.Address.AddressLine4;
-                vm.Postcode = defaultLegalEntity.Address.Postcode;
-            }
-
             if (vacancy.EmployerLocation != null)
             {
                 vm.AddressLine1 = vacancy.EmployerLocation.AddressLine1;
@@ -58,6 +48,15 @@ namespace Esfa.Recruit.Employer.Web.Orchestrators.Part1
                 vm.AddressLine3 = vacancy.EmployerLocation.AddressLine3;
                 vm.AddressLine4 = vacancy.EmployerLocation.AddressLine4;
                 vm.Postcode = vacancy.EmployerLocation.Postcode;
+            }
+            else if (employerData.LegalEntities.Count() == 1 && vacancy.EmployerLocation == null)
+            {
+                var defaultLegalEntity = employerData.LegalEntities.First();
+                vm.AddressLine1 = defaultLegalEntity.Address.AddressLine1;
+                vm.AddressLine2 = defaultLegalEntity.Address.AddressLine2;
+                vm.AddressLine3 = defaultLegalEntity.Address.AddressLine3;
+                vm.AddressLine4 = defaultLegalEntity.Address.AddressLine4;
+                vm.Postcode = defaultLegalEntity.Address.Postcode;
             }
 
             return vm;
