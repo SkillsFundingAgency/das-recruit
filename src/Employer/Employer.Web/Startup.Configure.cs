5b016074ab918d2f1f6ee6e800eb674e9b69fc73
Steve Holder <steve.holder@reavalleyit.com>
2018-07-06 12:30:31 +0100

    Initial commit

--- src/Employer/Employer.Web/Startup.Configure.cs
+++ src/Employer/Employer.Web/Startup.Configure.cs
@@ -46,7 +46,8 @@ namespace Esfa.Recruit.Employer.Web
                     s.Self()
                     .CustomSources("https://az416426.vo.msecnd.net", 
                                     "https://www.google-analytics.com/analytics.js", 
-                                    $"https://www.googletagmanager.com/")
+                                    $"https://www.googletagmanager.com/",
+                                    "http://services.postcodeanywhere.co.uk/")
                     .UnsafeInline()
                 ) // TODO: Look at moving AppInsights inline js code.
                 .FontSources(s => 
@@ -55,7 +56,8 @@ namespace Esfa.Recruit.Employer.Web
                 )
                 .ConnectSources(s => 
                     s.Self()
-                    .CustomSources("https://dc.services.visualstudio.com")
+                    .CustomSources("https://dc.services.visualstudio.com",
+                                    "https://api.postcodes.io/")
                 )
                 .ImageSources(s => 
                     s.Self()
