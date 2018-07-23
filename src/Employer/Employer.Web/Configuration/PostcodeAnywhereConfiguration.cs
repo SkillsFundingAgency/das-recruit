7e935175cfce55bcc3c061f3e428aeaf4e204042
sachin.patel <sachin.patel@valtech.co.uk>
2018-07-23 11:31:57 +0100

    ER-286 :wrench: - Adding configuration for PCA on Employer web, to be consumed by the address auto complete lookup functionality.

---
+++ src/Employer/Employer.Web/Configuration/PostcodeAnywhereConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace Esfa.Recruit.Employer.Web.Configuration
+{
+    public sealed class PostcodeAnywhereConfiguration
+    {
+        public string Key { get; set; }
+        public string FindEndpoint { get; set; }
+        public string RetrieveEndpoint { get; set; }
+    }
+}
