9a17a89e03a9ff2f22f56f9d79f2a79f4a2469e2
Chris Jensen <chrisjensenuk@gmail.com>
2018-07-24 07:44:04 +0100

    ER-586 - Explicitly deleting VacancyStatusFilter cookie on sign out

--- src/Employer/Employer.Web/Controllers/LogoutController.cs
+++ src/Employer/Employer.Web/Controllers/LogoutController.cs
@@ -5,22 +5,27 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Esfa.Recruit.Employer.Web.Controllers
 {
     [Route(RoutePrefixPaths.AccountRoutePath)]
     public class LogoutController : Controller
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
         private readonly ExternalLinksConfiguration _externalLinks;
 
-        public LogoutController(IOptions<ExternalLinksConfiguration> externalLinksOptions)
+        public LogoutController(IHostingEnvironment hostingEnvironment, IOptions<ExternalLinksConfiguration> externalLinksOptions)
         {
+            _hostingEnvironment = hostingEnvironment;
             _externalLinks = externalLinksOptions.Value;
         }
 
         [HttpGet, Route("logout", Name = RouteNames.Logout_Get)]
         public async Task<IActionResult> Logout()
         {
+            Response.Cookies.Delete(CookieNames.VacancyStatusFilter, EsfaCookieOptions.GetDefaultHttpCookieOption(_hostingEnvironment));
+
             await HttpContext.SignOutAsync("Cookies");
             await HttpContext.SignOutAsync("oidc");
 
