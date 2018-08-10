efc0bc161bf00ac9fcfdaadf5a257316ea236c1c
sachin.patel <sachin.patel@valtech.co.uk>
2018-08-10 08:23:49 +0100

    ER-593 :scroll: - Script to copy documents from queryViews collection into queryStore collection.

---
+++ src/Data/Recruit.ChangeScripts/040-queryViews_MigrateDocuments_queryStore.js
@@ -0,0 +1,27 @@
+{
+    print("Start copy of documents from queryViews collection to queryStore collection.");
+
+    let uniqueQueryViewsViewTypes = db.queryViews.distinct("viewType");
+
+    let uniqueViewTypes = db.queryStore.distinct("viewType");
+
+    uniqueViewTypes.forEach(doc => {
+        db.queryStore.deleteMany({ "viewType": doc.viewType });
+    });
+
+    let queryViewDocs = db.queryViews.find().toArray();
+
+    uniqueQueryViewsViewTypes.forEach(vt => {
+        print(`queryViews collection has ${db.queryViews.count({ "viewType": vt.viewType })} ${vt.viewType}.`);
+    });
+
+    let insertResult = db.queryStore.insertMany(queryViewDocs);
+
+    print(`Inserted ${insertResult.insertedIds.length} documents into queryStore.`)
+
+    uniqueQueryViewsViewTypes.forEach(vt => {
+        print(`queryStore collection has ${db.queryStore.count({ "viewType": vt.viewType })} ${vt.viewType}.`);
+    });
+
+    print("Finished copy of documents from queryViews collection to queryStore collection.");
+}
