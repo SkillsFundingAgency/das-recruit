890d6d322b9ae193c0d637c77dcb5f7b61c83803
Lee Wadhams <lee.wadhams@valtech.co.uk>
2018-08-21 13:20:59 +0100

    Add Event interface

--- src/Shared/Recruit.Vacancies.Client/Domain/Events/ApplicationSubmittedEvent.cs
+++ src/Shared/Recruit.Vacancies.Client/Domain/Events/ApplicationSubmittedEvent.cs
@@ -3,6 +3,7 @@ using MediatR;
 
 namespace Esfa.Recruit.Vacancies.Client.Domain.Events
 {
+    // Note: Doesn't implement IApplicationReviewEvent as it's an externally published event.
     public class ApplicationSubmittedEvent : EventBase, INotification
     {
         public Entities.Application Application { get; set; }
