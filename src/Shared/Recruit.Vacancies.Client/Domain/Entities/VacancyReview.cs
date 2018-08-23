8c8d8bc30cef14420acbabff56e971879efdebca
Deven Shah <deven.shah@valtech.co.uk>
2018-08-23 11:33:50 +0100

    ER-670 Added vacancy snapshot and few more fields to vacancy review document

--- src/Shared/Recruit.Vacancies.Client/Domain/Entities/VacancyReview.cs
+++ src/Shared/Recruit.Vacancies.Client/Domain/Entities/VacancyReview.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Esfa.Recruit.Vacancies.Client.Domain.Entities
 {
@@ -8,16 +9,42 @@ namespace Esfa.Recruit.Vacancies.Client.Domain.Entities
         public long VacancyReference { get; set; }
         public string Title { get; set; }
         public ReviewStatus Status { get; set; }
+
+        /// <summary>
+        /// Timestamp when this review record was created
+        /// </summary>
         public DateTime? CreatedDate { get; set; }
+
+        /// <summary>
+        /// Date when review was assigned 
+        /// </summary>
         public DateTime? ReviewedDate { get; set; }
+
         public VacancyUser ReviewedByUser { get; set; }
+        
+        /// <summary>
+        /// Date when the review status was set to closed
+        /// </summary>
         public DateTime? ClosedDate { get; set; }
         public ManualQaOutcome? ManualOutcome { get; set; }
+        public string ManualQaComment { get; set; }
+        public List<ManualQaFieldIndicator> ManualQaFieldIndicators { get; set; }
         public string PrivateReviewNotes { get; set; }
         public string EmployerAccountId { get; set; }
         public VacancyUser SubmittedByUser { get; set; }
         public int SubmissionCount { get; set; }
         public DateTime? SlaDeadline { get; set; }
+
+        /// <summary>
+        /// Snapshot of the vacancy at the time of creating this review record
+        /// </summary>
+        public Vacancy VacancySnapshot { get; set; }
+
+        /// <summary>
+        /// List of field identifiers, updated by the user before resubmitting the vacancy for review 
+        /// </summary>
+        public List<string> UpdatedFieldIdentifiers { get; set; }
+
 
         /// <summary>
         /// We can only approve reviews that are under review.
