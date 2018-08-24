be89c680c09fc7c32ac9cda3745499db11b48da7
Deven Shah <deven.shah@valtech.co.uk>
2018-08-24 16:15:05 +0100

    ER-635 Changed controller to carry on even if search term is empty

--- src/QA/QA.Web/Controllers/DashboardController.cs
+++ src/QA/QA.Web/Controllers/DashboardController.cs
@@ -20,22 +20,25 @@ namespace Esfa.Recruit.Qa.Web.Controllers
         [HttpGet("/", Name = RouteNames.Dashboard_Index_Get)]
         public async Task<IActionResult> Index()
         {
-            var vm = await _orchestrator.GetDashboardViewModelAsync();
+            var vm = await _orchestrator.GetDashboardViewModelAsync().ConfigureAwait(false);
             
             return View(vm);
         }
 
+        /// <summary>
+        /// Opted to use POST instead of GET as it keeps the URL clean of query string and path
+        /// </summary>
         [HttpPost("/", Name = RouteNames.Dashboard_Index_Post)]
         public async Task<IActionResult> Index(string searchTerm)
         {
             var vm = await _orchestrator.GetDashboardViewModelAsync().ConfigureAwait(false);
 
-            if (!string.IsNullOrWhiteSpace(searchTerm))
-            {
-                vm.SearchResults = await _orchestrator.GetSearchResultsAsync(searchTerm).ConfigureAwait(true);
-                vm.LastSearchTerm = searchTerm;
-                vm.IsPostBack = true;
-            }
+            //I would prefer not to go forward if search term is empty
+            //however the story says it should show no results
+            //As a safe guard, in the client, I am avoiding the DB call and returning an empty collection
+            vm.SearchResults = await _orchestrator.GetSearchResultsAsync(searchTerm).ConfigureAwait(true);
+            vm.LastSearchTerm = searchTerm;
+            vm.IsPostBack = true;
             return View(vm);
         }
     }
