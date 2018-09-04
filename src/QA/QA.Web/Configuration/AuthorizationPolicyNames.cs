d191da9f4f79039317a33c0e43ce5c0818420d91
Lee Wadhams <lee.wadhams@valtech.co.uk>
2018-09-04 11:37:58 +0100

    ER-629 Add TeamLead claim and policy

---
+++ src/QA/QA.Web/Configuration/AuthorizationPolicyNames.cs
@@ -0,0 +1,8 @@
+namespace Esfa.Recruit.Qa.Web.Configuration
+{
+    internal class AuthorizationPolicyNames
+    {
+        internal const string DoesUserBelongToGroupPolicyName = "DoesUserBelongToGroup";
+        internal const string IsUserATeamLeadPolicyName = "TeamLeadPolicy";
+    }
+}
