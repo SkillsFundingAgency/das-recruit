fe9afd90b035b4400a131d0bcd4c8abddbcb8fe3
Deven Shah <deven.shah@valtech.co.uk>
2018-09-05 13:48:17 +0100

    Implemented UserAuthorizationService to identify team lead

---
+++ src/QA/QA.Web/Security/IUserAuthorizationService.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Esfa.Recruit.Qa.Web.Security
+{
+    public interface IUserAuthorizationService
+    {
+        Task<bool> IsTeamLeadAsync(ClaimsPrincipal user);
+    }
+}
