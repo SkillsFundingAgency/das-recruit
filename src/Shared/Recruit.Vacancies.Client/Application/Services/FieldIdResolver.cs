a87bc982c61de18546a26e85df3d19bd2becab2e
Chris Jensen <chrisjensenuk@gmail.com>
2018-10-01 14:02:56 +0100

    ER-651 - Added FieldIdResolver

---
+++ src/Shared/Recruit.Vacancies.Client/Application/Services/FieldIdResolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq.Expressions;
+using Esfa.QA.Core.Extensions;
+using Esfa.Recruit.Vacancies.Client.Domain.Entities;
+
+namespace Esfa.Recruit.Vacancies.Client.Application.Services
+{
+    public static class FieldIdResolver
+    {
+        public static string ToFieldId<P>(Expression<Func<Vacancy, P>> fieldName)
+        {
+            return fieldName.GetQualifiedFieldId();
+        }
+    }
+}
