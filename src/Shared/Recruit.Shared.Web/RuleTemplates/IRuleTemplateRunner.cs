e6c540c0b6b671805fc007c4ec62a86d7f3c8c9a
Chris Jensen <chrisjensenuk@gmail.com>
2018-10-01 14:03:49 +0100

    ER-651 - Added Rule Templates to display human readable messages from rule data

---
+++ src/Shared/Recruit.Shared.Web/RuleTemplates/IRuleTemplateRunner.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Esfa.Recruit.Shared.Web.RuleTemplates
+{
+    public interface IRuleTemplateRunner
+    {
+        string ToText(object data, string fieldName);
+    }
+}
