b4b5e87148e59c7d00cdd6573b8f98248db98603
sachin.patel <sachin.patel@valtech.co.uk>
2018-10-04 13:04:33 +0100

    ER-455 :tractor: - Moving provider interface into providers folder and namespace.

--- src/Shared/Recruit.Vacancies.Client/Application/Services/TitlePopularityService.cs
+++ src/Shared/Recruit.Vacancies.Client/Application/Services/TitlePopularityService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Esfa.Recruit.Vacancies.Client.Application.Providers;
 
 namespace Esfa.Recruit.Vacancies.Client.Application.Services
 {
