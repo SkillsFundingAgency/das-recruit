58facca413d570ac0c89901beaeaea63a9170273
Chris Jensen <chrisjensenuk@gmail.com>
2018-10-08 10:15:20 +0100

    ER-728 - Handling Application Withdraw Event

---
+++ src/Jobs/Recruit.Vacancies.Jobs/DomainEvents/Handlers/Application/ApplicationWithdrawnHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Esfa.Recruit.Vacancies.Client.Domain.Events;
+using Esfa.Recruit.Vacancies.Client.Infrastructure.Client;
+using Microsoft.Extensions.Logging;
+
+namespace Esfa.Recruit.Vacancies.Jobs.DomainEvents.Handlers.Application
+{
+    public class ApplicationWithdrawnHandler : DomainEventHandler, IDomainEventHandler<ApplicationWithdrawnEvent>
+    {
+        private readonly ILogger<ApplicationWithdrawnEvent> _logger;
+        private readonly IJobsVacancyClient _client;
+
+        public ApplicationWithdrawnHandler(ILogger<ApplicationWithdrawnEvent> logger, IJobsVacancyClient client) : base(logger)
+        {
+            _logger = logger;
+            _client = client;
+        }
+
+        public async Task HandleAsync(string eventPayload)
+        {
+            var @event = DeserializeEvent<ApplicationWithdrawnEvent>(eventPayload);
+
+            try
+            {
+                _logger.LogInformation($"Processing {nameof(ApplicationWithdrawnEvent)} for vacancy: {{VacancyReference}} and candidate: {{CandidateId}}", @event.VacancyReference, @event.CandidateId);
+
+                
+
+                _logger.LogInformation($"Finished Processing {nameof(ApplicationWithdrawnEvent)} for vacancy: {{VacancyReference}} and candidate: {{CandidateId}}", @event.VacancyReference, @event.CandidateId);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unable to process {eventBody}", @event);
+                throw;
+            }
+        }
+    }
+}
