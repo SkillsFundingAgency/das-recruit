ac995fd12163b198e2688e69cb26a2accfb47338
Lee Wadhams <lee.wadhams@valtech.co.uk>
2018-10-08 09:25:54 +0100

    Add dummy text to populate view.

--- src/Employer/Employer.Web/ViewModels/CreateVacancy/CreateOptionsEditModel.cs
+++ src/Employer/Employer.Web/ViewModels/CreateVacancy/CreateOptionsEditModel.cs
@@ -2,6 +2,6 @@
 {
     public class CreateOptionsEditModel : CreateOptionsViewModel
     {
-        
+        public bool CreateNew { get; set; }
     }
 }
--- src/Employer/Employer.Web/ViewModels/CreateVacancy/CreateOptionsViewModel.cs
+++ src/Employer/Employer.Web/ViewModels/CreateVacancy/CreateOptionsViewModel.cs
@@ -9,9 +9,10 @@ namespace Esfa.Recruit.Employer.Web.ViewModels.CreateVacancy
 
         public List<ClonableVacancy> Vacancies { get; } = new List<ClonableVacancy>
         {
-            new ClonableVacancy { Id = "VAC111", Name = "VAC111" },
-            new ClonableVacancy { Id = "VAC222", Name = "VAC222" },
-            new ClonableVacancy { Id = "VAC333", Name = "VAC333" }
+            new ClonableVacancy { Id = "VAC011010590", Name = "VAC011010590", Summary = "Apprenitces Business Analyst, closing 15 Oct 2018, status: pending review, IS Business Analyst, Level: Higher (Standard)" },
+            new ClonableVacancy { Id = "VAC011010592", Name = "VAC011010592", Summary = "Apprentice Admin, closing 30 Spe 2018, status: live, Business Administrator, Level: Advanced (Standard)" },
+            new ClonableVacancy { Id = "VAC011010593", Name = "VAC011010593", Summary = "Apprentice Legal Admin, closing 15 Sep 2018, status: closed, Business and Administration: Legal Administration, Level: Intermediate (Framework)" },
+            new ClonableVacancy { Id = "VAC011010594", Name = "VAC011010594", Summary = "Apprentices Businesss Analst, closing 01 Sep 2018, status: closed, Business and Administration: Business Administration, Level: Intermediate (Framework)" }
         };
     }
 
@@ -19,5 +20,6 @@ namespace Esfa.Recruit.Employer.Web.ViewModels.CreateVacancy
     {
         public string Id { get; set; }
         public string Name { get; set; }
+        public string Summary { get; set; }
     }
 }
