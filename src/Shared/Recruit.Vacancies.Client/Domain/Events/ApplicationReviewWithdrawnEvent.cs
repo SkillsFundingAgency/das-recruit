1def89e5d956c420c5d73e8244155fff348b2882
Chris Jensen <chrisjensenuk@gmail.com>
2018-10-10 11:53:43 +0100

    ER-728 - Created ApplicationReviewWithdrawnEvent

--- src/Shared/Recruit.Vacancies.Client/Domain/Events/ApplicationReviewWithdrawnEvent.cs
+++ src/Shared/Recruit.Vacancies.Client/Domain/Events/ApplicationReviewWithdrawnEvent.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using Esfa.Recruit.Vacancies.Client.Domain.Events.Interfaces;
+﻿using Esfa.Recruit.Vacancies.Client.Domain.Events.Interfaces;
 using Esfa.Recruit.Vacancies.Client.Domain.Messaging;
 using MediatR;
 
 namespace Esfa.Recruit.Vacancies.Client.Domain.Events
 {
-    public class ApplicationReviewUpdatedEvent : EventBase, INotification, IApplicationReviewEvent
+    public class ApplicationReviewWithdrawnEvent : EventBase, INotification, IApplicationReviewEvent
     {
         public string EmployerAccountId { get; set; }
         public long VacancyReference { get; set; }
