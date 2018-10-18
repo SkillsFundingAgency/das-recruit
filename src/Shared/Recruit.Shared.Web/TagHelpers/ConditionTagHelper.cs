100406dc402ad0f51387dbb76ea295cbee2cbf72
Deven Shah <deven.shah@valtech.co.uk>
2018-10-18 10:10:07 +0100

    ER-721 Hides incomplete fields message when editing rejected vacancy (#297)
    * ER-721 Hides incomplete fields message when editing rejected vacancy

--- src/Shared/Recruit.Shared.Web/TagHelpers/ConditionTagHelper.cs
+++ src/Shared/Recruit.Shared.Web/TagHelpers/ConditionTagHelper.cs
@@ -15,4 +15,32 @@ namespace Esfa.Recruit.Shared.Web.TagHelpers
             }
         }
     }
+
+    [HtmlTargetElement(Attributes = "asp-hide")]
+    public class HideTagHelper : TagHelper
+    {
+        public bool AspHide { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (AspHide)
+            {
+                output.SuppressOutput();
+            }
+        }
+    }
+
+    [HtmlTargetElement(Attributes = "asp-show")]
+    public class ShowTagHelper : TagHelper
+    {
+        public bool AspShow { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!AspShow)
+            {
+                output.SuppressOutput();
+            }
+        }
+    }
 }
