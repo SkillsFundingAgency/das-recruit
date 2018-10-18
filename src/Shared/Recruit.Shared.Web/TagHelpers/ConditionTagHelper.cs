1c5b5fe76e65f4e1f3ca839a5867222a9a7f81ac
Deven Shah <deven.shah@valtech.co.uk>
2018-10-18 14:17:23 +0100

    Replaces tag helper asp-condition with asp-show

--- src/Shared/Recruit.Shared.Web/TagHelpers/ConditionTagHelper.cs
+++ src/Shared/Recruit.Shared.Web/TagHelpers/ConditionTagHelper.cs
@@ -1,21 +1,7 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace Esfa.Recruit.Shared.Web.TagHelpers
-{
-    [HtmlTargetElement(Attributes = "asp-condition")]
-    public class ConditionTagHelper : TagHelper
-    {
-        public bool AspCondition { get; set; }
-
-        public override void Process(TagHelperContext context, TagHelperOutput output)
-        {
-            if (!AspCondition)
-            {
-                output.SuppressOutput();
-            }
-        }
-    }
-
+{    
     [HtmlTargetElement(Attributes = "asp-hide")]
     public class HideTagHelper : TagHelper
     {
