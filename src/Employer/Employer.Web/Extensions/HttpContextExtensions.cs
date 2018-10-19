21fab31a769f652b451d8e5c330ed234bb245429
Chris Jensen <chrisjensenuk@gmail.com>
2018-10-19 11:30:14 +0100

    ER-663 - moving log out functionality into an extension for re-use

---
+++ src/Employer/Employer.Web/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Esfa.Recruit.Employer.Web.Extensions
+{
+    public static class HttpContextExtensions
+    {
+        public static async Task SignOutEmployerWebAsync(this HttpContext httpContext, bool signOutOidc = true)
+        {
+            await httpContext.SignOutAsync("Cookies");
+            await httpContext.SignOutAsync("oidc");
+        }
+    }
+}
