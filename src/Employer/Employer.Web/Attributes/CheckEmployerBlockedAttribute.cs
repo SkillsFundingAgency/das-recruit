e20b739a8a568ef6e000812753e11999ee7f4c15
Chris Jensen <chrisjensenuk@gmail.com>
2018-10-19 11:34:03 +0100

    ER-663 - Added logic to check if employer is blocked. Attribute, filter & exception handling

---
+++ src/Employer/Employer.Web/Attributes/CheckEmployerBlockedAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Esfa.Recruit.Employer.Web.Caching;
+using Esfa.Recruit.Employer.Web.Filters;
+using Esfa.Recruit.Vacancies.Client.Application.Providers;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Esfa.Recruit.Employer.Web.Attributes
+{
+    public class CheckEmployerBlockedAttribute : Attribute, IFilterFactory
+    {
+        public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
+        {
+            var blockedEmployersProvider = (IBlockedEmployersProvider)serviceProvider.GetService(typeof(IBlockedEmployersProvider));
+            var cache = (ICache)serviceProvider.GetService(typeof(ICache));
+
+            return new CheckEmployerBlockedFilter(blockedEmployersProvider, cache);
+        }
+
+        public bool IsReusable => false;
+    }
+}
