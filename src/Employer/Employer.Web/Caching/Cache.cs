9a6b3c669d64333204590f7219e9f43f4c194c39
Chris Jensen <chrisjensenuk@gmail.com>
2018-10-19 11:25:39 +0100

    ER-663 - Added in-memory cache and our own Cache wrapper

---
+++ src/Employer/Employer.Web/Caching/Cache.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Esfa.Recruit.Employer.Web.Caching
+{
+    public class Cache : ICache
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public Cache(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public Task<T> CacheAsideAsync<T>(string key, DateTime absoluteExpiration, Func<Task<T>> objectToCache)
+        {
+            return _memoryCache.GetOrCreate(key, entry =>
+            {
+                entry.AbsoluteExpiration = absoluteExpiration;
+
+                return objectToCache();
+            });
+        }
+    }
+}
