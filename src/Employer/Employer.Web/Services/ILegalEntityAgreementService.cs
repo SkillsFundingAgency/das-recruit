004c4c9e48db82e7438e9bf0bf0a4ce63bbb5bc2
Chris Jensen <chrisjensenuk@gmail.com>
2018-10-25 10:25:21 +0100

    ER-766 - Added LegalEntityAgreementService

---
+++ src/Employer/Employer.Web/Services/ILegalEntityAgreementService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Esfa.Recruit.Employer.Web.Services
+{
+    public interface ILegalEntityAgreementService
+    {
+        Task<bool> HasLegalEntityAgreementAsync(string employerAccountId, long legalEntityId);
+    }
+}
