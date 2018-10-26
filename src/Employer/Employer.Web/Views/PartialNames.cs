ea812b8d30dea5432b3ff77c32e6e88635ccbba7
sachin.patel <sachin.patel@valtech.co.uk>
2018-10-26 11:46:14 +0100

    ER-779 :chart_with_upwards_trend: - Adding GTM for Employer Recruit

--- src/Employer/Employer.Web/Views/PartialNames.cs
+++ src/Employer/Employer.Web/Views/PartialNames.cs
@@ -4,6 +4,8 @@
     {
         public const string DisabilityConfident = "_DisabilityConfidentPartial";
         public const string GoogleAnalyticsSnippet = "_GoogleAnalyticsPartial";
+        public const string GoogleTagManagerHeadPartial = "_GoogleTagManagerHeadPartial";
+        public const string GoogleTagManagerBodyPartial = "_GoogleTagManagerBodyPartial";
         public const string EditVacancyPartial = "_EditVacancyPartial";
         public const string NonEditableVacancyDisplay = "_NonEditableVacancyDisplayPartial";
         public const string VacancyApplicationsTable = "_VacancyApplicationsTablePartial";
