946021e429b874075e0faaf96fcf8710fdd6818a
sachin.patel <sachin.patel@valtech.co.uk>
2018-10-26 10:14:48 +0100

    ER-797 :hammer: - Adding a reference data class for the BlockedEmployers list.

---
+++ src/Shared/Recruit.Vacancies.Client/Infrastructure/ReferenceData/BlockedEmployers/BlockedEmployers.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using Esfa.Recruit.Vacancies.Client.Infrastructure.ReferenceData;
+
+namespace Recruit.Vacancies.Client.Infrastructure.ReferenceData.BlockedEmployers
+{
+    public class BlockedEmployers : IReferenceDataItem
+    {
+        public string Id { get; set; }
+        public DateTime LastUpdatedDate { get; set; }
+        public List<string> EmployerAccountIds { get; set; }
+    }
+}
