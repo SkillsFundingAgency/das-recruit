800808e8204675990f0cdfeb8c6356d624672500
sachin.patel <sachin.patel@valtech.co.uk>
2018-10-26 11:48:14 +0100

    ER-799 :fire: - Removing old GoogleAnalytics code.

--- src/Employer/Employer.Web/Configuration/GoogleAnalyticsConfiguration.cs
+++ src/Employer/Employer.Web/Configuration/GoogleAnalyticsConfiguration.cs
@@ -2,7 +2,6 @@
 {
     public sealed class GoogleAnalyticsConfiguration
     {
-        public string TrackingId { get; set; }
         public string TrackingManagerCode { get; set; }
     }
 }
--- src/Employer/Employer.Web/Views/PartialNames.cs
+++ src/Employer/Employer.Web/Views/PartialNames.cs
@@ -3,7 +3,6 @@
     public static class PartialNames
     {
         public const string DisabilityConfident = "_DisabilityConfidentPartial";
-        public const string GoogleAnalyticsSnippet = "_GoogleAnalyticsPartial";
         public const string GoogleTagManagerHeadPartial = "_GoogleTagManagerHeadPartial";
         public const string GoogleTagManagerBodyPartial = "_GoogleTagManagerBodyPartial";
         public const string EditVacancyPartial = "_EditVacancyPartial";
