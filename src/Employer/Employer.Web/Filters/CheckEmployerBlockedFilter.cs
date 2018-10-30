7215d9f13dff8aa385bc9f3854c45da3a947cd3e
sachin.patel <sachin.patel@valtech.co.uk>
2018-10-30 14:18:27 +0000

    ER-797 :hammer: - whitelisting around CheckBlockedEmployer filter to not apply on Error/Logout/ExternalLinks controller.

--- src/Employer/Employer.Web/Filters/CheckEmployerBlockedFilter.cs
+++ src/Employer/Employer.Web/Filters/CheckEmployerBlockedFilter.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Esfa.Recruit.Employer.Web.Caching;
 using Esfa.Recruit.Employer.Web.Configuration;
 using Esfa.Recruit.Employer.Web.Configuration.Routing;
+using Esfa.Recruit.Employer.Web.Controllers;
 using Esfa.Recruit.Employer.Web.Exceptions;
 using Esfa.Recruit.Vacancies.Client.Application.Providers;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -26,19 +28,31 @@ namespace Esfa.Recruit.Employer.Web.Filters
             ActionExecutingContext context,
             ActionExecutionDelegate next)
         {
-            var blockedEmployerAccountIds = await _cache.CacheAsideAsync(
-                CacheKeys.BlockedEmployersCacheKey,
-                CacheAbsoluteExpiryTime,
-                () => _blockedEmployersProvider.GetBlockedEmployerAccountIdsAsync());
-
-            var accountIdFromUrl = context.RouteData.Values[RouteValues.EmployerAccountId].ToString().ToUpper();
-
-            if (blockedEmployerAccountIds.Contains(accountIdFromUrl))
+            if (RequestIsForWhiteListedPage(context) == false)
             {
-                throw new BlockedEmployerException($"Employer account '{accountIdFromUrl}' is blocked");
+                var blockedEmployerAccountIds = await _cache.CacheAsideAsync(
+                    CacheKeys.BlockedEmployersCacheKey,
+                    CacheAbsoluteExpiryTime,
+                    () => _blockedEmployersProvider.GetBlockedEmployerAccountIdsAsync());
+
+                var accountIdFromUrl = context.RouteData.Values[RouteValues.EmployerAccountId].ToString().ToUpper();
+
+                if (blockedEmployerAccountIds.Contains(accountIdFromUrl))
+                {
+                    throw new BlockedEmployerException($"Employer account '{accountIdFromUrl}' is blocked");
+                }
             }
 
             await next();
+        }
+
+        private bool RequestIsForWhiteListedPage(ActionExecutingContext context)
+        {
+            var controllerName = (((Microsoft.AspNetCore.Mvc.Controllers.ControllerActionDescriptor)context.ActionDescriptor).ControllerTypeInfo).Name;
+
+            var whitelistControllers = new List<string>{ nameof(ErrorController), nameof(LogoutController), nameof(ExternalLinksController) };
+            
+            return whitelistControllers.Contains(controllerName);
         }
     }
 }
