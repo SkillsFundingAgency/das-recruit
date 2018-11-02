25a8ce8d4594e5121078841449ceb1c579f0d61e
chrisjensenuk <chrisjensenuk@gmail.com>
2018-11-02 09:36:56 +0000

    Unit tests and tidy up (#334)
    * Fixing typo. RuleTemplateMessageRunner > RuleMessageTemplateRunner
    
    * Added unit tests

---
+++ src/Employer/UnitTests/Employer.Web/Mappings/ReviewFieldIndicatorMapperTests.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Esfa.Recruit.Employer.Web.Mappings;
+using Esfa.Recruit.Shared.Web.RuleTemplates;
+using Esfa.Recruit.Vacancies.Client.Application.Rules;
+using Esfa.Recruit.Vacancies.Client.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace Esfa.Recruit.Employer.UnitTests.Employer.Web.Mappings
+{
+    public class ReviewFieldIndicatorMapperTests
+    {
+        [Fact]
+        public void ShouldMap()
+        {
+            var shortDescriptionProfanityCheckRuleOutcome =
+                new RuleOutcome(
+                    RuleId.ProfanityChecks,
+                    100,
+                    "Profanity 'drat' found in 'ShortDescription'",
+                    "ShortDescription",
+                    new List<RuleOutcome>(), "{\"Profanity\" : \"drat\",\"Occurrences\" : 1}");
+
+            var titleProfanityCheckRuleOutcome = new RuleOutcome(
+                RuleId.ProfanityChecks,
+                0,
+                "No profanities found in 'Title'",
+                "Title");
+
+            var profanityChecksRuleOutcome = new RuleOutcome(
+                RuleId.ProfanityChecks, 
+                100,
+                "No profanities found in 'Title', Profanity 'drat' found in 'ShortDescription', etc", 
+                "",
+                new List<RuleOutcome>
+                    {
+                        shortDescriptionProfanityCheckRuleOutcome,
+                        titleProfanityCheckRuleOutcome
+                    }
+                );
+
+            var sut = new ReviewFieldIndicatorMapper(new RuleMessageTemplateRunner());
+
+            var review = new VacancyReview
+            {
+                ManualQaFieldIndicators = new List<ManualQaFieldIndicator>
+                {
+                    new ManualQaFieldIndicator
+                    {
+                        IsChangeRequested = false,
+                        FieldIdentifier = VacancyReview.FieldIdentifiers.Title
+                    },
+                    new ManualQaFieldIndicator
+                    {
+                        IsChangeRequested = true,
+                        FieldIdentifier = VacancyReview.FieldIdentifiers.ShortDescription
+                    }
+                },
+                AutomatedQaOutcomeIndicators = new List<RuleOutcomeIndicator>
+                {
+                    new RuleOutcomeIndicator
+                    {
+                        IsReferred = false,
+                        RuleOutcomeId = titleProfanityCheckRuleOutcome.Id
+                    },
+                    new RuleOutcomeIndicator
+                    {
+                        IsReferred = true,
+                        RuleOutcomeId = shortDescriptionProfanityCheckRuleOutcome.Id
+                    }
+                },
+                AutomatedQaOutcome = new RuleSetOutcome {RuleOutcomes = new List<RuleOutcome> {profanityChecksRuleOutcome}}
+            };
+
+            var vm = sut.MapFromFieldIndicators(ReviewFieldMappingLookups.GetPreviewReviewFieldIndicators(), review).ToList();
+
+            vm.Count.Should().Be(1);
+
+            var shortDescription = vm.Single(v => v.ReviewFieldIdentifier == VacancyReview.FieldIdentifiers.ShortDescription);
+
+            shortDescription.ManualQaText.Should().NotBeNullOrEmpty();
+
+            shortDescription.AutoQaTexts.Count.Should().Be(1);
+            shortDescription.AutoQaTexts[0].Should().Be("Brief overview contains the phrase 'drat'");
+        }
+    }
+}
--- src/QA/QA.Web/Startup.ConfigureServices.cs
+++ src/QA/QA.Web/Startup.ConfigureServices.cs
@@ -67,7 +67,7 @@ namespace Esfa.Recruit.Qa.Web
             services.AddTransient<IGeocodeImageService>(_ => new GoogleMapsGeocodeImageService(_configuration.GetValue<string>("GoogleMapsPrivateKey")));
             services.AddScoped<ReviewMapper>();
 
-            services.AddScoped<IRuleMessageTemplateRunner, RuleTemplateMessageRunner>();
+            services.AddScoped<IRuleMessageTemplateRunner, RuleMessageTemplateRunner>();
         }
     }
 }
