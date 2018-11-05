7d11755f8da52dee3c223e88070647001e4da239
sachin.patel <sachin.patel@valtech.co.uk>
2018-11-05 07:07:56 +0000

    ER-615 :newspaper: :scroll: - Adding EmployerRecruit System configuration document to configurations collection.

--- src/Data/CosmosDb/Console.RecruitSeedDataWriter/SeedScript.ps1
+++ src/Data/CosmosDb/Console.RecruitSeedDataWriter/SeedScript.ps1
@@ -12,6 +12,7 @@ try {
     & dotnet "$PSScriptRoot/Esfa.Recruit.Console.RecruitSeedDataWriter.dll" $($dbConnectionString) -f "$PSScriptRoot/Data/Profanities.json" # Due to the sensitive content within this document, the actual profanity content will be loaded manually.
 
     & dotnet "$PSScriptRoot/Esfa.Recruit.Console.RecruitSeedDataWriter.dll" $($dbConnectionString) -c configuration -f "$PSScriptRoot/Data/QaRules.json"
+    & dotnet "$PSScriptRoot/Esfa.Recruit.Console.RecruitSeedDataWriter.dll" $($dbConnectionString) -c configuration -f "$PSScriptRoot/Data/EmployerRecruitSystem.json"
 }
 catch {
     throw $_
