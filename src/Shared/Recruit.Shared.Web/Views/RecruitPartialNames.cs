b50eba27872eac4dc4a0df7f959802d991c77d96
sachin.patel <sachin.patel@valtech.co.uk>
2019-01-03 13:38:08 +0000

    ER-780 :art: - Adding collapsable get help contact details near the footer of Employer & Provider Recruit pages. +semver: minor

---
+++ src/Shared/Recruit.Shared.Web/Views/RecruitPartialNames.cs
@@ -0,0 +1,11 @@
+namespace Esfa.Recruit.Shared.Web.Views
+{
+    public static class RecruitPartialNames
+    {
+        public const string GetHelpContactDetails = "_GetHelpContactDetailsPartial";
+        public const string GoogleTagManagerHead = "_GoogleTagManagerHeadPartial";
+        public const string GoogleTagManagerBody = "_GoogleTagManagerBodyPartial";
+
+        public const string ValidationSummary = "_ValidationSummaryPartial";
+    }
+}
