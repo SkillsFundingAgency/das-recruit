91c876432dd50d17f59db474f138eff7a309652e
sachin.patel <sachin.patel@valtech.co.uk>
2019-01-10 11:59:40 +0000

    :hammer: - Using App Insights JS partial and NWebSec the same way on QA as Employer and Provider Recruit +semver: skip

---
+++ src/QA/QA.Web/Views/PartialNames.cs
@@ -0,0 +1,9 @@
+using Esfa.Recruit.Shared.Web.Views;
+
+namespace Esfa.Recruit.Qa.Web.Views
+{
+    public static class PartialNames
+    {
+        public const string ApplicationInsights = RecruitPartialNames.ApplicationInsights;
+    }
+}
