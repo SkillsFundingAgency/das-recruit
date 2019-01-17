a0782f55089ace5355e04ce2874e46cb87b5b6a1
sachin.patel <sachin.patel@valtech.co.uk>
2019-01-17 10:55:26 +0000

    ER-920 :hammer: - Adding get provider related vacancies data access code  +semver: skip

--- src/Shared/Recruit.Vacancies.Client/Domain/Repositories/IVacancyQuery.cs
+++ src/Shared/Recruit.Vacancies.Client/Domain/Repositories/IVacancyQuery.cs
@@ -7,6 +7,7 @@ namespace Esfa.Recruit.Vacancies.Client.Domain.Repositories
     public interface IVacancyQuery
     {
         Task<IEnumerable<T>> GetVacanciesByEmployerAccountAsync<T>(string employerAccountId);
+        Task<IEnumerable<T>> GetVacanciesByProviderAccountAsync<T>(long ukprn);
         Task<Vacancy> GetSingleVacancyForPostcodeAsync(string postcode);
         Task<IEnumerable<Vacancy>> GetVacanciesByStatusAsync(VacancyStatus status);
         Task<IEnumerable<string>> GetDistinctEmployerAccountsAsync();
