c32c4d4739404116ce81156c2b8317971dca6d09
Deven Shah <deven.shah@valtech.co.uk>
2019-01-28 16:51:54 +0000

    ER-888 Removes dependency of edit model

--- src/Provider/Provider.Web/ViewModels/Part1/Employer/EmployersViewModel.cs
+++ src/Provider/Provider.Web/ViewModels/Part1/Employer/EmployersViewModel.cs
@@ -3,8 +3,9 @@ using Esfa.Recruit.Shared.Web.ViewModels;
 
 namespace Esfa.Recruit.Provider.Web.ViewModels.Part1.Employer
 {
-    public class EmployersViewModel : EmployersEditViewModel
+    public class EmployersViewModel
     {
+        public string SelectedEmployerId { get; set; }
         public IEnumerable<EmployerViewModel> Employers { get; set; }
         public PartOnePageInfoViewModel PageInfo { get; set; }
     }
