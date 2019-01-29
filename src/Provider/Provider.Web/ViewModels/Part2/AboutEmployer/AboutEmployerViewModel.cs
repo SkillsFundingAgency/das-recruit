33a5b09f8ae3a9647147a103ce1800d80b150ed9
Chris Jensen <chrisjensenuk@gmail.com>
2019-01-29 09:27:45 +0000

    ER-891 - Part 2 About Employer

---
+++ src/Provider/Provider.Web/ViewModels/Part2/AboutEmployer/AboutEmployerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Esfa.Recruit.Shared.Web.ViewModels;
+
+namespace Esfa.Recruit.Provider.Web.ViewModels.Part2.AboutEmployer
+{
+    public class AboutEmployerViewModel
+    {
+        public string Title { get; internal set; }
+        public string EmployerDescription { get; internal set; }
+        public string EmployerWebsiteUrl { get; internal set; }
+        public ReviewSummaryViewModel Review { get; set; } = new ReviewSummaryViewModel();
+
+        public IList<string> OrderedFieldNames => new List<string>
+        {
+            nameof(EmployerDescription),
+            nameof(EmployerWebsiteUrl)
+        };
+    }
+}
