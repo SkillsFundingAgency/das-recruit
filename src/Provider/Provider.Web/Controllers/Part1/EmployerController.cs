f07dc8f850563b6c5370b36b15e38ce0f31a970d
Chris Jensen <chrisjensenuk@gmail.com>
2019-02-06 14:47:58 +0000

    ER-886 - Simplifying Employer as route redirect always in wizard mode

--- src/Provider/Provider.Web/Controllers/Part1/EmployerController.cs
+++ src/Provider/Provider.Web/Controllers/Part1/EmployerController.cs
@@ -4,8 +4,6 @@ using Esfa.Recruit.Provider.Web.RouteModel;
 using Microsoft.AspNetCore.Mvc;
 using Esfa.Recruit.Provider.Web.Orchestrators.Part1;
 using Esfa.Recruit.Provider.Web.ViewModels.Part1.Employer;
-using Esfa.Recruit.Provider.Web.Extensions;
-using Esfa.Recruit.Shared.Web.Extensions;
 using Provider.Web.ViewModels;
 
 namespace Esfa.Recruit.Provider.Web.Controllers.Part1
@@ -21,16 +19,15 @@ namespace Esfa.Recruit.Provider.Web.Controllers.Part1
         }
 
         [HttpGet("employer", Name = RouteNames.Employer_Get)]
-        public async Task<IActionResult> Employer(VacancyRouteModel vacancyRouteModel, [FromQuery] string wizard = "true")
+        public async Task<IActionResult> Employer(VacancyRouteModel vacancyRouteModel)
         {
             var vm = await _orchestrator.GetEmployersViewModelAsync(vacancyRouteModel);
-            vm.PageInfo.SetWizard(wizard);
             return View(vm);
         }
 
         [HttpPost("employer", Name = RouteNames.Employer_Post)]
         public async Task<IActionResult> Employer(VacancyRouteModel vacancyRouteModel, 
-            EmployersEditModel model, [FromQuery] bool wizard)
+            EmployersEditModel model)
         {            
             if (string.IsNullOrWhiteSpace(model.SelectedEmployerId))
             {
@@ -40,14 +37,10 @@ namespace Esfa.Recruit.Provider.Web.Controllers.Part1
             if(!ModelState.IsValid)
             {
                 var vm = await _orchestrator.GetEmployersViewModelAsync(model);
-                vm.PageInfo.SetWizard(wizard);
                 return View(vm);
             }
-            
-            return
-                wizard 
-                    ? RedirectToRoute(RouteNames.CreateVacancy_Get, new {employerAccountId = model.SelectedEmployerId}) 
-                    : RedirectToRoute(RouteNames.Vacancy_Preview_Get);
+
+            return RedirectToRoute(RouteNames.CreateVacancy_Get, new {employerAccountId = model.SelectedEmployerId});
         }
     }
 }
