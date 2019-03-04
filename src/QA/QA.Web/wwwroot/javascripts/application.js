8b691b405ef6fc83f57ac46d943ff8d2784fc394
Steve Holder <steve.holder@reavalleyit.com>
2019-03-04 16:21:34 +0000

    Fixed Cookie banner bug that has been introduced since GDS update

--- src/QA/QA.Web/wwwroot/javascripts/application.js
+++ src/QA/QA.Web/wwwroot/javascripts/application.js
@@ -1,3 +1,66 @@
+/* -----------------------
+    Cookie methods
+    --------------
+    Usage:
+    Setting a cookie:
+    CookieBanner.init('hobnob', 'tasty', { days: 30 });
+    Reading a cookie:
+    CookieBanner.init('hobnob');
+    Deleting a cookie:
+    CookieBanner.init('hobnob', null);
+-------------------------- */
+var CookieBanner = {
+    init: function (name, value, options) {
+        if (typeof value !== 'undefined') {
+        if (value === false || value === null) {
+            return CookieBanner.setCookie(name, '', { days: -1 })
+        } else {
+            return CookieBanner.setCookie(name, value, options)
+        }
+        } else {
+        return CookieBanner.getCookie(name)
+        }
+    },
+    setCookie: function (name, value, options) {
+        if (typeof options === 'undefined') {
+        options = {};
+        }
+        var cookieString = name + '=' + value + '; path=/';
+        if (options.days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (options.days * 24 * 60 * 60 * 1000));
+        cookieString = cookieString + '; expires=' + date.toGMTString();
+        }
+        if (document.location.protocol === 'https:') {
+        cookieString = cookieString + '; Secure';
+        }
+        document.cookie = cookieString;
+    },
+    getCookie: function (name) {
+        var nameEQ = name + '=';
+        var cookies = document.cookie.split(';');
+        for (var i = 0, len = cookies.length; i < len; i++) {
+        var cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(nameEQ) === 0) {
+            return decodeURIComponent(cookie.substring(nameEQ.length))
+        }
+        }
+        return null
+    },
+    addCookieMessage: function () {
+        var message = document.querySelector('.js-cookie-banner');
+        var hasCookieMessage = (message && CookieBanner.init('seen_cookie_message') === null);
+
+        if (hasCookieMessage) {
+        message.style.display = 'block';
+        CookieBanner.init('seen_cookie_message', 'yes', { days: 28 });
+        }
+    }
+};
+
 /* -------------------------------------------------------------
   Character count behaviour for textareas
 ------------------------------------------------------------- */
@@ -95,3 +158,8 @@ setEditorMaxLength = function(element, tinyMceEditor) {
     var innerTextLength = innerText.charAt(innerText.length - 1) === String.fromCharCode(10) ? innerText.length - 1 : innerText.length;
     characterCount(element, innerTextLength);
 };
+
+$(function () {
+    // Add cookie message
+	CookieBanner.addCookieMessage();
+});
