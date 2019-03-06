27898abb6160f1425e13a8b3d6414026dec1fe19
sachin.patel <sachin.patel@valtech.co.uk>
2019-03-06 08:19:01 +0000

    ☂ - Resolving build warning around incorrect use of async and simplifying complex tests. +semver: skip

--- src/Provider/UnitTests/Provider.Web/UtilityTests/GetAuthorisedApplicationReviewAsyncTests.cs
+++ src/Provider/UnitTests/Provider.Web/UtilityTests/GetAuthorisedApplicationReviewAsyncTests.cs
@@ -13,54 +13,65 @@ namespace Esfa.Recruit.Provider.UnitTests.Provider.Web.UtilityTests
 {
     public class GetAuthorisedApplicationReviewAsyncTests
     {
-        [Theory]
-        [InlineData(12345678, 12345678, true)]
-        [InlineData(12345678, 123456789, false)]
-        public async Task GetAuthorisedApplicationReviewAsync_ShouldAllowForProviderUKPRN(long applicationReviewUkprn,
-            long requestedUkprn, bool shouldAllow)
-        {
-            var applicationReviewId = Guid.NewGuid();
-            var vacancyId = Guid.NewGuid(); 
-            var provider=new TrainingProvider()
-            {
-                Ukprn = applicationReviewUkprn
-            };
-            var client = new Mock<IRecruitVacancyClient>();
-            client.Setup(c => c.GetApplicationReviewAsync(applicationReviewId)).Returns(Task.FromResult(
-                new ApplicationReview
-                {
-                    Id = applicationReviewId,                    
-                    VacancyReference = 1000000001
-                }));
+        private readonly Guid _applicationReviewId;
+        private readonly Guid _vacancyId;
+        private long _applicationReviewUkprn = 12345678;
+        private readonly Mock<IRecruitVacancyClient> _mockVacancyClient;
 
-            client.Setup(c=>c.GetVacancyAsync(vacancyId)).Returns(Task.FromResult(
-                new Vacancy() {
-                    VacancyReference = 1000000001,
-                    Id = vacancyId,
-                    TrainingProvider = provider
-                }));
+        public GetAuthorisedApplicationReviewAsyncTests()
+        {
+            _applicationReviewId = Guid.NewGuid();
+            _vacancyId = Guid.NewGuid();
+
+            _mockVacancyClient = new Mock<IRecruitVacancyClient>();
+            _mockVacancyClient
+            .Setup(c => c.GetApplicationReviewAsync(_applicationReviewId))
+            .ReturnsAsync(new ApplicationReview { Id = _applicationReviewId });
+
+            _mockVacancyClient
+            .Setup(c => c.GetVacancyAsync(_vacancyId))
+            .ReturnsAsync(
+            new Vacancy()
+            {
+                Id = _vacancyId,
+                TrainingProvider = new TrainingProvider { Ukprn = _applicationReviewUkprn }
+            });
+        }
+
+        [Fact]
+        public async Task GetAuthorisedApplicationReviewAsync_ShouldAllowForProviderAssociatedUkprn()
+        {
+            const long requestedUkprn = 12345678;
 
             var rm = new ApplicationReviewRouteModel
             {
                 Ukprn = requestedUkprn,
-                ApplicationReviewId = applicationReviewId,
-                VacancyId = vacancyId
+                ApplicationReviewId = _applicationReviewId,
+                VacancyId = _vacancyId
             };
 
-            Func<Task<ApplicationReview>> act = () => Utility.GetAuthorisedApplicationReviewAsync(client.Object, rm);
+            var applicationReview = await Utility.GetAuthorisedApplicationReviewAsync(_mockVacancyClient.Object, rm);
+            applicationReview.Should().NotBeNull();
+        }
 
-            if (shouldAllow)
+        [Fact]
+        public async Task GetAuthorisedApplicationReviewAsync_ShouldNotAllowForProviderUnassociatedUkprn()
+        {
+            const long requestedUkprn = 123456789;
+
+            var rm = new ApplicationReviewRouteModel
             {
-                var applicationReview = act().Result;
-                applicationReview.Should().NotBeNull();                
-            }
-            else
-            {
-                var ex = Assert.ThrowsAsync<AuthorisationException>(act);
-                ex.Result.Message.Should().Be(
-                    $"The provider account '{requestedUkprn}' cannot access provider account '{applicationReviewUkprn}' " +
-                    $"application '{rm.ApplicationReviewId}' for vacancy '{vacancyId}'.");
-            }
+                Ukprn = requestedUkprn,
+                ApplicationReviewId = _applicationReviewId,
+                VacancyId = _vacancyId
+            };
+
+            Func<Task<ApplicationReview>> act = () => Utility.GetAuthorisedApplicationReviewAsync(_mockVacancyClient.Object, rm);
+
+            var ex = await Assert.ThrowsAsync<AuthorisationException>(act);
+            ex.Message.Should().Be(
+                $"The provider account '{requestedUkprn}' cannot access provider account '{_applicationReviewUkprn}' " +
+                $"application '{rm.ApplicationReviewId}' for vacancy '{_vacancyId}'.");
         }
     }
 }
