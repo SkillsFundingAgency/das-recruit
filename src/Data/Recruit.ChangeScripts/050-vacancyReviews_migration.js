fdca8a18ef6d55c8133f3ed19a3f414914d6c233
Deven Shah <deven.shah@valtech.co.uk>
2019-03-11 10:44:48 +0000

    Fixed pre review comments

--- src/Data/Recruit.ChangeScripts/050-vacancyReviews_migration.js
+++
@@ -1,7 +0,0 @@
-{
-    print((new Date()) + "Start migrating documents from reviews to vacancyReviews collection.");
-
-    db.reviews.find().forEach( function(x){db.vacancyReviews.insert(x);} );
-
-    print((new Date()) + "Finished migrating vacancyReviews documents.");
-}
