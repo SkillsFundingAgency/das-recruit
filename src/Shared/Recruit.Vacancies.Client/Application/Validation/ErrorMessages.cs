4abad9f92ad4884de7d60e5805af5949a1f890b7
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2019-03-13 13:08:03 +0000

    Content change for error messages.

--- src/Shared/Recruit.Vacancies.Client/Application/Validation/ErrorMessages.cs
+++ src/Shared/Recruit.Vacancies.Client/Application/Validation/ErrorMessages.cs
@@ -3,6 +3,6 @@ namespace Esfa.Recruit.Vacancies.Client.Application.Validation
 {
     public static class ErrorMessages
     {
-        public const string ChangedClosingDateMustBeNewer = "The closing date can't be changed to an earlier date than used previously.";
+        public const string ChangedClosingDateMustBeNewer = "Possible apprenticeship closing date can't be today or earlier. We advise using a date more than two weeks from now.";
     }
 }
