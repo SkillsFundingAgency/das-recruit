7f069cdf76b80ed2dd701687694ee22c0736b7db
sachin.patel <sachin.patel@valtech.co.uk>
2019-03-19 13:24:22 +0000

    ER-1204 🔨 - Changing slack notifications to mention vacancy ownerType and /closed vacancy slack message to output a hyperlink to the vacancy on FAA +semver: patch

---
+++ src/Shared/UnitTests/Vacancies.Client/Infrastructure/Slack/SlackNotifyVacancyUpdatesTests.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Esfa.Recruit.Vacancies.Client.Domain.Entities;
+using Esfa.Recruit.Vacancies.Client.Infrastructure.Slack;
+using Microsoft.Extensions.Options;
+using Moq;
+using Xunit;
+
+namespace Esfa.Recruit.UnitTests.Vacancies.Client.Infrastructure.Slack
+{
+    public class SlackNotifyVacancyUpdatesTests
+    {
+        [Fact]
+        public async Task WhenVacancyClosedByUser_ThenASlackMessageShouldBePostedWithTheNewIconAndSuitableMessage()
+        {
+            var vacancyReference = 1000000001;
+            var vacancy = new Vacancy{VacancyReference = vacancyReference, OwnerType = OwnerType.Employer, ClosedByUser = new VacancyUser()};
+            var mockSlackClient = new Mock<ISlackClient>();
+
+            mockSlackClient
+                .Setup(c => c.PostAsync(
+                    It.Is<SlackMessage>(s => 
+                        s.Text.Contains("has been closed") && 
+                        s.Text.Contains(" (Employer)") && 
+                        s.Text.Contains(vacancyReference.ToString())),
+                    It.Is<SlackVacancyNotificationType>(e => e.Equals(SlackVacancyNotificationType.ManuallyClosed))))
+                    .Returns(Task.CompletedTask)
+                    .Verifiable();
+
+            var client = new SlackNotifyVacancyUpdates(mockSlackClient.Object, Options.Create(new SlackConfiguration()));
+
+            await client.VacancyManuallyClosed(vacancy);
+
+            mockSlackClient.Verify();
+        }
+    }
+}
