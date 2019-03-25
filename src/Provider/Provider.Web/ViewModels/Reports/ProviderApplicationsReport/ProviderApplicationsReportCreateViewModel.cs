690852859c1aebca867e7c1fb9a01542fd957883
Chris Jensen <chrisjensenuk@gmail.com>
2019-03-25 10:56:23 +0000

    ER-470 - Addressing PR comments

--- src/Provider/Provider.Web/ViewModels/Reports/ProviderApplicationsReport/ProviderApplicationsReportCreateViewModel.cs
+++ src/Provider/Provider.Web/ViewModels/Reports/ProviderApplicationsReport/ProviderApplicationsReportCreateViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Esfa.Recruit.Provider.Web.ViewModels.Reports.ProviderApplicationsReport
 {
