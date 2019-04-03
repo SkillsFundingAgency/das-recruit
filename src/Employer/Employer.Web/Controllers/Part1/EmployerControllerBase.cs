1fb8db7bca067d76c4ae24c6baabbb5c928980c1
Deven Shah <deven.shah@valtech.co.uk>
2019-04-03 16:11:44 +0100

    ER-1002 fixing redirections

--- src/Employer/Employer.Web/Controllers/Part1/EmployerControllerBase.cs
+++ src/Employer/Employer.Web/Controllers/Part1/EmployerControllerBase.cs
@@ -44,8 +44,8 @@ namespace Esfa.Recruit.Employer.Web.Controllers.Part1
         {
             DeleteVacancyEmployerInfoCookie();
             return wizard 
-                ? RedirectToRoute(RouteNames.Vacancy_Preview_Get, Anchors.AboutEmployerSection) 
-                : RedirectToRoute(RouteNames.Dashboard_Index_Get);
+                ? RedirectToRoute(RouteNames.Dashboard_Index_Get) 
+                : RedirectToRoute(RouteNames.Vacancy_Preview_Get, Anchors.AboutEmployerSection);
         }
     }    
 }
