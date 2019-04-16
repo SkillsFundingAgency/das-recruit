fbb6351a1084bbb0119b99bf7f919b697103bbd7
Chris Jensen <chrisjensenuk@gmail.com>
2019-04-16 13:19:01 +0100

    ER-1192 - Missing service claim redirects to provider portal

--- src/Provider/Provider.Web/Configuration/ProviderRecruitClaims.cs
+++ src/Provider/Provider.Web/Configuration/ProviderRecruitClaims.cs
@@ -8,5 +8,7 @@ namespace Esfa.Recruit.Provider.Web.Configuration
         public static string IdamsUserServiceTypeClaimTypeIdentifier = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/service";
         public static string IdamsUserUkprnClaimsTypeIdentifier = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/ukprn";
         public static string IdamsUserEmailClaimTypeIdentifier = "http://schemas.portal.com/mail";
+
+        public static string ServiceClaimValue = "DAA";
     }
 }
