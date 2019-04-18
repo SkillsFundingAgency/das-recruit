9c3ab1554526513a298a17ebd9870b6c5602e3e1
sachin.patel <sachin.patel@valtech.co.uk>
2019-04-18 13:47:32 +0100

    ER-1081 🔥 - Removing the permission check/unit test around clone vacancy action on Provider Recruit +semver: skip

--- src/Provider/Provider.Web/Exceptions/MissingPermissionsException.cs
+++ src/Provider/Provider.Web/Exceptions/MissingPermissionsException.cs
@@ -1,16 +1,11 @@
-using Esfa.Recruit.Provider.Web.Model;
-using Esfa.Recruit.Shared.Web.Models;
 using Esfa.Recruit.Vacancies.Client.Domain.Exceptions;
 
 namespace Esfa.Recruit.Provider.Web.Exceptions
 {
     public class MissingPermissionsException : RecruitException
     {
-        public RecruitVacancyAction VacancyAction { get; private set; }
-
-        public MissingPermissionsException(string message, RecruitVacancyAction va) : base(message)
+        public MissingPermissionsException(string message) : base(message)
         {
-            VacancyAction = va;
         }
     }
 }
--- src/Provider/Provider.Web/ViewModels/MissingPermissionsViewModel.cs
+++ src/Provider/Provider.Web/ViewModels/MissingPermissionsViewModel.cs
@@ -1,10 +1,9 @@
-using Esfa.Recruit.Provider.Web.Model;
 using Esfa.Recruit.Provider.Web.RouteModel;
 using Esfa.Recruit.Shared.Web.Models;
 
 namespace Esfa.Recruit.Provider.Web.ViewModels
 {
-    public class MissingPermissionsViewModel
+	public class MissingPermissionsViewModel
     {
         public VacancyRouteModel RouteValues { get; internal set; }
         public VacancyActionType ActionType { get; internal set; }
