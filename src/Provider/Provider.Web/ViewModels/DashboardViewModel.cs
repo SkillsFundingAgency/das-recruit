191f5813868246379ad11fea33ceba2737c8c965
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2019-05-03 14:28:55 +0100

    Minor code fixes.

--- src/Provider/Provider.Web/ViewModels/DashboardViewModel.cs
+++ src/Provider/Provider.Web/ViewModels/DashboardViewModel.cs
@@ -29,6 +29,8 @@ namespace Esfa.Recruit.Provider.Web.ViewModels
         public bool HasSubmittedVacancy => VacancyCountSubmitted > 0;
         public int NoOfNewApplications => Vacancies.Count(v => v.NoOfNewApplications > 0);
         public bool HasNewApplications => NoOfNewApplications > 0;
+        public int AllApplications => Vacancies.Count(v => v.NoOfSuccessfulApplications > 0);
+        public bool ShowAllApplications => AllApplications > 0;
         public string ApplicationTextLive => "application".ToQuantity(NoOfNewApplications, ShowQuantityAs.None);
         public int NoOfVacanciesClosingSoon { get; set; }
         public string VacancyTextClosingSoon => "vacancy".ToQuantity(NoOfVacanciesClosingSoon, ShowQuantityAs.None);
