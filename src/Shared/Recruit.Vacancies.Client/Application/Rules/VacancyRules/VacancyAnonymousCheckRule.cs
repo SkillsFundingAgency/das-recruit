4425b573a79ca0bdb7e894f7029fa7b189f389e9
chrisjensenuk <chrisjensenuk@gmail.com>
2019-05-20 08:04:28 +0100

    ER-1019 - Added VacancyAnonymousCheckRule auto QA rule (#602) +semver: minor

---
+++ src/Shared/Recruit.Vacancies.Client/Application/Rules/VacancyRules/VacancyAnonymousCheckRule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Esfa.Recruit.Vacancies.Client.Application.Rules.Engine;
+using Esfa.Recruit.Vacancies.Client.Domain.Entities;
+
+namespace Esfa.Recruit.Vacancies.Client.Application.Rules.VacancyRules
+{
+    public sealed class VacancyAnonymousCheckRule : Rule, IRule<Vacancy>
+    {
+        public VacancyAnonymousCheckRule() : base(RuleId.VacancyAnonymous)
+        {
+            
+        }
+
+        public Task<RuleOutcome> EvaluateAsync(Vacancy subject)
+        {
+            var outcomeBuilder = RuleOutcomeDetailsBuilder.Create(RuleId);
+
+            var score = subject.IsAnonymous ? 100 : 0;
+
+            var outcomeResult = new RuleOutcome(
+                RuleId, 
+                score, 
+                "Anonymous employer",
+                nameof(Vacancy.EmployerName));
+
+            var outcome = outcomeBuilder.Add(new [] { outcomeResult })
+                .ComputeSum();
+
+            return Task.FromResult(outcome);
+        }
+    }
+}
---
+++ src/Shared/UnitTests/Vacancies.Client/Application/Rules/VacancyRules/VacancyAnonymousCheckRuleTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Esfa.Recruit.UnitTests.Vacancies.Client.Application.Rules.VacancyRules;
+using Esfa.Recruit.Vacancies.Client.Application.Rules;
+using Esfa.Recruit.Vacancies.Client.Application.Rules.VacancyRules;
+using Esfa.Recruit.Vacancies.Client.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace Esfa.Recruit.Vacancies.Client.UnitTests.Vacancies.Client.Application.Rules.VacancyRules
+{
+    public class VacancyAnonymousCheckRuleTests
+    {
+        [Fact]
+        public async Task WhenInvoked_ForNonAnonymousEmployer()
+        {
+            var rule = new VacancyAnonymousCheckRule();
+
+            var vacancy = new Vacancy 
+            {
+                EmployerNameOption = EmployerNameOption.RegisteredName
+            };
+
+            var outcome = await rule.EvaluateAsync(vacancy);
+
+            outcome.RuleId.Should().Be(RuleId.VacancyAnonymous);
+            outcome.Score.Should().Be(0);
+        }
+
+        [Fact]
+        public async Task WhenInvoked_ForAnonymousEmployer()
+        {
+            var rule = new VacancyAnonymousCheckRule();
+
+            var vacancy = new Vacancy {
+                EmployerNameOption = EmployerNameOption.Anonymous
+            };
+
+            var outcome = await rule.EvaluateAsync(vacancy);
+
+            outcome.RuleId.Should().Be(RuleId.VacancyAnonymous);
+            outcome.Score.Should().Be(100);
+        }
+    }
+}
