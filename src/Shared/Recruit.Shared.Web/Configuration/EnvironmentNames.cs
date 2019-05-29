36194c1fc18e26ea19b7ca0c6bbbc4437c11bf84
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2019-05-29 08:53:58 +0100

    PR review comments.

--- src/Shared/Recruit.Shared.Web/Configuration/EnvironmentNames.cs
+++ src/Shared/Recruit.Shared.Web/Configuration/EnvironmentNames.cs
@@ -8,13 +8,14 @@ namespace Esfa.Recruit.Shared.Web.Configuration
         public const string Development = "DEVELOPMENT";
         public const string AT = "AT";
         public const string TEST = "TEST";
+        public const string TEST2 = "TEST2";
         public const string DEMO = "DEMO";
         public const string PREPROD = "PREPROD";
         public const string PROD = "PROD";
 
         public static string[] GetTestEnvironmentNames() => new []{ Development, AT, TEST, DEMO };
         public static string GetTestEnvironmentNamesCommaDelimited() => string.Join(",", Development, AT, TEST, DEMO);
-        public static string GetEnvironmentNamesCommaDelimitedForGMT() => string.Join(",", DEMO, PREPROD, PROD);
+        public static string GetEnvironmentNamesCommaDelimitedForGTM() => string.Join(",", DEMO, PREPROD, PROD);
         public static string GetNonProdEnvironmentNamesCommaDelimited() => string.Join(",", Development, AT, TEST, DEMO, PREPROD);
         public static bool IsProductionEnvironment(IHostingEnvironment hostingEnvironment)
         {
