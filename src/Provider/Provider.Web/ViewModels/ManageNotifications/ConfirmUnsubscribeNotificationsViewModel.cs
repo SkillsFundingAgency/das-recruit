0fe9d47961350b04472d35f35e581597cab85231
Deven Shah <deven.shah@valtech.co.uk>
2019-06-03 15:46:41 +0100

    ER-1236 Implements manage notification view in providers (#613) +semver: minor

---
+++ src/Provider/Provider.Web/ViewModels/ManageNotifications/ConfirmUnsubscribeNotificationsViewModel.cs
@@ -0,0 +1,7 @@
+namespace Esfa.Recruit.Provider.Web.ViewModels.ManageNotifications
+{
+    public class ConfirmUnsubscribeNotificationsViewModel
+    {
+        public bool? ConfirmUnsubscribe { get; set; }
+    }
+}
