7c712f19c97144f5945d201bcd9b478a7a00fed7
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2019-06-10 15:55:45 +0100

    Using existing routes.

--- src/Employer/Employer.Web/Configuration/Routing/RoutePaths.cs
+++ src/Employer/Employer.Web/Configuration/Routing/RoutePaths.cs
@@ -5,7 +5,6 @@
         public const string Services = "services";
         public const string AccountLevelServices = "services/{employerAccountId:minlength(6)}";
         public const string AccountRoutePath = "accounts/{employerAccountId:minlength(6)}";
-        public const string EmployerAccountRoutePath = AccountRoutePath + "/" + "employer";
 
         public const string VacancyRoutePath = "vacancies/{vacancyId:guid}";
         public const string AccountVacancyRoutePath = AccountRoutePath + "/" + VacancyRoutePath;
