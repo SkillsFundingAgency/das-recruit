f704d15db875e52b4cbaf759475105fa3985ef14
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2019-06-10 15:55:45 +0100

    Using existing routes.

--- src/Employer/Employer.Web/Controllers/EmployerVacancyController.cs
+++ src/Employer/Employer.Web/Controllers/EmployerVacancyController.cs
@@ -1,19 +1,18 @@
-using Esfa.Recruit.Employer.Web.Configuration;
 using Esfa.Recruit.Employer.Web.Configuration.Routing;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Esfa.Recruit.Employer.Web.Controllers.Part1
+namespace Esfa.Recruit.Employer.Web.Controllers
 {
-    [Route(RoutePaths.EmployerAccountRoutePath)]
+    [Route(RoutePaths.AccountRoutePath)]
     public class EmployerVacancyController : Controller
     {        
-        [HttpGet("create-vacancy", Name = RouteNames.EmployerCreateVacancy_Get)]        
+        [HttpGet("employer-create-vacancy", Name = RouteNames.EmployerCreateVacancy_Get)]        
         public IActionResult CreateVacancy()
         {
             return RedirectToRoute(RouteNames.CreateVacancyOptions_Get);
         }
 
-        [HttpGet("manage-vacancy", Name = RouteNames.EmployerManageVacancy_Get)]
+        [HttpGet("employer-manage-vacancy", Name = RouteNames.EmployerManageVacancy_Get)]
         public IActionResult ManageVacancy()
         {
             return RedirectToRoute(RouteNames.Dashboard_Index_Get);
