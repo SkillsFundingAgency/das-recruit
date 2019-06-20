097ca982e5dd4d780927a2f36033a79eae9f6ceb
Deven Shah <deven.shah@valtech.co.uk>
2019-06-20 08:44:02 +0100

    Updated CommunicationRequest

--- src/Communication/Communication.Types/CommunicationRequest.cs
+++ src/Communication/Communication.Types/CommunicationRequest.cs
@@ -8,22 +8,22 @@ namespace Communication.Types
         public Guid RequestId { get; }
         public string RequestType { get; }
         public DateTime RequestDateTime { get; }
-        public string RecipientsResolver { get; }
+        public string ParticipantsResolverName { get; }
         public string OriginatingService { get; }
         public List<Entity> Entities { get; }
-        public CommunicationRequest(string requestType, string recipientsResolver, string originatingService)
+        public CommunicationRequest(string requestType, string participantsResolverName, string originatingService)
         {
             RequestId = Guid.NewGuid();
             RequestDateTime = DateTime.UtcNow;
 
             RequestType = requestType;
-            RecipientsResolver = recipientsResolver;
+            ParticipantsResolverName = participantsResolverName;
             OriginatingService = originatingService;
 
             Entities = new List<Entity>();
         }
 
-        public void AddEntity<T>(string entityType, T entityId)
+        public void AddEntity(string entityType, object entityId)
         {
             Entities.Add(new Entity(entityType, entityId));
         }
