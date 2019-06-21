b27c4264610453527f7ad272b756f22a708cb9f2
Deven Shah <deven.shah@valtech.co.uk>
2019-06-21 17:10:10 +0100

    Provider identifier properties given more meaningful names

--- src/Communication/Communication.Types/Interfaces/IEntityDataItemProvider.cs
+++ src/Communication/Communication.Types/Interfaces/IEntityDataItemProvider.cs
@@ -5,7 +5,10 @@ namespace Communication.Types.Interfaces
 {
     public interface IEntityDataItemProvider
     {
-        string ProviderName { get; }
+        /// EntityType will allow resolving data items required for the message template
+        /// This should be a unique value across the services
+        /// Example value: VacancyServices.Recruit.Vacancy 
+        string EntityType { get; }
         Task<IEnumerable<CommunicationDataItem>> GetDataItems(object entityId);
     }
 }
--- src/Communication/Communication.Types/Interfaces/ITemplateIdProvider.cs
+++ src/Communication/Communication.Types/Interfaces/ITemplateIdProvider.cs
@@ -4,7 +4,10 @@ namespace Communication.Types.Interfaces
 {
     public interface ITemplateIdProvider
     {
-        string ProviderName { get; }
+        /// ServiceName will allow communication processor to map a request to its template provider
+        /// This has to be unique value across all the possible services
+        /// Example value: VacancyServices.Recruit.Employer
+        string ServiceName { get; }
         Task<string> GetTemplateId(CommunicationMessage message);
     }
 }
