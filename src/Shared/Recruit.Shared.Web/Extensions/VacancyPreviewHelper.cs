b8dd3521dbce1ec9aafd57152943eb992f2c1fe8
Shoma <shoma.gujjar@valtech.co.uk>
2019-06-24 10:06:57 +0100

    ER-1307 improve summary block (#642)
    ER-1304-New screen.

---
+++ src/Shared/Recruit.Shared.Web/Extensions/VacancyPreviewHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace Esfa.Recruit.Shared.Web.Extensions
+{
+    public static class VacancyPreviewHelper
+    {
+        public static IHtmlContent ActionLink(IUrlHelper urlHelper, string linkText, string routeName, object routeValues, string screenReaderText, string linkClass, string dataAutomationId)
+        {
+            /*
+             *<a href="duration?edit=yes" class="govuk-link das-edit-link">Change
+             *  <span class="govuk-visually-hidden"> working week</span>
+             * </a>
+             */
+            var visuallyHiddenSpan = new TagBuilder("span");
+            visuallyHiddenSpan.AddCssClass("govuk-visually-hidden");
+            visuallyHiddenSpan.InnerHtml.Append(screenReaderText);
+            var tagBuilderAtag = new TagBuilder("a");
+            tagBuilderAtag.AddCssClass(linkClass);
+            tagBuilderAtag.InnerHtml.Append(linkText);
+            tagBuilderAtag.InnerHtml.AppendHtml(visuallyHiddenSpan);
+            tagBuilderAtag.MergeAttribute("href", urlHelper.RouteUrl(routeName, routeValues));
+            tagBuilderAtag.MergeAttribute("data-automation", dataAutomationId);
+            return tagBuilderAtag;
+        }
+    }
+}
