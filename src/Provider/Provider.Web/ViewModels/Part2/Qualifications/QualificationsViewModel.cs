db0a1fd4a102f3e6b68d6e44e9f202518f2cd20e
chrisjensenuk <chrisjensenuk@gmail.com>
2019-06-25 13:39:36 +0100

    ER-1312 - Provider Qualifications (#639)

--- src/Provider/Provider.Web/ViewModels/Part2/Qualifications/QualificationsViewModel.cs
+++ src/Provider/Provider.Web/ViewModels/Part2/Qualifications/QualificationsViewModel.cs
@@ -1,34 +1,17 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Esfa.Recruit.Shared.Web.ViewModels;
 using Esfa.Recruit.Shared.Web.ViewModels.Qualifications;
-using Esfa.Recruit.Vacancies.Client.Domain.Entities;
 
 namespace Esfa.Recruit.Provider.Web.ViewModels.Part2.Qualifications
 {
     public class QualificationsViewModel
     {
+        public string Title { get; set; }
         public List<QualificationEditModel> Qualifications { get; set; }
-
-        public string Title { get; internal set; }
-
-        public IList<string> QualificationTypes { get; set; }
-
-        public bool HasQualifications => Qualifications.Any();
+        public string InfoMessage { get; set; }
 
         public ReviewSummaryViewModel Review { get; set; } = new ReviewSummaryViewModel();
 
-        public string QualificationType { get; set; }
-        public string Subject { get; set; }
-        public string Grade { get; set; }
-        public QualificationWeighting? Weighting { get; set; }
-
-        public IList<string> OrderedFieldNames => new List<string>
-        {
-            nameof(QualificationType),
-            nameof(Subject),
-            nameof(Grade),
-            nameof(Weighting)
-        };
+        public bool HasInfo => string.IsNullOrWhiteSpace(InfoMessage) == false;
     }
 }
