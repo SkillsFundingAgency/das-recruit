e655bd476bd99e248716b1f74b6ae77b08752425
chrisjensenuk <chrisjensenuk@gmail.com>
2019-06-28 08:26:04 +0100

    ER-1187 - UTC to GMT Standard conversion (#621)

--- src/Provider/UnitTests/Provider.Web/Orchestrators/Reports/ProviderApplicationsReportOrchestratorTests.cs
+++ src/Provider/UnitTests/Provider.Web/Orchestrators/Reports/ProviderApplicationsReportOrchestratorTests.cs
@@ -14,19 +14,25 @@ namespace Esfa.Recruit.Provider.UnitTests.Provider.Web.Orchestrators.Reports
 {
     public class ProviderApplicationsReportOrchestratorTests
     {
-        private readonly Mock<IProviderVacancyClient> _client = new Mock<IProviderVacancyClient>();
+        
 
         [Theory]
-        [InlineData(DateRangeType.Last7Days, "2019-02-26", "2019-03-05")]
-        [InlineData(DateRangeType.Last14Days, "2019-02-19", "2019-03-05")]
-        [InlineData(DateRangeType.Last30Days, "2019-02-03", "2019-03-05")]
+        [InlineData(DateRangeType.Last7Days, "2019-02-26Z", "2019-03-05Z")] //GMT
+        [InlineData(DateRangeType.Last14Days, "2019-02-19Z", "2019-03-05Z")] //GMT
+        [InlineData(DateRangeType.Last30Days, "2019-02-03Z", "2019-03-05Z")] //GMT
+        [InlineData(DateRangeType.Last7Days, "2019-04-01Z", "2019-04-08Z")] //BST
+        [InlineData(DateRangeType.Last14Days, "2019-04-01Z", "2019-04-15Z")] //BST
+        [InlineData(DateRangeType.Last30Days, "2019-04-01Z", "2019-05-01Z")] //BST
         public async Task PostCreateViewModelAsync_ShouldUseCorrectTimespan(DateRangeType dateRangeType, string fromDate, string toDate)
         {
-            var orchestrator = GetOrchestrator();
+            var mockClient = new Mock<IProviderVacancyClient>();
+            var orchestrator = GetOrchestrator(mockClient.Object, $"{toDate}");
+
+            var fromDateUtc = DateTimeOffset.Parse(fromDate).UtcDateTime;
+            var toDateUtc = DateTimeOffset.Parse(toDate).UtcDateTime;
 
             long ukprn = 12345678;
-            string reportName = $"{DateTime.Parse(fromDate).AsGdsDate()} to {DateTime.Parse(toDate).AsGdsDate()}";
-
+            
             var model = new ProviderApplicationsReportCreateEditModel {
                 Ukprn = ukprn,
                 DateRange = dateRangeType
@@ -36,55 +42,66 @@ namespace Esfa.Recruit.Provider.UnitTests.Provider.Web.Orchestrators.Reports
 
             await orchestrator.PostCreateViewModelAsync(model, user);
 
-            _client.Verify(c => c.CreateProviderApplicationsReportAsync(
-                ukprn, 
-                DateTime.Parse(fromDate), 
-                DateTime.Parse(toDate).AddDays(1).AddTicks(-1), 
-                user, 
-                reportName), 
+            string expectedReportName = $"{fromDateUtc.AsGdsDate()} to {toDateUtc.AsGdsDate()}";
+
+            mockClient.Verify(c => c.CreateProviderApplicationsReportAsync(
+                ukprn,
+                fromDateUtc,
+                toDateUtc.AddDays(1).AddTicks(-1), 
+                user,
+                expectedReportName), 
                 Times.Once);
         }
 
-        [Fact]
-        public async Task PostCreateViewModelAsync_ShouldUseCustomTimespan()
+        [Theory]
+        [InlineData("1", "2", "2018", "20", "2", "2019")] //GMT
+        [InlineData("1", "4", "2018", "20", "4", "2019")] //BST
+        public async Task PostCreateViewModelAsync_ShouldUseCustomTimespan(string fromDay, string fromMonth, string fromYear, string toDay, string toMonth, string toYear)
         {
+            
             long ukprn = 12345678;
 
             var model = new ProviderApplicationsReportCreateEditModel {
                 Ukprn = ukprn,
                 DateRange = DateRangeType.Custom,
-                FromDay = "1",
-                FromMonth = "2",
-                FromYear = "2018",
-                ToDay = "3",
-                ToMonth = "4",
-                ToYear = "2019",
+                FromDay = fromDay,
+                FromMonth = fromMonth,
+                FromYear = fromYear,
+                ToDay = toDay,
+                ToMonth = toMonth,
+                ToYear = toYear,
             };
 
             var user = new VacancyUser();
 
-            var orchestrator = GetOrchestrator();
+            var mockClient = new Mock<IProviderVacancyClient>();
+            var orchestrator = GetOrchestrator(mockClient.Object, "2019-12-05Z");
+
+            var fromDateUtc = DateTimeOffset.Parse($"{fromYear}-{fromMonth}-{fromDay}Z").UtcDateTime;
+            var toDateUtc = DateTimeOffset.Parse($"{toYear}-{toMonth}-{toDay}Z").UtcDateTime;
 
             await orchestrator.PostCreateViewModelAsync(model, user);
 
-            _client.Verify(c => c.CreateProviderApplicationsReportAsync(
+            string expectedReportName = $"{fromDateUtc.AsGdsDate()} to {toDateUtc.AsGdsDate()}";
+
+            mockClient.Verify(c => c.CreateProviderApplicationsReportAsync(
                     ukprn,
-                    DateTime.Parse("2018-02-01").ToUniversalTime(),
-                    DateTime.Parse("2019-04-03").AddDays(1).AddTicks(-1).ToUniversalTime(), 
+                    fromDateUtc,
+                    toDateUtc.AddDays(1).AddTicks(-1),
                     user,
-                    "01 Feb 2018 to 03 Apr 2019"),
+                    expectedReportName),
                 Times.Once);
         }
 
-        private ProviderApplicationsReportOrchestrator GetOrchestrator()
+        private ProviderApplicationsReportOrchestrator GetOrchestrator(IProviderVacancyClient client, string todayDate)
         {
             var timeProvider = new Mock<ITimeProvider>();
 
-            var today = DateTime.Parse("2019-03-05").ToUniversalTime();
+            var today = DateTimeOffset.Parse(todayDate).UtcDateTime;
             timeProvider.Setup(t => t.Today).Returns(today);
             timeProvider.Setup(t => t.NextDay).Returns(today.AddDays(1));
 
-            return new ProviderApplicationsReportOrchestrator(_client.Object, timeProvider.Object);
+            return new ProviderApplicationsReportOrchestrator(client, timeProvider.Object);
         }
     }
 }
