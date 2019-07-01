bd856aad497f8d4e0f53d846435cf9d4e2a5c7ff
Chris Jensen <chrisjensenuk@gmail.com>
2019-07-01 09:33:58 +0100

    ER-1339 - Employer duration

--- src/Employer/Employer.Web/ViewModels/Part1/Wage/WageViewModel.cs
+++ src/Employer/Employer.Web/ViewModels/Part1/Wage/WageViewModel.cs
@@ -1,11 +1,34 @@
 ﻿using System.Collections.Generic;
 using Esfa.Recruit.Shared.Web.ViewModels;
+using Esfa.Recruit.Shared.Web.ViewModels.Validations;
+using Esfa.Recruit.Vacancies.Client.Domain.Entities;
+using ErrMsg = Esfa.Recruit.Shared.Web.ViewModels.ValidationMessages.WageValidationMessages;
 
 namespace Esfa.Recruit.Employer.Web.ViewModels.Part1.Wage
 {
-    public class WageViewModel : WageEditModel
+    public class WageViewModel
     {
+        [TypeOfInteger(ErrorMessage = ErrMsg.TypeOfInteger.Duration)]
+        public string Duration { get; set; }
+
+        public DurationUnit DurationUnit { get; set; }
+
+        public string WorkingWeekDescription { get; set; }
+
+        [TypeOfDecimal(2, ErrorMessage = ErrMsg.TypeOfDecimal.WeeklyHours)]
+        public string WeeklyHours { get; set; }
+
+        public WageType? WageType { get; set; }
+
+        [TypeOfMoney(ErrorMessage = ErrMsg.TypeOfMoney.FixedWageYearlyAmount)]
+        public string FixedWageYearlyAmount { get; set; }
+
+        public string WageAdditionalInformation { get; set; }
+
         public ReviewSummaryViewModel Review { get; set; } = new ReviewSummaryViewModel();
+
+        public string TrainingTitle { get; set; }
+        public int TrainingDurationMonths { get; set; }
 
         public IList<string> OrderedFieldNames => new List<string>
         {
@@ -18,5 +41,7 @@ namespace Esfa.Recruit.Employer.Web.ViewModels.Part1.Wage
         };
 
         public PartOnePageInfoViewModel PageInfo { get; set; }
+
+        public bool ShowTraining => string.IsNullOrWhiteSpace(TrainingTitle) == false && TrainingDurationMonths > 0;
     }
 }
