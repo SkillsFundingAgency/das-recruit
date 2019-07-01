20124faa3ac815d12d30802bafdd87cf30ead475
Peter Morris <mrpmorris@gmail.com>
2019-07-01 16:16:48 +0100

    ER-1235 Remove Foundation and Masters programme levels

--- src/Shared/Recruit.Vacancies.Client/Domain/Entities/ProgrammeLevel.cs
+++ src/Shared/Recruit.Vacancies.Client/Domain/Entities/ProgrammeLevel.cs
@@ -1,12 +1,17 @@
-﻿namespace Esfa.Recruit.Vacancies.Client.Domain.Entities
+﻿using System;
+
+namespace Esfa.Recruit.Vacancies.Client.Domain.Entities
 {
     public enum ProgrammeLevel
     {
-        
         Unknown = 0,
         Intermediate = 2,
         Advanced = 3,
         Higher = 4,
-        Degree = 6
+        [Obsolete("Use Higher")]
+        FoundationDegree = 5,
+        Degree = 6,
+        [Obsolete("Use Degree")]
+        Masters = 7
     }
 }
