ac4e1be08fa60c9bf44607fde61839d5cab29b6b
Chris Jensen <chrisjensenuk@gmail.com>
2019-07-05 15:05:58 +0100

    ER-1335 - Training - Handling empty auto suggestion submits

---
+++ src/Employer/Employer.Web/wwwroot/javascripts/esfa-training-suggestions/training-suggestions-service.js
@@ -0,0 +1,17 @@
+﻿var trainingSuggestionsService = function () {
+
+    var init = function(selectElementSelector) {
+        var selectElement = document.querySelector(selectElementSelector);
+
+        accessibleAutocomplete.enhanceSelectElement({
+            selectElement: selectElement,
+            displayMenu: "inline",
+            defaultValue: "",
+            confirmOnBlur: true
+        });
+    };
+
+    return {
+        init: init
+    };
+}();
