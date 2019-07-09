d6a013dcac43afbe934cd87bf97a8049d771020c
Chris Jensen <chrisjensenuk@gmail.com>
2019-07-09 15:01:50 +0100

    ER-1337 - Training confirmation Employer

--- src/Employer/Employer.Web/ViewModels/Part1/Training/ConfirmTrainingViewModel.cs
+++ src/Employer/Employer.Web/ViewModels/Part1/Training/ConfirmTrainingViewModel.cs
@@ -11,29 +11,5 @@ namespace Esfa.Recruit.Employer.Web.ViewModels.Part1.Training
         public string ProgrammeType {get; set; }
         public string ProgrammeId { get; set; }
         public PartOnePageInfoViewModel PageInfo { get; set; }
-
-        public string LevelAsText
-        {
-            get
-            {
-                switch (Level)
-                {
-                    case ProgrammeLevel.Intermediate:
-                        return "Level:2 (equivalent to GCSEs at grades A* to C)";
-                    case ProgrammeLevel.Advanced:
-                        return "Level:3 (equivalent to A levels at grades A to E)";
-                    case ProgrammeLevel.Higher:
-                        return "Level:4 (equivalent to certificate of higher education)";
-                    case ProgrammeLevel.FoundationDegree:
-                        return "Level:5 (equivalent to foundation degree)";
-                    case ProgrammeLevel.Degree:
-                        return "Level:6 (equivalent to bachelor’s degree)";
-                    case ProgrammeLevel.Masters:
-                        return "Level:7 (equivalent to master’s degree)";
-                    default:
-                        return "";
-                }
-            }
-        }
     }
 }
