d92ae70af46652862cb773f992eb1afe22d7b16f
Deven Shah <deven.shah@valtech.co.uk>
2019-07-10 09:53:37 +0100

    ER-1385 Hide de-scoped  options in notification  preferences

--- src/Employer/Employer.Web/Orchestrators/ManageNotificationsOrchestrator.cs
+++ src/Employer/Employer.Web/Orchestrators/ManageNotificationsOrchestrator.cs
@@ -44,6 +44,8 @@ namespace Esfa.Recruit.Employer.Web.Orchestrators
                 return new OrchestratorResponse(_notificationTypeIsRequiredForTheFirstTime);
             }
 
+            if (editModel.IsApplicationSubmittedSelected) editModel.NotificationFrequency = NotificationFrequency.Immediately;
+
             var preferences = GetDomainModel(editModel, vacancyUser.UserId);
 
             return await ValidateAndExecute(
