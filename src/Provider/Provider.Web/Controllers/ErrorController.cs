0a3ff11099b1e02edae80c74f15afe9c9106ea35
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2019-07-10 11:44:40 +0100

    Update ErrorController.cs

--- src/Provider/Provider.Web/Controllers/ErrorController.cs
+++ src/Provider/Provider.Web/Controllers/ErrorController.cs
@@ -135,13 +135,11 @@ namespace Esfa.Recruit.Provider.Web.Controllers
 
         private IActionResult AccessDenied()
         {
-            LogUserClaims();
-
             var serviceClaims = User.FindAll(ProviderRecruitClaims.IdamsUserServiceTypeClaimTypeIdentifier);
 
             if (serviceClaims.Any(claim => claim.Value.Equals(ProviderRecruitClaims.ServiceClaimValue) == false))
             {
-                _logger.LogInformation($"User {User.Identity.Name} does not have service claim.");
+                _logger.LogInformation("User does not have service claim.");
                 return Redirect(_externalLinks.ProviderApprenticeshipSiteUrl);
             }
 
@@ -170,16 +168,6 @@ namespace Esfa.Recruit.Provider.Web.Controllers
             
             Response.StatusCode = (int)HttpStatusCode.Unauthorized;
             return View(ViewNames.AccessDenied);
-        }
-
-        private void LogUserClaims()
-        {
-            var sb = new StringBuilder();
-            foreach (var claim in User.Claims)
-            {
-                sb.AppendLine($"{claim.Type}: {claim.Value}");
-            }
-            _logger.LogInformation(sb.ToString());
         }
 
         private IActionResult PageNotFound()
