f8989b40fa753abb74701eccdaf236d65f9ebf02
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2019-07-11 15:04:02 +0100

    ER-1366_get ukprn and programmeid from query string.

--- src/Employer/Employer.Web/Controllers/EmployerVacancyController.cs
+++ src/Employer/Employer.Web/Controllers/EmployerVacancyController.cs
@@ -8,10 +8,16 @@ namespace Esfa.Recruit.Employer.Web.Controllers
     public class EmployerVacancyController : Controller
     {        
         [HttpGet("employer-create-vacancy", Name = RouteNames.EmployerCreateVacancy_Get)]        
-        public IActionResult CreateVacancy()
+        public IActionResult CreateVacancy(string ukprn, string programmeId)
         {
             TempData[TempDataKeys.ReferredFromMAHome] = true;
-            return RedirectToRoute(RouteNames.CreateVacancyOptions_Get);
+            if (string.IsNullOrWhiteSpace(ukprn) && string.IsNullOrWhiteSpace(programmeId))
+            {
+                return RedirectToRoute(RouteNames.CreateVacancyOptions_Get);
+            }
+            TempData[TempDataKeys.ReferredFromMAHome_UKPRN] = ukprn;
+            TempData[TempDataKeys.ReferredFromMAHome_ApprenticeshipId] = programmeId;
+            return RedirectToRoute(RouteNames.CreateVacancy_Get);
         }
 
         [HttpGet("employer-manage-vacancy", Name = RouteNames.EmployerManageVacancy_Get)]
