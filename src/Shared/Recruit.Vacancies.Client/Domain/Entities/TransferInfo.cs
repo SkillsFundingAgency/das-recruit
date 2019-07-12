1f1b8e58e614ccb49f359b6c1aab213b8197a271
Deven Shah <deven.shah@valtech.co.uk>
2019-07-12 08:40:24 +0100

    ER-1414 Renamed property in TransferInfo

--- src/Shared/Recruit.Vacancies.Client/Domain/Entities/TransferInfo.cs
+++ src/Shared/Recruit.Vacancies.Client/Domain/Entities/TransferInfo.cs
@@ -7,7 +7,7 @@ namespace Esfa.Recruit.Vacancies.Client.Domain.Entities
         public long Ukprn { get; set; }
         public string ProviderName { get; set; }
         public string LegalEntityName { get; set; }
-        public VacancyUser TransferredBy { get; set; }
+        public VacancyUser TransferredByUser { get; set; }
         public DateTime TransferredDate { get; set; }
     }
 }
