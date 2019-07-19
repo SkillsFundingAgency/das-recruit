d20f80bca4cc11955611c82c8c7e2f7744048c62
sachin.patel <sachin.patel@valtech.co.uk>
2019-07-19 15:28:38 +0100

    ER-1411 🚫 - Provider access revoked page on sign-in if they have been revoked on Recruit +semver: minor

--- src/Shared/Recruit.Shared.Web/Views/RecruitPartialNames.cs
+++ src/Shared/Recruit.Shared.Web/Views/RecruitPartialNames.cs
@@ -3,6 +3,7 @@ namespace Esfa.Recruit.Shared.Web.Views
     public static class RecruitPartialNames
     {
         public const string ApplicationInsights = "_ApplicationInsightsPartial";
+        public const string ContactDetailsListPartial = "_ContactDetailsListPartial";
         public const string DisabilityConfident = "_DisabilityConfidentPartial";
         public const string GetHelpContactDetails = "_GetHelpContactDetailsPartial";
         public const string GoogleTagManagerHead = "_GoogleTagManagerHeadPartial";
