5170952f38599ceda340368aaadaa2484e27b642
Peter Morris <mrpmorris@gmail.com>
2019-07-19 12:51:03 +0100

    ER-1351 Move service registration back out of IoC.cs file because it makes Deven sad

--- src/QA/QA.Web/Startup.ConfigureServices.cs
+++ src/QA/QA.Web/Startup.ConfigureServices.cs
@@ -1,6 +1,7 @@
 ﻿using Esfa.Recruit.Qa.Web.Configuration;
 using Esfa.Recruit.Qa.Web.Mappings;
 using Esfa.Recruit.Qa.Web.Orchestrators;
+using Esfa.Recruit.Qa.Web.Orchestrators.Reports;
 using Esfa.Recruit.Qa.Web.Security;
 using Esfa.Recruit.QA.Web.Configuration;
 using Esfa.Recruit.QA.Web.Filters;
@@ -11,7 +12,6 @@ using Esfa.Recruit.Shared.Web.Services;
 using Esfa.Recruit.Vacancies.Client.Application.Configuration;
 using Esfa.Recruit.Vacancies.Client.Ioc;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -22,6 +22,8 @@ namespace Esfa.Recruit.Qa.Web
     {
         private readonly IConfiguration _configuration;
         private readonly AuthenticationConfiguration _authenticationConfig;
+        private readonly AuthorizationConfiguration _legacyAuthorizationConfig;
+        private readonly AuthorizationConfiguration _authorizationConfig;
         private readonly ExternalLinksConfiguration _externalLinks;
         private readonly ILoggerFactory _loggerFactory;
 
@@ -29,6 +31,8 @@ namespace Esfa.Recruit.Qa.Web
         {
             _configuration = configuration;
             _authenticationConfig = _configuration.GetSection("Authentication").Get<AuthenticationConfiguration>();
+            _legacyAuthorizationConfig = _configuration.GetSection("LegacyAuthorization").Get<AuthorizationConfiguration>();
+            _authorizationConfig = _configuration.GetSection("Authorization").Get<AuthorizationConfiguration>();
             _externalLinks = _configuration.GetSection("ExternalLinks").Get<ExternalLinksConfiguration>();
             _loggerFactory = loggerFactory;
         }
@@ -36,7 +40,10 @@ namespace Esfa.Recruit.Qa.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddIoC(_configuration);
+            services.Configure<ApplicationInsightsConfiguration>(_configuration.GetSection("ApplicationInsights"));
+
+            //A service provider for resolving services configured in IoC
+            var sp = services.BuildServiceProvider();
 
             // Routing has to come before adding Mvc
             services.AddRouting(opt =>
@@ -56,9 +63,32 @@ namespace Esfa.Recruit.Qa.Web
                 }
             );
 
-            services.Configure<RazorViewEngineOptions>(o =>
+            services.AddApplicationInsightsTelemetry(_configuration);
+            services.AddAuthenticationService(_authenticationConfig);
+            services.AddAuthorizationService(_legacyAuthorizationConfig, _authorizationConfig);
+
+            services.AddRecruitStorageClient(_configuration);
+            services.AddScoped<DashboardOrchestrator>();
+            services.AddScoped<ReviewOrchestrator>();
+            services.AddScoped<ReportDashboardOrchestrator>();
+            services.AddScoped<ApplicationsReportOrchestrator>();
+            services.AddScoped<ReportConfirmationOrchestrator>();
+
+            services.AddTransient<UserAuthorizationService>();
+
+            services.AddTransient<IGeocodeImageService>(_ => new GoogleMapsGeocodeImageService(_configuration.GetValue<string>("GoogleMapsPrivateKey")));
+            services.AddScoped<ReviewMapper>();
+            services.AddTransient<IReviewSummaryService, ReviewSummaryService>();
+            services.AddTransient<ReviewFieldIndicatorMapper>();
+
+            services.AddScoped<IRuleMessageTemplateRunner, RuleMessageTemplateRunner>();
+
+            services.AddScoped<PlannedOutageResultFilter>();
+
+            services.AddSingleton(x =>
             {
-                o.ViewLocationFormats.Add("/Views/Reports/{1}/{0}" + RazorViewEngine.ViewExtension);
+                var svc = x.GetService<IConfigurationReader>();
+                return svc.GetAsync<QaRecruitSystemConfiguration>("QaRecruitSystem").Result;
             });
         }
     }
