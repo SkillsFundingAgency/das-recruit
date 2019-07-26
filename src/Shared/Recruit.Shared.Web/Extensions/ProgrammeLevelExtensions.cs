7f8ef0954b859596fd53d45988e4fd391769d2ad
Peter Morris <mrpmorris@gmail.com>
2019-07-26 16:10:46 +0100

    ER-1235 remove some apprenticeship levels +semver: minor (#714)
    * ER-1235 Remove two apprenticeship levels

--- src/Shared/Recruit.Shared.Web/Extensions/ProgrammeLevelExtensions.cs
+++ src/Shared/Recruit.Shared.Web/Extensions/ProgrammeLevelExtensions.cs
@@ -14,12 +14,8 @@ namespace Esfa.Recruit.Shared.Web.Extensions
                     return "Level:3 (equivalent to A levels at grades A to E)";
                 case ProgrammeLevel.Higher:
                     return "Level:4 (equivalent to certificate of higher education)";
-                case ProgrammeLevel.FoundationDegree:
-                    return "Level:5 (equivalent to foundation degree)";
                 case ProgrammeLevel.Degree:
                     return "Level:6 (equivalent to bachelor’s degree)";
-                case ProgrammeLevel.Masters:
-                    return "Level:7 (equivalent to master’s degree)";
                 default:
                     return "";
             }
