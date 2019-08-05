5e5f0643dadf8822ad69d13c230b997e38c40fdd
sachin.patel <sachin.patel@valtech.co.uk>
2019-08-05 14:00:58 +0100

    ER-1435 🔨 - Prevent vacancy from going through publish process when transferred draft vacancy has a review that is approved +semver: patch

--- src/Shared/Recruit.Vacancies.Client/Application/CommandHandlers/ApproveVacancyReviewCommandHandler.cs
+++ src/Shared/Recruit.Vacancies.Client/Application/CommandHandlers/ApproveVacancyReviewCommandHandler.cs
@@ -15,18 +15,21 @@ namespace Esfa.Recruit.Vacancies.Client.Application.CommandHandlers
     public class ApproveVacancyReviewCommandHandler: IRequestHandler<ApproveVacancyReviewCommand>
     {
         private readonly ILogger<ApproveVacancyReviewCommandHandler> _logger;
+        private readonly IVacancyRepository _vacancyRepository;
         private readonly IVacancyReviewRepository _vacancyReviewRepository;
         private readonly IMessaging _messaging;
         private readonly AbstractValidator<VacancyReview> _vacancyReviewValidator;
         private readonly ITimeProvider _timeProvider;
 
-        public ApproveVacancyReviewCommandHandler(ILogger<ApproveVacancyReviewCommandHandler> logger, 
-                                        IVacancyReviewRepository vacancyReviewRepository, 
+        public ApproveVacancyReviewCommandHandler(ILogger<ApproveVacancyReviewCommandHandler> logger,
+                                        IVacancyReviewRepository vacancyReviewRepository,
+                                        IVacancyRepository vacancyRepository,
                                         IMessaging messaging,
                                         AbstractValidator<VacancyReview> vacancyReviewValidator,
                                         ITimeProvider timeProvider)
         {
             _logger = logger;
+            _vacancyRepository = vacancyRepository;
             _vacancyReviewRepository = vacancyReviewRepository;
             _messaging = messaging;
             _vacancyReviewValidator = vacancyReviewValidator;
@@ -38,13 +41,14 @@ namespace Esfa.Recruit.Vacancies.Client.Application.CommandHandlers
             _logger.LogInformation("Approving review {reviewId}.", message.ReviewId);
 
             var review = await _vacancyReviewRepository.GetAsync(message.ReviewId);
+            var vacancy = await _vacancyRepository.GetVacancyAsync(review.VacancyReference);
 
             if (!review.CanApprove)
             {
                 _logger.LogWarning($"Unable to approve review {{reviewId}} due to review having a status of {review.Status}.", message.ReviewId);
                 return;
             }
-            
+
             review.ManualOutcome = ManualQaOutcome.Approved;
             review.Status = ReviewStatus.Closed;
             review.ClosedDate = _timeProvider.Now;
@@ -61,11 +65,10 @@ namespace Esfa.Recruit.Vacancies.Client.Application.CommandHandlers
 
             await _vacancyReviewRepository.UpdateAsync(review);
 
-            await _messaging.PublishEvent(new VacancyReviewApprovedEvent
+            if (vacancy.Status != VacancyStatus.Draft) // it has been referred back as part of vacancy transfer (i.e. It was in Submitted state prior to transfer)
             {
-                ReviewId = message.ReviewId,
-                VacancyReference = review.VacancyReference
-            });
+                await RaiseVacancyEventSoVacancyIsPublished(message, review);
+            }
         }
 
         private void Validate(VacancyReview review)
@@ -75,6 +78,15 @@ namespace Esfa.Recruit.Vacancies.Client.Application.CommandHandlers
             {
                 throw new ValidationException(validationResult.Errors);
             }
+        }
+
+        private Task RaiseVacancyEventSoVacancyIsPublished(ApproveVacancyReviewCommand message, VacancyReview review)
+        {
+            return _messaging.PublishEvent(new VacancyReviewApprovedEvent
+            {
+                ReviewId = message.ReviewId,
+                VacancyReference = review.VacancyReference
+            });
         }
     }
 }
