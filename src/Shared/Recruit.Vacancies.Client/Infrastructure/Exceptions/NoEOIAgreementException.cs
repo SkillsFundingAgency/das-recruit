e5b21a1a84bc5b831a172b65616f347b3d675b35
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2019-08-06 09:56:42 +0100

    ER-1390_Delete unused file.

--- src/Shared/Recruit.Vacancies.Client/Infrastructure/Exceptions/NoEOIAgreementException.cs
+++
@@ -1,9 +0,0 @@
-﻿using Esfa.Recruit.Vacancies.Client.Domain.Exceptions;
-
-namespace Esfa.Recruit.Vacancies.Client.Infrastructure.Exceptions
-{
-    public class NoEOIAgreementException : RecruitException
-    {
-        public NoEOIAgreementException(string message) : base(message) { }
-    }
-}
