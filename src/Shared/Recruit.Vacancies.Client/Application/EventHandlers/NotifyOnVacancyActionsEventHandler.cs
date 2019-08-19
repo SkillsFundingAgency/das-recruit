2cfbf0de4b1c1151f0334497ad0cb111a6b63f6d
Deven Shah <deven.shah@valtech.co.uk>
2019-08-19 16:09:59 +0100

    ER-1387 Moved sending communication command to vacancy closed event handler

--- src/Shared/Recruit.Vacancies.Client/Application/EventHandlers/NotifyOnVacancyActionsEventHandler.cs
+++ src/Shared/Recruit.Vacancies.Client/Application/EventHandlers/NotifyOnVacancyActionsEventHandler.cs
@@ -13,35 +13,26 @@ using Esfa.Recruit.Vacancies.Client.Infrastructure.StorageQueue;
 
 namespace Esfa.Recruit.Vacancies.Client.Application.EventHandlers
 {
-    public class NotifyOnVacancyActionsEventHandler : INotificationHandler<VacancyClosedEvent>,
+    public class NotifyOnVacancyActionsEventHandler :   INotificationHandler<VacancyClosedEvent>,
                                                         INotificationHandler<LiveVacancyUpdatedEvent>
     {
         private readonly INotifyVacancyUpdates _vacancyStatusNotifier;
         private readonly IVacancyRepository _vacancyRepository;
         private readonly ILogger<NotifyOnVacancyActionsEventHandler> _logger;
-        private readonly ICommunicationQueueService _communicationQueueService;
 
         public NotifyOnVacancyActionsEventHandler(
             ILogger<NotifyOnVacancyActionsEventHandler> logger, 
             INotifyVacancyUpdates vacancyStatusNotifier, 
-            IVacancyRepository vacancyRepository, 
-            ICommunicationQueueService communicationQueueService)
+            IVacancyRepository vacancyRepository)
         {
             _vacancyStatusNotifier = vacancyStatusNotifier;
             _vacancyRepository = vacancyRepository;
             _logger = logger;
-            _communicationQueueService = communicationQueueService;
         }
 
         public async Task Handle(VacancyClosedEvent notification, CancellationToken cancellationToken)
         {
             var vacancy = await _vacancyRepository.GetVacancyAsync(notification.VacancyId);
-
-            if (vacancy.ClosureReason == ClosureReason.WithdrawnByQa)
-            {
-                var commsRequest = GetVacancyWithdrawnByQaCommunicationRequest(vacancy.VacancyReference.Value);
-                await _communicationQueueService.AddMessageAsync(commsRequest);
-            }
 
             try
             {
@@ -77,14 +68,6 @@ namespace Esfa.Recruit.Vacancies.Client.Application.EventHandlers
             {
                 _logger.LogError(ex, $"Unable to send notification for {nameof(LiveVacancyUpdatedEvent)} and VacancyReference: {{vacancyReference}}", notification.VacancyReference);
             }
-        }
-
-        private CommunicationRequest GetVacancyWithdrawnByQaCommunicationRequest(long vacancyReference)
-        {
-            var commsRequest = new CommunicationRequest(
-                CommunicationConstants.RequestType.VacancyWithdrawnByQa, CommunicationConstants.ServiceName, CommunicationConstants.ServiceName);
-            commsRequest.AddEntity(CommunicationConstants.EntityTypes.Vacancy, vacancyReference);
-            return commsRequest;
         }
     }
 }
