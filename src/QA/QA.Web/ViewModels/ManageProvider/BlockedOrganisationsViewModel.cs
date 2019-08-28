2dc06d9461f866755fcd580d91c84c3325d49552
Deven Shah <deven.shah@valtech.co.uk>
2019-08-28 14:02:00 +0100

    ER-1492 Removed cancel button and content changes

--- src/QA/QA.Web/ViewModels/ManageProvider/BlockedOrganisationsViewModel.cs
+++ src/QA/QA.Web/ViewModels/ManageProvider/BlockedOrganisationsViewModel.cs
@@ -6,5 +6,7 @@ namespace Esfa.Recruit.QA.Web.ViewModels.ManageProvider
     {
         public List<BlockedOrganisationViewModel> BlockedOrganisations { get; set; } = new List<BlockedOrganisationViewModel>();
         public bool HasBlockedOrganisations => BlockedOrganisations.Count > 0;
+
+        public bool HasOneBlockedProviders => BlockedOrganisations.Count == 1;
     }
 }
