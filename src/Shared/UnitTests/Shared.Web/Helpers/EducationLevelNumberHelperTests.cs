78ff682db9c290b7dd70906deb02b38e17f19931
Peter Morris <mrpmorris@gmail.com>
2019-09-05 16:03:33 +0100

    Update unit tests

--- src/Shared/UnitTests/Shared.Web/Helpers/EducationLevelNumberHelperTests.cs
+++ src/Shared/UnitTests/Shared.Web/Helpers/EducationLevelNumberHelperTests.cs
@@ -9,8 +9,8 @@ namespace Esfa.Recruit.Vacancies.Client.UnitTests.Shared.Web.Helpers
         [Theory]
         [InlineData(2, "Level 2 (GCSE)")]
         [InlineData(3, "Level 3 (A level)")]
-        [InlineData(4, "Level 4 (Higher national Certificate)")]
-        [InlineData(5, "Level 5 (Higher national Diploma)")]
+        [InlineData(4, "Level 4 (Higher national certificate)")]
+        [InlineData(5, "Level 5 (Higher national diploma)")]
         [InlineData(6, "Level 6 (Degree with honours)")]
         [InlineData(7, "Level 7 (Master's degree)")]
         public void WhenGettingEducationLevelNumberName_ShouldReturnCorrectDescription(int level, string expectedDescription)
