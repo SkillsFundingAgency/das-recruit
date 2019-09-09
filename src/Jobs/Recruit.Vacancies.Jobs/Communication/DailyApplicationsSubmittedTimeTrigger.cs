852f055754c14ea88ab02755611ddefa29b0d949
sachin.patel <sachin.patel@valtech.co.uk>
2019-09-09 11:24:02 +0100

    ER-1242 ⏰ - Daily and weekly triggered jobs for ApplicationSubmitted notifications +semver: minor

---
+++ src/Jobs/Recruit.Vacancies.Jobs/Communication/DailyApplicationsSubmittedTimeTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Communication.Types;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Esfa.Recruit.Vacancies.Client.Infrastructure.StorageQueue;
+using Esfa.Recruit.Vacancies.Client.Application.Providers;
+using Esfa.Recruit.Vacancies.Client.Application.Communications;
+
+namespace Esfa.Recruit.Vacancies.Jobs.Communication
+{
+    public class DailyApplicationsSubmittedTimeTrigger
+    {
+        private readonly ILogger<DailyApplicationsSubmittedTimeTrigger> _logger;
+        private readonly ICommunicationQueueService _queue;
+        private readonly ITimeProvider _timeProvider;
+
+        public DailyApplicationsSubmittedTimeTrigger(ILogger<DailyApplicationsSubmittedTimeTrigger> logger, ICommunicationQueueService queue, ITimeProvider timeProvider)
+        {
+            _logger = logger;
+            _queue = queue;
+            _timeProvider = timeProvider;
+        }
+
+        public Task DailyApplicationsSubmittedCreateAggregateRequestAsync([TimerTrigger(Schedules.EightAmDaily)] TimerInfo timerInfo, TextWriter log)
+        {
+            const int eightAmHour = 8;
+            _logger.LogInformation($"Timer trigger {this.GetType().Name} fired");
+
+            var to = _timeProvider.Today.AddHours(eightAmHour);
+            var from = to.AddDays(-1);
+
+            var message = new AggregateCommunicationRequest(Guid.NewGuid(), CommunicationConstants.RequestType.ApplicationSubmitted, DeliveryFrequency.Daily, _timeProvider.Now, from, to);
+
+            return _queue.AddMessageAsync(message);
+        }
+    }
+}
--- src/Shared/Recruit.Vacancies.Client/Infrastructure/StorageQueue/CommunicationStorageQueueService.cs
+++ src/Shared/Recruit.Vacancies.Client/Infrastructure/StorageQueue/CommunicationStorageQueueService.cs
@@ -1,11 +1,19 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Communication.Types;
 using Microsoft.WindowsAzure.Storage;
 
 namespace Esfa.Recruit.Vacancies.Client.Infrastructure.StorageQueue
 {
     internal class CommunicationStorageQueueService : StorageQueueServiceBase, ICommunicationQueueService
     {
-        private const string QueueName = "communication-requests-queue";
+        private readonly IDictionary<Type, string> _messageToCommunicationStorageQueueMapper = new Dictionary<Type, string>
+        {
+            { typeof(CommunicationRequest), "communication-requests-queue" },
+            { typeof(AggregateCommunicationRequest), "aggregate-communication-requests-queue" }
+        };
+
         protected override string ConnectionString { get; }
 
         public CommunicationStorageQueueService(string connString)
@@ -15,10 +23,15 @@ namespace Esfa.Recruit.Vacancies.Client.Infrastructure.StorageQueue
 
         public override async Task AddMessageAsync<T>(T message)
         {
+            var queueName = _messageToCommunicationStorageQueueMapper[typeof(T)];
+
+            if(string.IsNullOrEmpty(queueName))
+                throw new ArgumentException($"Cannot map type {typeof(T).Name} to a queue name");
+
             var storageAccount = CloudStorageAccount.Parse(ConnectionString);
             var client = storageAccount.CreateCloudQueueClient();
 
-            var queue = client.GetQueueReference(QueueName);
+            var queue = client.GetQueueReference(queueName);
 
             await AddMessageToQueueAsync(queue, message);
         }
