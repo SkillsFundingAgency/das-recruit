6f878b0f04b2ff594d702138166801d0e47da695
chrisjensenuk <chrisjensenuk@gmail.com>
2019-10-15 13:55:47 +0100

    ER-1427 - Ignoring withdrawn applications on dashboard count (#818) +semver: patch
    * ER-1427 - Ignoring withdrawn applications on dashboard count
    
    * ER-1427 - filtering out withdrawn applications

--- src/Shared/Recruit.Vacancies.Client/Infrastructure/Services/VacancySummariesProvider/VacancySummaryAggQueryBuilder.cs
+++ src/Shared/Recruit.Vacancies.Client/Infrastructure/Services/VacancySummariesProvider/VacancySummaryAggQueryBuilder.cs
@@ -1,176 +1,156 @@
-using Esfa.Recruit.Vacancies.Client.Domain.Entities;
-using Esfa.Recruit.Vacancies.Client.Infrastructure.Mongo;
+using System.Linq;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 
 namespace Esfa.Recruit.Vacancies.Client.Infrastructure.Services.VacancySummariesProvider
 {
     public static class VacancySummaryAggQueryBuilder
     {
-        private const string PathSpecifierFieldName = "path";
-        private const string PreserveNullAndEmptyArraysSpecifierFieldName = "preserveNullAndEmptyArrays";
-        private const string ApplicationReviewLookupAliasName = "candidateApplicationReview";
-        private static readonly BsonArray _newApplicationReviewStatusClause = new BsonArray { "$appStatus", ApplicationReviewStatus.New.ToString() };
-
-        private static readonly BsonArray _successfulApplicationReviewStatusClause = new BsonArray { "$appStatus", ApplicationReviewStatus.Successful.ToString() };
-
-        private static readonly BsonArray _unsuccessfulApplicationReviewStatusClause = new BsonArray { "$appStatus", ApplicationReviewStatus.Unsuccessful.ToString() };
-
-        private static readonly BsonDocument _lookupClause = new BsonDocument
-        {
+        private const string Pipeline = @"[
             {
-                "$lookup",
-                new BsonDocument
-                {
-                    { "from", MongoDbCollectionNames.ApplicationReviews },
-                    { "localField", "vacancyReference" },
-                    { "foreignField", "vacancyReference" },
-                    { "as", ApplicationReviewLookupAliasName }
+                '$lookup': {
+                    'from': 'applicationReviews',
+                    'localField': 'vacancyReference',
+                    'foreignField': 'vacancyReference',
+                    'as': 'candidateApplicationReview'
                 }
-            }
-        };
-        private static readonly BsonDocument _unwindClause = new BsonDocument
-        {
+            },
             {
-                "$unwind",
-                new BsonDocument().Add(PathSpecifierFieldName, $"${ApplicationReviewLookupAliasName}")
-                                .Add(PreserveNullAndEmptyArraysSpecifierFieldName, true)
-            }
-        };
-        private static readonly BsonDocument _projectionOneClause = new BsonDocument
-        {
-            {
-                "$project",
-                new BsonDocument
-                {
-                    { "vacancyGuid", "$_id" },
-                    { "vacancyReference", 1 },
-                    { "title", 1 },
-                    { "status", 1 },
-                    { "appStatus", $"${ApplicationReviewLookupAliasName}.status" },
-                    { "legalEntityId", 1 },
-                    { "legalEntityName", 1 },
-                    { "employerAccountId", 1 },
-                    { "employerName", 1 },
-                    { "ukprn", $"$trainingProvider.ukprn" },
-                    { "createdDate", 1 },
-                    { "closingDate", 1 },
-                    { "startDate", 1 },
-                    { "closedDate", 1 },
-                    { "closureReason", 1 },
-                    { "applicationMethod", 1 },
-                    { "programmeId", 1 },
-                    { "duration", "$wage.duration" },
-                    { "durationUnit", "$wage.durationUnit" },
-                    { "transferInfoUkprn", "$transferInfo.ukprn" },
-                    { "transferInfoProviderName", "$transferInfo.providerName" },
-                    { "transferInfoTransferredDate", "$transferInfo.transferredDate" },
-                    { "transferInfoReason", "$transferInfo.reason" },
-                    { "trainingProviderName", "$trainingProvider.name" }
+                '$unwind': {
+                    'path': '$candidateApplicationReview',
+                    'preserveNullAndEmptyArrays': true
                 }
-            }
-        };
-        private static readonly BsonDocument _projectionTwoClause = new BsonDocument
-        {
+            },
             {
-                "$project",
-                new BsonDocument
-                {
-                    { "vacancyGuid", 1 },
-                    { "vacancyReference", 1 },
-                    { "title", 1 },
-                    { "status", 1 },
-                    { "legalEntityId", 1 },
-                    { "legalEntityName", 1 },
-                    { "employerAccountId", 1 },
-                    { "employerName", 1 },
-                    { "ukprn", 1 },
-                    { "createdDate", 1 },
-                    { "closingDate", 1 },
-                    { "startDate", 1 },
-                    { "closedDate", 1 },
-                    { "closureReason", 1 },
-                    { "applicationMethod", 1 },
-                    { "programmeId", 1 },
-                    { "duration", 1 },
-                    { "durationUnit", 1 },
-                    { "transferInfoUkprn", 1 },
-                    { "transferInfoProviderName", 1 },
-                    { "transferInfoTransferredDate", 1 },
-                    { "transferInfoReason", 1 },
-                    { "trainingProviderName", 1 }
+                '$match': { 
+                    '$or': [
+                    { 'candidateApplicationReview.isWithdrawn': {'$exists' : false} }, 
+                    { 'candidateApplicationReview.isWithdrawn': false }] }
+            },
+            {
+                '$project': {
+                    'vacancyGuid': '$_id',
+                    'vacancyReference': 1,
+                    'title': 1,
+                    'status': 1,
+                    'appStatus': '$candidateApplicationReview.status',
+                    'legalEntityId': 1,
+                    'legalEntityName': 1,
+                    'employerAccountId': 1,
+                    'employerName': 1,
+                    'ukprn': '$trainingProvider.ukprn',
+                    'createdDate': 1,
+                    'closingDate': 1,
+                    'startDate': 1,
+                    'closedDate': 1,
+                    'closureReason': 1,
+                    'applicationMethod': 1,
+                    'programmeId': 1,
+                    'duration': '$wage.duration',
+                    'durationUnit': '$wage.durationUnit',
+                    'transferInfoUkprn': '$transferInfo.ukprn',
+                    'transferInfoProviderName': '$transferInfo.providerName',
+                    'transferInfoTransferredDate': '$transferInfo.transferredDate',
+                    'transferInfoReason': '$transferInfo.reason',
+                    'trainingProviderName': '$trainingProvider.name'
                 }
-                .Add("isNew", new BsonDocument()
-                                .Add("$cond", new BsonDocument().Add("if", new BsonDocument().Add("$eq", _newApplicationReviewStatusClause))
-                                                                .Add("then", 1)
-                                                                .Add("else", 0)
-                                    )
-                    )
-                .Add("isSuccessful", new BsonDocument()
-                                        .Add("$cond", new BsonDocument().Add("if", new BsonDocument().Add("$eq", _successfulApplicationReviewStatusClause))
-                                                                        .Add("then", 1)
-                                                                        .Add("else", 0)
-                                            )
-                    )
-                .Add("isUnsuccessful", new BsonDocument()
-                                            .Add("$cond", new BsonDocument().Add("if", new BsonDocument().Add("$eq", _unsuccessfulApplicationReviewStatusClause))
-                                                                            .Add("then", 1)
-                                                                            .Add("else", 0)
-                                                )
-                    )
-            }
-        };
-        private static readonly BsonDocument _groupClause = new BsonDocument
-        {
+            },
             {
-                "$group",
-                new BsonDocument
-                {
-                    {
-                        "_id", new BsonDocument
-                        {
-                            { "vacancyGuid", "$vacancyGuid" },
-                            { "vacancyReference", "$vacancyReference" },
-                            { "title", "$title" },
-                            { "status", "$status" },
-                            { "legalEntityId", "$legalEntityId" },
-                            { "legalEntityName", "$legalEntityName" },
-                            { "employerAccountId", "$employerAccountId" },
-                            { "employerName", "$employerName" },
-                            { "ukprn", $"$ukprn" },
-                            { "createdDate", "$createdDate" },
-                            { "closingDate", "$closingDate" },
-                            { "startDate", "$startDate" },
-                            { "closedDate", "$closedDate" },
-                            { "closureReason", "$closureReason" },
-                            { "applicationMethod", "$applicationMethod" },
-                            { "programmeId", "$programmeId" },
-                            { "duration", "$duration" },
-                            { "durationUnit", "$durationUnit" },
-                            { "transferInfoUkprn", "$transferInfoUkprn" },
-                            { "transferInfoProviderName", "$transferInfoProviderName" },
-                            { "transferInfoTransferredDate", "$transferInfoTransferredDate" },
-                            { "transferInfoReason", "$transferInfoReason" },
-                            { "trainingProviderName", "$trainingProviderName" }
+                '$project': {
+                    'vacancyGuid': 1,
+                    'vacancyReference': 1,
+                    'title': 1,
+                    'status': 1,
+                    'legalEntityId': 1,
+                    'legalEntityName': 1,
+                    'employerAccountId': 1,
+                    'employerName': 1,
+                    'ukprn': 1,
+                    'createdDate': 1,
+                    'closingDate': 1,
+                    'startDate': 1,
+                    'closedDate': 1,
+                    'closureReason': 1,
+                    'applicationMethod': 1,
+                    'programmeId': 1,
+                    'duration': 1,
+                    'durationUnit': 1,
+                    'transferInfoUkprn': 1,
+                    'transferInfoProviderName': 1,
+                    'transferInfoTransferredDate': 1,
+                    'transferInfoReason': 1,
+                    'trainingProviderName': 1,
+                    'isNew': {
+                        '$cond': {
+                            'if': {'$eq': [ '$appStatus', 'New']},
+                            'then': 1,
+                            'else': 0
                         }
                     },
-                    { "noOfNewApplications", new BsonDocument().Add("$sum", "$isNew") },
-                    { "noOfSuccessfulApplications", new BsonDocument().Add("$sum", "$isSuccessful") },
-                    { "noOfUnsuccessfulApplications", new BsonDocument().Add("$sum", "$isUnsuccessful") }
+                    'isSuccessful': {
+                        '$cond': {
+                            'if': {'$eq': [ '$appStatus', 'Successful']},
+                            'then': 1,
+                            'else': 0
+                        }
+                    },
+                    'isUnsuccessful': {
+                        '$cond': {
+                            'if': {'$eq': [ '$appStatus', 'Unsuccessful']},
+                            'then': 1,
+                            'else': 0
+                        }
+                    }
+                }
+            },
+            {
+                '$group': {
+                    '_id': {
+                        'vacancyGuid': '$vacancyGuid',
+                        'vacancyReference': '$vacancyReference',
+                        'title': '$title',
+                        'status': '$status',
+                        'legalEntityId': '$legalEntityId',
+                        'legalEntityName': '$legalEntityName',
+                        'employerAccountId': '$employerAccountId',
+                        'employerName': '$employerName',
+                        'ukprn': '$ukprn',
+                        'createdDate': '$createdDate',
+                        'closingDate': '$closingDate',
+                        'startDate': '$startDate',
+                        'closedDate': '$closedDate',
+                        'closureReason': '$closureReason',
+                        'applicationMethod': '$applicationMethod',
+                        'programmeId': '$programmeId',
+                        'duration': '$duration',
+                        'durationUnit': '$durationUnit',
+                        'transferInfoUkprn': '$transferInfoUkprn',
+                        'transferInfoProviderName': '$transferInfoProviderName',
+                        'transferInfoTransferredDate': '$transferInfoTransferredDate',
+                        'transferInfoReason': '$transferInfoReason',
+                        'trainingProviderName': '$trainingProviderName'
+                    },
+                    'noOfNewApplications': {
+                        '$sum': '$isNew'
+                    },
+                    'noOfSuccessfulApplications': {
+                        '$sum': '$isSuccessful'
+                    },
+                    'noOfUnsuccessfulApplications': {
+                        '$sum': '$isUnsuccessful'
+                    }
                 }
             }
-        };
+        ]";
 
         public static BsonDocument[] GetAggregateQueryPipeline(BsonDocument vacanciesMatchClause)
         {
-            return new BsonDocument[]
-            {
-                vacanciesMatchClause,
-                _lookupClause,
-                _unwindClause,
-                _projectionOneClause,
-                _projectionTwoClause,
-                _groupClause
-            };
+            var pipeline = BsonSerializer.Deserialize<BsonArray>(Pipeline);
+            pipeline.Insert(0, vacanciesMatchClause);
+
+            var pipelineDefinition = pipeline.Values.Select(p => p.ToBsonDocument()).ToArray();
+
+            return pipelineDefinition;
         }
     }
 }
