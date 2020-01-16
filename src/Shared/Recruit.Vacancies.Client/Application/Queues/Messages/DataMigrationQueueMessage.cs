b45f43584b715769b1988463409986d9a4b3a40b
dev4valtech <dev4valtech@gmail.com>
2020-01-16 11:46:57 +0000

    CON-1185 added delay and retries

--- src/Shared/Recruit.Vacancies.Client/Application/Queues/Messages/DataMigrationQueueMessage.cs
+++ src/Shared/Recruit.Vacancies.Client/Application/Queues/Messages/DataMigrationQueueMessage.cs
@@ -4,6 +4,13 @@ namespace Esfa.Recruit.Vacancies.Client.Application.Queues.Messages
 {
     public class DataMigrationQueueMessage
     {
-        public Guid VacancyId { get; set; }
+        public int SerialNumber { get; }
+        public Guid VacancyId { get; }
+
+        public DataMigrationQueueMessage(int serialNumber, Guid vacancyId)
+        {
+            SerialNumber = serialNumber;
+            VacancyId = vacancyId; 
+        }
     }
 }
