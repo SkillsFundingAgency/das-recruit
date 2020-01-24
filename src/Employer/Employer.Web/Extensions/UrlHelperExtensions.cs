b41fafec8b75a94ee6eb4f79e3dc31b59cae94df
vas <vasantha.kasirajan@digital.education.gov.uk>
2020-01-24 15:10:25 +0000

    CON-1200 External Link for Cookies

---
+++ src/Employer/Employer.Web/Extensions/UrlHelperExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Esfa.Recruit.Employer.Web.Extensions
+{
+    public static class UrlHelperExtensions
+    {
+        public static string ExternalUrlAction(this IUrlHelper helper, string baseUrl, string controllerName, string actionName = "", bool ignoreAccountId = false)
+        {
+            var accountId = helper.ActionContext.RouteData.Values["employerAccountId"];
+
+            baseUrl = baseUrl.EndsWith("/") ? baseUrl : baseUrl + "/";
+
+            return ignoreAccountId ? $"{baseUrl}{controllerName}/{actionName}"
+                  : $"{baseUrl}accounts/{accountId}/{controllerName}/{actionName}";
+        }
+    }
+}
