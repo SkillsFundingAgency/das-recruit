9c93daea46da80768af03b40db0b16732b736b53
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2020-03-31 16:36:57 +0100

    CON-1125-fix build error

--- src/Shared/Recruit.Shared.Web/Configuration/ZendeskConfiguration.cs
+++ src/Shared/Recruit.Shared.Web/Configuration/ZendeskConfiguration.cs
@@ -1,6 +1,6 @@
 ﻿namespace Esfa.Recruit.Shared.Web.Configuration
 {
-    public class ZendeskConfiguration
+    public class ZenDeskConfiguration
     {
         public string SnippetKey { get; set; }
         public string SectionId { get; set; }
