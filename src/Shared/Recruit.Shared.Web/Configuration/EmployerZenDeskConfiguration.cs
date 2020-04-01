f0c3f3e404eac41847016c04a5ff4e81f49aa973
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2020-04-01 10:46:52 +0100

    CON-1125-rename attempt.

--- src/Shared/Recruit.Shared.Web/Configuration/EmployerZenDeskConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Esfa.Recruit.Shared.Web.Configuration
-{
-    public class EmployerZenDeskConfiguration
-    {
-        public string SnippetKey { get; set; }
-        public string SectionId { get; set; }
-        public string CobrowsingSnippetKey { get; set; }
-    }
-}
