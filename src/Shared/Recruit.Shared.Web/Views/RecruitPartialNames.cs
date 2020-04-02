c287543466a1f10ec3a15382c59e306decfa23cd
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2020-04-02 12:04:43 +0100

    Update RecruitPartialNames.cs

--- src/Shared/Recruit.Shared.Web/Views/RecruitPartialNames.cs
+++ src/Shared/Recruit.Shared.Web/Views/RecruitPartialNames.cs
@@ -10,14 +10,12 @@ namespace Esfa.Recruit.Shared.Web.Views
         public const string GoogleAnalyticsInfo = "_GoogleAnalyticsInfoPartial";
         public const string GoogleTagManagerBody = "_GoogleTagManagerBodyPartial";
         public const string ZenDeskWidget = "_ZenDeskWidgetPartial";
-        public const string ZenDeskApi = "_ZenDeskApiPartial";        
+        public const string ZenDeskApi = "_ZenDeskApiPartial";
 
         public const string Pager = "_PagerPartial";
         public const string ValidationSummary = "_ValidationSummaryPartial";
         public const string ApplyThroughExternalApplicationSiteVacancyAnalytics = "_ApplyThroughExternalApplicationSiteVacancyAnalyticsSummaryPartial";
         public const string ApplyThroughFaaVacancyAnalytics = "_ApplyThroughFaaVacancyAnalyticsSummaryPartial";
         public const string ErrorListItem = "_ErrorListItemPartial";
-        public const string ZenDeskWidget = "_ZenDeskWidgetPartial";
-        public const string ZenDeskApi = "_ZenDeskApiPartial";
     }
 }
