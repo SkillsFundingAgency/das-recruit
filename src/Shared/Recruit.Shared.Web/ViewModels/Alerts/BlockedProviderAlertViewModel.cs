f52bcd88f048291e01b6754851688589988f494b
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2020-11-09 11:40:33 +0000

    CON-2096-content change

--- src/Shared/Recruit.Shared.Web/ViewModels/Alerts/BlockedProviderAlertViewModel.cs
+++ src/Shared/Recruit.Shared.Web/ViewModels/Alerts/BlockedProviderAlertViewModel.cs
@@ -9,10 +9,19 @@ namespace Esfa.Recruit.Shared.Web.ViewModels.Alerts
         public List<string> ClosedVacancies { get; set; }
         public List<string> BlockedProviderNames { get; internal set; }
 
-        public string CountCaption => $"{"vacancy".ToQuantity(ClosedVacancies.Count)} {(ClosedVacancies.Count == 1 ? "has" : "have")} been closed.";
+        public string CountCaption => GetCaptionCount();
 
         public string BlockedProviderNamesCaption => BlockedProviderNames.Humanize().RemoveOxfordComma();
 
         public bool HasMultipleBlockedProviders => BlockedProviderNames.Count > 1;
+
+        private string GetCaptionCount()
+        {
+            if (ClosedVacancies.Count == 1)
+            {
+                return $"{"advert".ToQuantity(ClosedVacancies.Count, ShowQuantityAs.Words)} has been transferred";
+            }
+            return $"{"advert".ToQuantity(ClosedVacancies.Count)} have been transferred";
+        }
     }
 }
