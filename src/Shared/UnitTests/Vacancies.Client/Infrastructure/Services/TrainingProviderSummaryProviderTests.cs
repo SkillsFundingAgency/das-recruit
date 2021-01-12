ed21f62ceb0f80a025a7b9680aa1bd0d82a1801b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-01-12 22:09:08 +0000

    FAT2-296 - Change TrainingProviderSummaryProvider to use service

--- src/Shared/UnitTests/Vacancies.Client/Infrastructure/Services/TrainingProviderSummaryProviderTests.cs
+++ src/Shared/UnitTests/Vacancies.Client/Infrastructure/Services/TrainingProviderSummaryProviderTests.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Esfa.Recruit.Vacancies.Client.Application.Configuration;
 using Esfa.Recruit.Vacancies.Client.Application.Providers;
+using Esfa.Recruit.Vacancies.Client.Domain.Entities;
+using Esfa.Recruit.Vacancies.Client.Infrastructure.Services.TrainingProvider;
 using Esfa.Recruit.Vacancies.Client.Infrastructure.Services.TrainingProviderSummaryProvider;
 using FluentAssertions;
 using Moq;
-using SFA.DAS.Apprenticeships.Api.Types.Providers;
-using SFA.DAS.Providers.Api.Client;
 using Xunit;
 
 namespace Esfa.Recruit.Vacancies.Client.UnitTests.Vacancies.Client.Infrastructure.Services
@@ -16,11 +17,9 @@ namespace Esfa.Recruit.Vacancies.Client.UnitTests.Vacancies.Client.Infrastructur
         [Fact]
         public async Task GetAsync_ShouldReturnEsfaTestProviderForUkrpn()
         {
-            var providerClientMock = new Mock<IProviderApiClient>();
-            var cache = new TestCache();
-            var timeProviderMock = new Mock<ITimeProvider>();
+            var providerClientMock = new Mock<ITrainingProviderService>();
 
-            var sut = new TrainingProviderSummaryProvider(providerClientMock.Object, cache, timeProviderMock.Object);
+            var sut = new TrainingProviderSummaryProvider(providerClientMock.Object);
 
             var provider = await sut.GetAsync(EsfaTestTrainingProvider.Ukprn);
 
@@ -32,25 +31,49 @@ namespace Esfa.Recruit.Vacancies.Client.UnitTests.Vacancies.Client.Infrastructur
         [Fact]
         public async Task GetAsync_ShouldAttemptToFindTrainingProvider()
         {
-            var providerClientMock = new Mock<IProviderApiClient>();
-            providerClientMock.Setup(p => p.FindAllAsync())
-                .ReturnsAsync(new List<ProviderSummary>
+            var ukprn = 88888888;
+            var providerClientMock = new Mock<ITrainingProviderService>();
+            providerClientMock.Setup(p => p.GetProviderAsync(ukprn))
+                .ReturnsAsync(new TrainingProvider
                 {
-                    new ProviderSummary{Ukprn = 88888888, ProviderName = "provider 1"},
-                    new ProviderSummary{Ukprn = 88888889, ProviderName = "provider 2"}
+                    Ukprn = 88888888, 
+                    Name = "provider 1"
                 });
 
-            var cache = new TestCache();
-            var timeProviderMock = new Mock<ITimeProvider>();
-
-            var sut = new TrainingProviderSummaryProvider(providerClientMock.Object, cache, timeProviderMock.Object);
+            var sut = new TrainingProviderSummaryProvider(providerClientMock.Object);
 
             var provider = await sut.GetAsync(88888888);
 
-            providerClientMock.Verify(c => c.FindAllAsync(), Times.Once);
-
             provider.Ukprn.Should().Be(88888888);
             provider.ProviderName.Should().Be("provider 1");
+        }
+
+        [Fact]
+        public async Task GetAll_ShouldGetAllTrainingProviders()
+        {
+            var providerClientMock = new Mock<ITrainingProviderService>();
+            providerClientMock.Setup(p => p.FindAllAsync())
+                .ReturnsAsync(new List<TrainingProvider>
+                {
+                    new TrainingProvider
+                    {
+                        Ukprn = 88888888, 
+                        Name = "provider 1"
+                    },
+                    new TrainingProvider
+                    {
+                        Ukprn = 88888888, 
+                        Name = "provider 1"
+                    }
+                    
+                });
+
+            var sut = new TrainingProviderSummaryProvider(providerClientMock.Object);
+
+            var providers = await sut.FindAllAsync();
+
+            providers.Count().Should().Be(2);
+            providers.All(c => c.Ukprn.Equals(88888888) && c.ProviderName.Equals("provider 1")).Should().BeTrue();
         }
     }
 }
