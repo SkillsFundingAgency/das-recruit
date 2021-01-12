9d4ea4cd0f3e3b550d5c85e98c3a3f22260976aa
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-01-12 15:58:44 +0000

    FAT2-296 - Add request for getting providers

---
+++ src/Shared/Recruit.Vacancies.Client/Infrastructure/OuterApi/Requests/GetProvidersRequest.cs
@@ -0,0 +1,7 @@
+namespace Esfa.Recruit.Vacancies.Client.Infrastructure.OuterApi.Requests
+{
+    public class GetProvidersRequest : IGetApiRequest
+    {
+        public string GetUrl => "providers";
+    }
+}
---
+++ src/Shared/UnitTests/Vacancies.Client/Infrastructure/OuterApi/Requests/WhenConstructingGetProvidersUrl.cs
@@ -0,0 +1,19 @@
+using Esfa.Recruit.Vacancies.Client.Infrastructure.OuterApi.Requests;
+using FluentAssertions;
+using Xunit;
+
+namespace Esfa.Recruit.Vacancies.Client.UnitTests.Vacancies.Client.Infrastructure.OuterApi.Requests
+{
+    public class WhenConstructingGetProvidersUrl
+    {
+        [Fact]
+        public void Then_It_Is_Correctly_Constructed()
+        {
+            //Arrange
+            var actual = new GetProvidersRequest();
+            
+            //Assert
+            actual.GetUrl.Should().Be("providers");
+        }
+    }
+}
