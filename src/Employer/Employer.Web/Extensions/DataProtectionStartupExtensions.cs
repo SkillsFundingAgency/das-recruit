ea1673f78fb9cae13bf3badf9a0c1e5b28ecefc6
Corey <corey.faulconbridge@digital.education.gov.uk>
2021-01-14 14:19:35 +0000

    [CON-2991] Add extension method for Redis cache data protection

---
+++ src/Employer/Employer.Web/Extensions/DataProtectionStartupExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Esfa.Recruit.Shared.Web.Configuration;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace Esfa.Recruit.Employer.Web.Extensions
+{
+    public static class DataProtectionStartupExtensions
+    {
+        public static IServiceCollection AddDataProtection(this IServiceCollection services, IConfiguration configuration, IHostingEnvironment environment)
+        {
+            if (!environment.IsDevelopment())
+            {
+                var redisConfiguration = configuration.GetSection("RedisConnectionSettings")
+                    .Get<RedisConnectionSettings>();
+
+                if (redisConfiguration != null)
+                {
+                    var redisConnectionString = redisConfiguration.RedisConnectionString;
+                    var dataProtectionKeysDatabase = redisConfiguration.DataProtectionKeysDatabase;
+
+                    var redis = ConnectionMultiplexer
+                        .Connect($"{redisConnectionString},{dataProtectionKeysDatabase}");
+
+                    services.AddDataProtection()
+                        .SetApplicationName("das-employer-recruit")
+                        .PersistKeysToStackExchangeRedis(redis, "DataProtection-Keys");
+                }
+            }
+
+            return services;
+        }
+    }
+}
