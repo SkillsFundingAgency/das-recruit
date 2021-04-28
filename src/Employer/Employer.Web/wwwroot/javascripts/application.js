9d70d3bc5ee0d5f5dfdb5eb7443fd29815fdcc42
James King <hello@jamesmichaelking.com>
2021-04-28 14:49:39 +0100

    Update datalayer script to push all validation messages

--- src/Employer/Employer.Web/wwwroot/javascripts/application.js
+++ src/Employer/Employer.Web/wwwroot/javascripts/application.js
@@ -284,19 +284,18 @@ $(function () {
     // Form validation - dataLayer pushes
     var errorSummary = document.querySelector('.govuk-error-summary');
     if (errorSummary !== null) {
-        var validationErrors = errorSummary.querySelectorAll('ul.govuk-error-summary__list li');
-        var numberOfErrors = validationErrors.length;
-        var validationMessage = 'Form validation';
-        var dataLayerObj;
-        if (numberOfErrors === 1) {
-            validationMessage = validationErrors[0].innerText
-        }
-        dataLayerObj = {
-            event: 'form submission error',
-            page: pageTitle,
-            message: validationMessage
-        }
-        window.dataLayer.push(dataLayerObj)
+      var validationErrors = errorSummary.querySelectorAll('ul.govuk-error-summary__list li a');
+      var validationErrorsArr = [];
+      nodeListForEach(validationErrors, function(validationError) {
+        validationErrorsArr.push(validationError.innerText)
+      });
+      var validationMessage = validationErrorsArr.join();
+      var dataLayerObj = {
+        event: 'form submission error',
+        page: pageTitle,
+        message: validationMessage
+      }
+      window.dataLayer.push(dataLayerObj)
     }
     // Radio button selection - dataLayer pushes
     var radioWrapper = document.querySelector('.govuk-radios');
