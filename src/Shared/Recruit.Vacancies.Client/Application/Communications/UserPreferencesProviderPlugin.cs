7bce628d4685dcadd005c2af3a509ee33a352a6e
Paul Howes <paul.howes@cognetics.net>
2021-05-26 13:52:09 +0100

    Update UserPreferencesProviderPlugin.cs

--- src/Shared/Recruit.Vacancies.Client/Application/Communications/UserPreferencesProviderPlugin.cs
+++ src/Shared/Recruit.Vacancies.Client/Application/Communications/UserPreferencesProviderPlugin.cs
@@ -66,7 +66,7 @@ namespace Esfa.Recruit.Vacancies.Client.Application.Communications
             {
                 userPref.Channels = DeliveryChannelPreferences.EmailOnly;
                 userPref.Frequency = DeliveryFrequency.Immediate;
-                userPref.Scope = userPreference.NotificationScope.GetValueOrDefault().ConvertToCommunicationScope();
+                userPref.Scope = NotificationScope.Organisation;
             }
         }
 
