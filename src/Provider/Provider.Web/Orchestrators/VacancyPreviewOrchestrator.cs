68734d3c541db878dbde547c0ac1aa57362f2781
Chris Woodcock <chris@humesoftware.com>
2021-09-09 14:53:22 +0100

    Clear employer rejected reason when provider re(submits)

--- src/Provider/Provider.Web/Orchestrators/VacancyPreviewOrchestrator.cs
+++ src/Provider/Provider.Web/Orchestrators/VacancyPreviewOrchestrator.cs
@@ -102,7 +102,8 @@ namespace Esfa.Recruit.Provider.Web.Orchestrators
                 throw new InvalidStateException(string.Format(ErrMsg.VacancyNotAvailableForEditing, vacancy.Title));
             
             vacancy.EmployerName = await _vacancyClient.GetEmployerNameAsync(vacancy);
-            
+            vacancy.EmployerRejectedReason = null;
+
             return await ValidateAndExecute(
                 vacancy,
                 v => ValidateVacancy(v, SubmitValidationRules),
