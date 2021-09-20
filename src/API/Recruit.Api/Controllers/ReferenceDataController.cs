b8c234abd0ca55810708489bdd049124d91c1ae3
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-09-20 14:29:35 +0100

    FAI-74 - Add new reference data controller
    Add new controller with action to get candidate skills

---
+++ src/API/Recruit.Api/Controllers/ReferenceDataController.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Recruit.Api.Queries;
+
+namespace SFA.DAS.Recruit.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ReferenceDataController : ApiControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ReferenceDataController (IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        [Route("candidate-skills")]
+        public async Task<IActionResult> GetCandidateSkills()
+        {
+            var result = await _mediator.Send(new GetSkillsQuery());
+
+            return GetApiResponse(result);
+        }
+    }
+}
