9c7ebf9dab121d8c05bea55d1465742d534a7c46
Paul Howes <paul.howes@cognetics.net>
2021-10-06 09:33:04 +0100

    Reformat JS

--- src/Data/Recruit.ChangeScripts/010-VacancyRejectedByEmployer_Add.js
+++ src/Data/Recruit.ChangeScripts/010-VacancyRejectedByEmployer_Add.js
@@ -1,102 +1,102 @@
 {
-  const batchLimit = 100;
+    const batchLimit = 100;
 
-  // 1. Add VacancyRejectedByEmployer preference to all Providers that already have a userNotificationPreferences.
-  do {
-    var providersWithoutVacancyRejectedByEmployer = db.userNotificationPreferences.aggregate([{
-        $lookup: {
-          from: "users",
-          "let": {
-            idamsUserId: {
-              $toString: "$_id"
-            }
-          },
-          pipeline: [{
-            $match: {
-              $expr: {
-                $and: [{
-                    $eq: ["$$idamsUserId", "$idamsUserId"]
-                  },
-                  {
-                    $eq: ["$userType", "Provider"]
-                  }
-                ]
-              }
-            }
-          }],
-          as: "User"
-        }
-      },
-      {
-        $match: {
-          $and: [{
-              "User": {
-                $exists: true,
-                $not: {
-                  $size: 0
+    // 1. Add VacancyRejectedByEmployer preference to all Providers that already have a userNotificationPreferences.
+    do {
+        var providersWithoutVacancyRejectedByEmployer = db.userNotificationPreferences.aggregate([{
+                $lookup: {
+                    from: "users",
+                    "let": {
+                        idamsUserId: {
+                            $toString: "$_id"
+                        }
+                    },
+                    pipeline: [{
+                        $match: {
+                            $expr: {
+                                $and: [{
+                                        $eq: ["$$idamsUserId", "$idamsUserId"]
+                                    },
+                                    {
+                                        $eq: ["$userType", "Provider"]
+                                    }
+                                ]
+                            }
+                        }
+                    }],
+                    as: "User"
                 }
-              }
             },
             {
-              $or: [{
-                "notificationTypes": {
-                  $not: /VacancyRejectedByEmployer/
+                $match: {
+                    $and: [{
+                            "User": {
+                                $exists: true,
+                                $not: {
+                                    $size: 0
+                                }
+                            }
+                        },
+                        {
+                            $or: [{
+                                "notificationTypes": {
+                                    $not: /VacancyRejectedByEmployer/
+                                }
+                            }, {
+                                "notificationTypes": {
+                                    $exists: false
+                                }
+                            }]
+                        }
+                    ]
                 }
-              }, {
-                "notificationTypes": {
-                  $exists: false
+            },
+            {
+                $limit: batchLimit
+            },
+            {
+                $project: {
+                    _id: 1,
+                    notificationTypes: 1,
+                    user: {
+                        "$arrayElemAt": ["$User", 0]
+                    }
                 }
-              }]
             }
-          ]
+        ]);
+
+        print(`Found ${providersWithoutVacancyRejectedByEmployer._batch.length} users without VacancyRejectedByEmployer preference set`);
+
+        while (providersWithoutVacancyRejectedByEmployer.hasNext()) {
+            var doc = providersWithoutVacancyRejectedByEmployer.next();
+            var updatedNotificationTypes = "VacancyRejectedByEmployer";
+
+            print(`Updating ${doc.user.name}`);
+
+            if (doc.notificationTypes && doc.notificationTypes.length > 1) {
+                print(`${doc.user.name} has existing notificationTypes. Adding VacancyRejectedByEmployer to existing.`);
+                updatedNotificationTypes = doc.notificationTypes + ", VacancyRejectedByEmployer";
+            }
+
+            var updateDocument = {
+                $set: {
+                    "notificationTypes": updatedNotificationTypes
+                }
+            };
+
+            var writeResult = db.userNotificationPreferences.update({
+                "_id": doc._id
+            }, updateDocument, {
+                upsert: false
+            });
+
+            if (writeResult.hasWriteConcernError()) {
+                printjson(writeResult.writeConcernError);
+                quit(14);
+            }
+
+            print(`Updated ${doc.user.name}`);
         }
-      },
-      {
-        $limit: batchLimit
-      },
-      {
-        $project: {
-          _id: 1,
-          notificationTypes: 1,
-          user: {
-            "$arrayElemAt": ["$User", 0]
-          }
-        }
-      }
-    ]);
 
-    print(`Found ${providersWithoutVacancyRejectedByEmployer._batch.length} users without VacancyRejectedByEmployer preference set`);
-
-    while (providersWithoutVacancyRejectedByEmployer.hasNext()) {
-      var doc = providersWithoutVacancyRejectedByEmployer.next();
-      var updatedNotificationTypes = "VacancyRejectedByEmployer";
-
-      print(`Updating ${doc.user.name}`);
-
-      if (doc.notificationTypes && doc.notificationTypes.length > 1) {
-        print(`${doc.user.name} has existing notificationTypes. Adding VacancyRejectedByEmployer to existing.`);
-        updatedNotificationTypes = doc.notificationTypes + ", VacancyRejectedByEmployer";
-      }
-
-      var updateDocument = {
-        $set: {
-          "notificationTypes": updatedNotificationTypes
-        }
-      };
-
-      var writeResult = db.userNotificationPreferences.update({
-        "_id": doc._id
-      }, updateDocument, {
-        upsert: false
-      });
-
-      if (writeResult.hasWriteConcernError()) {
-        printjson(writeResult.writeConcernError);
-        quit(14);
-      }
-
-      print(`Updated ${doc.user.name}`);
-    }
-
-  } while (providersWithoutVacancyRejectedByEmployer._batch.length >= batchLimit);
+    } while (providersWithoutVacancyRejectedByEmployer._batch.length >= batchLimit);
 }
