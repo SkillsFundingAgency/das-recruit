e3c1031371d0511665d937e4c03330f95589cf07
A-J-Wright <alissa.wright@education.gov.uk>
2022-03-24 12:54:56 +0000

    update content

--- src/Provider/Provider.Web/Orchestrators/Part1/EmployerOrchestrator.cs
+++ src/Provider/Provider.Web/Orchestrators/Part1/EmployerOrchestrator.cs
@@ -1,25 +1,35 @@
 using System.Linq;
 using System.Threading.Tasks;
+using Esfa.Recruit.Provider.Web.Configuration.Routing;
 using Esfa.Recruit.Provider.Web.Exceptions;
 using Esfa.Recruit.Provider.Web.RouteModel;
 using Esfa.Recruit.Provider.Web.ViewModels.Part1.Employer;
 using Esfa.Recruit.Shared.Web.Models;
+using Esfa.Recruit.Shared.Web.Services;
+using Esfa.Recruit.Vacancies.Client.Application.Communications;
 using Esfa.Recruit.Vacancies.Client.Infrastructure.Client;
+using Microsoft.Extensions.Logging;
 
 namespace Esfa.Recruit.Provider.Web.Orchestrators.Part1
 {
 	public class EmployerOrchestrator 
     {
         private readonly IProviderVacancyClient _providerVacancyClient;
+        private readonly IRecruitVacancyClient _vacancyClient;
+        private readonly IReviewSummaryService _reviewSummaryService;
 
-        public EmployerOrchestrator(IProviderVacancyClient providerVacancyClient)
+
+        public EmployerOrchestrator(IProviderVacancyClient providerVacancyClient, IRecruitVacancyClient vacancyClient, ILogger<EmployerOrchestrator> logger, IReviewSummaryService reviewSummaryService)
         {
             _providerVacancyClient = providerVacancyClient;
+            _vacancyClient = vacancyClient;
+            _reviewSummaryService = reviewSummaryService;
         }
 
         public async Task<EmployersViewModel> GetEmployersViewModelAsync(VacancyRouteModel vrm)
         {
             var editVacancyInfo = await _providerVacancyClient.GetProviderEditVacancyInfoAsync(vrm.Ukprn);
+            var vacancy = await Utility.GetAuthorisedVacancyForEditAsync(_providerVacancyClient, _vacancyClient, vrm, RouteNames.AboutEmployer_Get);
 
             if (editVacancyInfo.Employers.Any() == false)
             {
@@ -28,7 +38,8 @@ namespace Esfa.Recruit.Provider.Web.Orchestrators.Part1
 
             var vm = new EmployersViewModel
             {
-                Employers = editVacancyInfo.Employers.Select(e => new EmployerViewModel {Id = e.EmployerAccountId, Name = e.Name})
+                Employers = editVacancyInfo.Employers.Select(e => new EmployerViewModel {Id = e.EmployerAccountId, Name = e.Name}),
+                Title = vacancy.Title
             };
 
             return vm;
--- src/Provider/Provider.Web/ViewModels/Part1/Employer/EmployersViewModel.cs
+++ src/Provider/Provider.Web/ViewModels/Part1/Employer/EmployersViewModel.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
+using Esfa.Recruit.Shared.Web.ViewModels;
 
 namespace Esfa.Recruit.Provider.Web.ViewModels.Part1.Employer
 {
     public class EmployersViewModel
     {
+        public PartOnePageInfoViewModel PageInfo { get; set; }
+        public string Title { get; set; }
         public string SelectedEmployerId { get; set; }
         public IEnumerable<EmployerViewModel> Employers { get; set; }
     }
