b6350cb08c50ceca8cefcd4204e23386c79093e2
A-J-Wright <alissa.wright@education.gov.uk>
2022-03-24 13:07:53 +0000

    undo orchestrator changes

--- src/Provider/Provider.Web/Orchestrators/Part1/EmployerOrchestrator.cs
+++ src/Provider/Provider.Web/Orchestrators/Part1/EmployerOrchestrator.cs
@@ -15,21 +15,17 @@ namespace Esfa.Recruit.Provider.Web.Orchestrators.Part1
 	public class EmployerOrchestrator 
     {
         private readonly IProviderVacancyClient _providerVacancyClient;
-        private readonly IRecruitVacancyClient _vacancyClient;
-        private readonly IReviewSummaryService _reviewSummaryService;
+
 
 
         public EmployerOrchestrator(IProviderVacancyClient providerVacancyClient, IRecruitVacancyClient vacancyClient, ILogger<EmployerOrchestrator> logger, IReviewSummaryService reviewSummaryService)
         {
             _providerVacancyClient = providerVacancyClient;
-            _vacancyClient = vacancyClient;
-            _reviewSummaryService = reviewSummaryService;
         }
 
         public async Task<EmployersViewModel> GetEmployersViewModelAsync(VacancyRouteModel vrm)
         {
             var editVacancyInfo = await _providerVacancyClient.GetProviderEditVacancyInfoAsync(vrm.Ukprn);
-            var vacancy = await Utility.GetAuthorisedVacancyForEditAsync(_providerVacancyClient, _vacancyClient, vrm, RouteNames.AboutEmployer_Get);
 
             if (editVacancyInfo.Employers.Any() == false)
             {
@@ -39,7 +35,6 @@ namespace Esfa.Recruit.Provider.Web.Orchestrators.Part1
             var vm = new EmployersViewModel
             {
                 Employers = editVacancyInfo.Employers.Select(e => new EmployerViewModel {Id = e.EmployerAccountId, Name = e.Name}),
-                Title = vacancy.Title
             };
 
             return vm;
