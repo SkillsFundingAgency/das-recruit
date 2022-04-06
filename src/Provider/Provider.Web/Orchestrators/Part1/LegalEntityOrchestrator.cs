17d627b281fa5ee700fd9008e689846b523c1722
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-04-06 12:22:09 +0100

    FAI-316 - Correct issue with legal entity change not being persisted

--- src/Provider/Provider.Web/Orchestrators/Part1/LegalEntityOrchestrator.cs
+++ src/Provider/Provider.Web/Orchestrators/Part1/LegalEntityOrchestrator.cs
@@ -160,8 +160,13 @@ namespace Esfa.Recruit.Provider.Web.Orchestrators.Part1
 
         public async Task SetAccountLegalEntityPublicId(VacancyRouteModel vacancyRouteModel, LegalEntityEditModel vacancyEmployerInfoModel, VacancyUser vacancyUser)
         {
+            
             var vacancy = await _utility.GetAuthorisedVacancyForEditAsync(vacancyRouteModel, RouteNames.LegalEntity_Get);
+            var employerVacancyInfo = await _providerVacancyClient.GetProviderEmployerVacancyDataAsync(vacancyRouteModel.Ukprn, vacancy.EmployerAccountId);
             vacancy.AccountLegalEntityPublicHashedId = vacancyEmployerInfoModel.SelectedOrganisationId;
+            
+            var selectedOrganisation = employerVacancyInfo.LegalEntities.Single(l => l.AccountLegalEntityPublicHashedId == vacancyEmployerInfoModel.SelectedOrganisationId);
+            vacancy.LegalEntityName = selectedOrganisation.Name;
             
             await ValidateAndExecute(
                 vacancy,
