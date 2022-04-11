560e7577c94987cf4c942000b63e57cc44b59c6a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-04-11 12:37:55 +0100

    FAI-410 - Remove old appinsights registration

--- src/Jobs/Recruit.Vacancies.Jobs/Program.cs
+++ src/Jobs/Recruit.Vacancies.Jobs/Program.cs
@@ -85,14 +85,6 @@ namespace Esfa.Recruit.Vacancies.Jobs
                         b.AddDebug();
                         b.AddConsole();
                         b.AddNLog();
-
-                        // If this key exists in any config, use it to enable App Insights
-                        string appInsightsKey = context.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
-                        if (!string.IsNullOrEmpty(appInsightsKey))
-                        {
-                            b.AddApplicationInsights(o => o.InstrumentationKey = appInsightsKey);
-                        }
-
                         b.ConfigureRecruitLogging();
                     })
                     .ConfigureServices((context, services) =>
