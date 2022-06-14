6c5157e4984e5cbefbb170ddb4e4c502b6cf538a
Scott Wakefield <scott@expertcc.co.uk>
2022-06-14 16:03:50 +0100

    FAI-456 moving interfaces, classes.

---
+++ src/Shared/Recruit.Vacancies.Client/Infrastructure/Services/VacancySummariesProvider/VacancyTaskListStatusService.cs
@@ -0,0 +1,18 @@
+﻿using Esfa.Recruit.Vacancies.Client.Domain.Entities;
+using Esfa.Recruit.Vacancies.Client.Domain.Interfaces;
+
+namespace Esfa.Recruit.Vacancies.Client.Infrastructure.Services.VacancySummariesProvider
+{
+    public class VacancyTaskListStatusService : IVacancyTaskListStatusService
+    {
+        public bool IsTaskListCompleted(ITaskListVacancy vacancy)
+        {
+            if(vacancy.VacancyType == VacancyType.Apprenticeship)
+                return vacancy.ApplicationMethod != null;
+            if (vacancy.VacancyType == VacancyType.Traineeship)
+                return !string.IsNullOrEmpty(vacancy.EmployerDescription);
+
+            return false;
+        }
+    }
+}
