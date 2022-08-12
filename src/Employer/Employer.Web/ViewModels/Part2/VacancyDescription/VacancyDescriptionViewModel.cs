35a610997ef8c7090dc63cc4736c772807c73b6e
Scott Wakefield <scott@expertcc.co.uk>
2022-08-12 10:02:19 +0100

    FAI-508 remove comments and fix vm tests.

--- src/Employer/Employer.Web/ViewModels/Part2/VacancyDescription/VacancyDescriptionViewModel.cs
+++ src/Employer/Employer.Web/ViewModels/Part2/VacancyDescription/VacancyDescriptionViewModel.cs
@@ -8,13 +8,11 @@ namespace Esfa.Recruit.Employer.Web.ViewModels.Part2.VacancyDescription
         public string Title { get; internal set; }
         public string VacancyDescription { get; internal set; }
         public string TrainingDescription { get; internal set; }
-        //public string OutcomeDescription { get; internal set; }
         public ReviewSummaryViewModel Review { get; set; } = new ReviewSummaryViewModel();
         public IList<string> OrderedFieldNames => new List<string>
         {
             nameof(VacancyDescriptionEditModel.VacancyDescription),
-            nameof(VacancyDescriptionEditModel.TrainingDescription),
-            //nameof(VacancyDescriptionEditModel.OutcomeDescription)
+            nameof(VacancyDescriptionEditModel.TrainingDescription)
         };
 
         public bool IsTaskListCompleted { get ; set ; }
