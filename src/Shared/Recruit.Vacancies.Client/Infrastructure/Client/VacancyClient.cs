b0c11acd2c6adcb4fce93998c2652fb826896e78
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-08-24 20:15:00 +0100

    FAI-556 - Additional using required from cherry pick

--- src/Shared/Recruit.Vacancies.Client/Infrastructure/Client/VacancyClient.cs
+++ src/Shared/Recruit.Vacancies.Client/Infrastructure/Client/VacancyClient.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Esfa.Recruit.Vacancies.Client.Application;
 using Esfa.Recruit.Vacancies.Client.Application.Commands;
