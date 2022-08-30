079f069246515fc7e2b4423fec4f20aefb0a9fb1
A-J-Wright <alissa.wright@education.gov.uk>
2022-08-30 09:52:58 +0100

    Changed some code to Azure.Messaging.ServiceBus; - some errors

--- src/Shared/Recruit.Vacancies.Client/Infrastructure/Services/FAA/FaaService.cs
+++ src/Shared/Recruit.Vacancies.Client/Infrastructure/Services/FAA/FaaService.cs
@@ -23,24 +23,24 @@ namespace Esfa.Recruit.Vacancies.Client.Infrastructure.Services.FAA
 
         public Task PublishApplicationStatusSummaryAsync(FaaApplicationStatusSummary message)
         {
-            TopicClient topicClient = CreateTopicClient(ApplicationStatusSummaryTopicName);
+            ServiceBusSender topicClient = CreateTopicClient(ApplicationStatusSummaryTopicName);
 
             var brokeredMessage = CreateMessage(message);
 
-            return topicClient.SendAsync(brokeredMessage);
+            return topicClient.SendMessageAsync(brokeredMessage);
         }
 
         public Task PublishVacancyStatusSummaryAsync(FaaVacancyStatusSummary message)
         {
-            TopicClient topicClient = CreateTopicClient(UpdateApprenticeshipVacancyStatus);
+            ServiceBusSender topicClient = CreateTopicClient(UpdateApprenticeshipVacancyStatus);
             var brokeredMessage = CreateMessage(message);
-            return topicClient.SendAsync(brokeredMessage);
+            return topicClient.SendMessageAsync(brokeredMessage);
         }
 
-        private static Message CreateMessage<T>(T messageObject) where T : class
+        private static ServiceBusMessage CreateMessage<T>(T messageObject) where T : class
         {
             string json = JsonConvert.SerializeObject(messageObject);
-            var message = new Message(Encoding.UTF8.GetBytes(json))
+            var message = new ServiceBusMessage(Encoding.UTF8.GetBytes(json))
             {
                 ContentType = "application/json"
             };
