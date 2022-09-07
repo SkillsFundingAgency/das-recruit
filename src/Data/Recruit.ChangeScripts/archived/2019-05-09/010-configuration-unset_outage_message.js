c63ec71b8686a44c3797ed6d15fdf40eada7d027
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-09-07 15:26:04 +0100

    FAI-605 - Remove data project

--- src/Data/Recruit.ChangeScripts/archived/2019-05-09/010-configuration-unset_outage_message.js
+++
@@ -1,8 +0,0 @@
-print("Start clearing outage message.");
-db.configuration.update({_id:"EmployerRecruitSystem"}, {$set: {plannedOutageMessage: ""}});
-print("Outage message cleared on employer.");
-db.configuration.update({_id:"QaRecruitSystem"}, {$set: {plannedOutageMessage: ""}});
-print("Outage message cleared on QA.");
-db.configuration.update({_id:"ProviderRecruitSystem"}, {$set: {plannedOutageMessage: ""}});
-print("Outage message cleared on provider.");
-print("Completed clearing outage message.");
