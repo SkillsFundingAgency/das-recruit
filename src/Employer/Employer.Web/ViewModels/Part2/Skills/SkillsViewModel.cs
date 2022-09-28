2d29ecf91fa73992435cb93b71eb7ef9abc95a61
Scott Wakefield <scott@expertcc.co.uk>
2022-09-28 09:56:24 +0100

    FAI-605 change skills base to interface

--- src/Employer/Employer.Web/ViewModels/Part2/Skills/SkillsViewModel.cs
+++ src/Employer/Employer.Web/ViewModels/Part2/Skills/SkillsViewModel.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections.Generic;
+using Esfa.Recruit.Employer.Web.RouteModel;
 using Esfa.Recruit.Shared.Web.ViewModels;
 using Esfa.Recruit.Shared.Web.ViewModels.Skills;
 
 namespace Esfa.Recruit.Employer.Web.ViewModels.Part2.Skills
 {
-    public class SkillsViewModel : SkillsViewModelBase
+    public class SkillsViewModel : ISkillsViewModel//, VacancyRouteModel
     {
         public string Title { get; internal set; }
         public string AddCustomSkillName { get; set; }
         public List<string> Skills { get; set; }
         public ReviewSummaryViewModel Review { get; set; } = new ReviewSummaryViewModel();
         public bool IsTaskListCompleted { get ; set ; }
+        public List<SkillViewModel> Column1Checkboxes { get; set; }
+        public List<SkillViewModel> Column2Checkboxes { get; set; }
     }
 }
