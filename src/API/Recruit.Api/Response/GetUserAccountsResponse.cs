2d8c0247600b5f0e6ad906656a3e1a32d04e7945
A-J-Wright <alissa.wright@education.gov.uk>
2022-11-04 10:41:54 +0000

    Add govuk package and api request and response

---
+++ src/API/Recruit.Api/Response/GetUserAccountsResponse.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace SFA.DAS.Recruit.Api.Response;
+
+public class GetUserAccountsResponse
+
+{
+    [JsonProperty("UserAccounts")] public List<EmployerIdentifier> UserAccounts { get; set; }
+}
+
+public class EmployerIdentifier
+{
+    [JsonProperty("EncodedAccountId")] public string AccountId { get; set; }
+    [JsonProperty("DasAccountName")] public string EmployerName { get; set; }
+    [JsonProperty("Role")] public string Role { get; set; }
+}
