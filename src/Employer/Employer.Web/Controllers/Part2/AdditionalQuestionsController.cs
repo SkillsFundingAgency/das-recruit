89e6fe96516483a10bc863365c689c09e6265d29
Scott Wakefield <scott@expertcc.co.uk>
2022-11-28 16:20:36 +0000

    FAI-671 adding view models and initial controller

---
+++ src/Employer/Employer.Web/Controllers/Part2/AdditionalQuestionsController.cs
@@ -0,0 +1,27 @@
+﻿using Esfa.Recruit.Employer.Web.Configuration.Routing;
+using Esfa.Recruit.Employer.Web.RouteModel;
+using Esfa.Recruit.Employer.Web.ViewModels.Part2.AdditionalQuestions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Esfa.Recruit.Employer.Web.Controllers.Part2;
+
+[Route(RoutePaths.AccountVacancyRoutePath)]
+public class AdditionalQuestionsController : Controller
+{
+    public AdditionalQuestionsController()
+    {
+        
+    }
+
+    [HttpGet("additional-questions", Name = RouteNames.AdditionalQuestions_Get)]
+    public IActionResult AdditionalQuestions(VacancyRouteModel vrm)
+    {
+        return View(new AdditionalQuestionsViewModel());
+    }
+    
+    [HttpPost("additional-questions", Name = RouteNames.AdditionalQuestions_Post)]
+    public IActionResult AdditionalQuestions(AdditionalQuestionsEditModel m)
+    {
+        return View(new AdditionalQuestionsViewModel());
+    }
+}
---
+++ src/Employer/Employer.Web/ViewModels/Part2/AdditionalQuestions/AdditionalQuestionsEditModel.cs
@@ -0,0 +1,9 @@
+﻿using Esfa.Recruit.Employer.Web.RouteModel;
+
+namespace Esfa.Recruit.Employer.Web.ViewModels.Part2.AdditionalQuestions;
+
+public class AdditionalQuestionsEditModel : VacancyRouteModel
+{
+    public string AdditionalQuestion1 { get; set; }
+    public string AdditionalQuestion2 { get; set; }
+}
