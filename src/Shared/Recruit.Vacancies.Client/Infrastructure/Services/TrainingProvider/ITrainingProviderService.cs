9a1ee0c5f7ad7a7feba09e98cb834443d276dc66
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2023-09-04 17:02:00 +0100

    Added extra validation to validate the Training Provider Scenarios to cover the draft Vacancy to submit validation

--- src/Shared/Recruit.Vacancies.Client/Infrastructure/Services/TrainingProvider/ITrainingProviderService.cs
+++ src/Shared/Recruit.Vacancies.Client/Infrastructure/Services/TrainingProvider/ITrainingProviderService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Esfa.Recruit.Vacancies.Client.Infrastructure.OuterApi.Responses;
 
 namespace Esfa.Recruit.Vacancies.Client.Infrastructure.Services.TrainingProvider
 {
@@ -7,5 +8,12 @@ namespace Esfa.Recruit.Vacancies.Client.Infrastructure.Services.TrainingProvider
     {
         Task<Domain.Entities.TrainingProvider> GetProviderAsync(long ukprn);
         Task<IEnumerable<Domain.Entities.TrainingProvider>> FindAllAsync();
+
+        /// <summary>
+        /// Contract to get the details of Provider from Outer API by given ukprn number.
+        /// </summary>
+        /// <param name="ukprn">ukprn number.</param>
+        /// <returns></returns>
+        Task<GetProviderResponseItem> GetProviderDetails(long ukprn);
     }
 }
