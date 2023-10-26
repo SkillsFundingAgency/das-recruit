5529ec875e03be7470a3599e55f49f8f9bae583e
Sarah Duncan <sarah.duncan@education.gov.uk>
2023-10-26 15:20:56 +0100

    FAI-971 implement handler

--- src/API/Recruit.Api/Queries/GetLiveVacanciesQueryHandler.cs
+++ src/API/Recruit.Api/Queries/GetLiveVacanciesQueryHandler.cs
@@ -1,14 +1,30 @@
-﻿using System;
+﻿using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Esfa.Recruit.Vacancies.Client.Infrastructure.QueryStore;
 using MediatR;
 
 namespace SFA.DAS.Recruit.Api.Queries;
 
 public class GetLiveVacanciesQueryHandler : IRequestHandler<GetLiveVacanciesQuery, GetLiveVacanciesQueryResponse>
 {
+    private readonly IQueryStoreReader _queryStoreReader;
+
+    public GetLiveVacanciesQueryHandler(IQueryStoreReader queryStoreReader)
+    {
+        _queryStoreReader = queryStoreReader;
+    }
+
     public async Task<GetLiveVacanciesQueryResponse> Handle(GetLiveVacanciesQuery request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var queryResult = await _queryStoreReader.GetAllLiveVacancies();
+
+        if (queryResult == null) { return new GetLiveVacanciesQueryResponse { ResultCode = Models.ResponseCode.NotFound }; }
+
+        //var numberResults = queryResult.ToList().Count();
+
+        return new GetLiveVacanciesQueryResponse { ResultCode = Models.ResponseCode.Success, Data = queryResult.ToList() };
+
+        // implement paging
     }
 }
