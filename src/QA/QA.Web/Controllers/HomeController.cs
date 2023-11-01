6ba6f128eb41b6abaf8ce2d8762a9630d312790c
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2023-11-01 12:06:43 +0000

    Navigate to Dashboard when the DfESignIn toggle is turned on

--- src/QA/QA.Web/Controllers/HomeController.cs
+++ src/QA/QA.Web/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace Esfa.Recruit.Qa.Web.Controllers
             bool isDfESignInAllowed = _configuration.GetValue<bool>("UseDfESignIn");
 
             // if the user is authenticated, then redirect the user to dashboard.
-            if (User.Identity is { IsAuthenticated: true }) return RedirectToAction("Index", "Dashboard");
+            if (isDfESignInAllowed && User.Identity is { IsAuthenticated: true }) return RedirectToAction("Index", "Dashboard");
 
             return View(new HomeIndexViewModel
             {
