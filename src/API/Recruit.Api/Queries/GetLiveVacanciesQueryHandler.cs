6cc2cf1c8aa0519546f573a6db8339e5e342769b
chrisfoster76 <chrisfoster186@googlemail.com>
2023-12-19 15:59:07 +0000

    Refactoring

--- src/API/Recruit.Api/Queries/GetLiveVacanciesQueryHandler.cs
+++ src/API/Recruit.Api/Queries/GetLiveVacanciesQueryHandler.cs
@@ -5,10 +5,9 @@ using System.Threading.Tasks;
 using Esfa.Recruit.Vacancies.Client.Infrastructure.QueryStore;
 using Esfa.Recruit.Vacancies.Client.Infrastructure.QueryStore.Projections.Vacancy;
 using MediatR;
+using SFA.DAS.Recruit.Api.Extensions;
 using SFA.DAS.Recruit.Api.Helpers;
 using SFA.DAS.Recruit.Api.Models;
-using SFA.DAS.VacancyServices.Wage;
-using WageType = SFA.DAS.Recruit.Api.Models.WageType;
 
 namespace SFA.DAS.Recruit.Api.Queries;
 
@@ -25,7 +24,6 @@ public class GetLiveVacanciesQueryHandler : IRequestHandler<GetLiveVacanciesQuer
     {
         var vacanciesToGetCount = request.PageSize > 1000 ? 1000 : request.PageSize;
         var vacanciesToSkipCount = request.PageNumber < 2 ? 0 : (request.PageNumber - 1) * vacanciesToGetCount;
-        
 
         var queryResult = await _queryStoreReader.GetAllLiveVacancies(vacanciesToSkipCount, vacanciesToGetCount);
 
@@ -34,39 +32,7 @@ public class GetLiveVacanciesQueryHandler : IRequestHandler<GetLiveVacanciesQuer
             return new GetLiveVacanciesQueryResponse { ResultCode = ResponseCode.Success, Data = Enumerable.Empty<LiveVacancy>() };
         }
 
-        foreach (var vacancy in queryResult)
-        {
-            if (vacancy.Wage.WageType == WageType.FixedWage.ToString())
-            {
-                vacancy.Wage.ApprenticeMinimumWage = vacancy.Wage.FixedWageYearlyAmount;
-                vacancy.Wage.Under18NationalMinimumWage = vacancy.Wage.FixedWageYearlyAmount;
-                vacancy.Wage.Between18AndUnder21NationalMinimumWage = vacancy.Wage.FixedWageYearlyAmount;
-                vacancy.Wage.Between21AndUnder25NationalMinimumWage = vacancy.Wage.FixedWageYearlyAmount;
-                vacancy.Wage.Over25NationalMinimumWage = vacancy.Wage.FixedWageYearlyAmount;
-            }
-            else if (vacancy.Wage.WageType == WageType.NationalMinimumWageForApprentices.ToString())
-            {
-                var weeklyHours = (int)decimal.Round(vacancy.Wage.WeeklyHours, MidpointRounding.AwayFromZero);
-                var rates = NationalMinimumWageService.GetAnnualRates(vacancy.StartDate, weeklyHours);
-
-                vacancy.Wage.ApprenticeMinimumWage = rates.ApprenticeMinimumWage;
-                vacancy.Wage.Under18NationalMinimumWage = rates.ApprenticeMinimumWage;
-                vacancy.Wage.Between18AndUnder21NationalMinimumWage = rates.ApprenticeMinimumWage;
-                vacancy.Wage.Between21AndUnder25NationalMinimumWage = rates.ApprenticeMinimumWage;
-                vacancy.Wage.Over25NationalMinimumWage = rates.ApprenticeMinimumWage;
-            }
-            else
-            {
-                var weeklyHours = (int) decimal.Round(vacancy.Wage.WeeklyHours, MidpointRounding.AwayFromZero);
-                var rates = NationalMinimumWageService.GetAnnualRates(vacancy.StartDate, weeklyHours);
-
-                vacancy.Wage.ApprenticeMinimumWage = rates.ApprenticeMinimumWage;
-                vacancy.Wage.Under18NationalMinimumWage = rates.Under18NationalMinimumWage;
-                vacancy.Wage.Between18AndUnder21NationalMinimumWage = rates.Between18AndUnder21NationalMinimumWage;
-                vacancy.Wage.Between21AndUnder25NationalMinimumWage = rates.Between21AndUnder25NationalMinimumWage;
-                vacancy.Wage.Over25NationalMinimumWage = rates.Over25NationalMinimumWage;
-            }
-        }
+        queryResult.ToList().ForEach(x => x.AddMinimumWageData());
 
         var totalLiveVacanciesReturned = queryResult.Count();
         var liveVacanciesCount = await _queryStoreReader.GetAllLiveVacanciesCount();
