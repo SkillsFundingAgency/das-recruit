15afc1be0319803c783eb05502c103965f52c003
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-07-08 10:33:55 +0100

    FAI-1243 - Add json property attributes

--- src/Shared/Recruit.Vacancies.Client/Infrastructure/OuterApi/Responses/GetVacancyMetricResponse.cs
+++ src/Shared/Recruit.Vacancies.Client/Infrastructure/OuterApi/Responses/GetVacancyMetricResponse.cs
@@ -1,9 +1,18 @@
+using Newtonsoft.Json;
+
 namespace Esfa.Recruit.Vacancies.Client.Infrastructure.OuterApi.Responses;
 
 public class GetVacancyMetricResponse
 {
+    [JsonProperty("viewsCount")]
     public int ViewsCount { get; set; }
+
+    [JsonProperty("searchResultsCount")]
     public int SearchResultsCount { get; set; }
+
+    [JsonProperty("applicationStartedCount")]
     public int ApplicationStartedCount { get; set; }
+
+    [JsonProperty("applicationSubmittedCount")]
     public int ApplicationSubmittedCount { get; set; }
 }
