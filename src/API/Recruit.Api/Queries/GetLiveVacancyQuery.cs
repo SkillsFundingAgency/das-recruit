cf34ca4abdc322275f96466fb621886650c0de08
Chris Foster <chrisfoster186@googlemail.com>
2024-07-19 16:00:45 +0100

    Adds more logging

--- src/API/Recruit.Api/Queries/GetLiveVacancyQuery.cs
+++ src/API/Recruit.Api/Queries/GetLiveVacancyQuery.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.Recruit.Api.Extensions;
 using SFA.DAS.Recruit.Api.Models;
 using IQueryStoreReader = Esfa.Recruit.Vacancies.Client.Infrastructure.QueryStore.IQueryStoreReader;
@@ -25,15 +27,34 @@ namespace SFA.DAS.Recruit.Api.Queries
     public class GetLiveVacancyQueryHandler : IRequestHandler<GetLiveVacancyQuery, GetLiveVacancyQueryResponse>
     {
         private readonly IQueryStoreReader _queryStoreReader;
+        private readonly ILogger<GetLiveVacancyQueryHandler> _logger;
 
-        public GetLiveVacancyQueryHandler(IQueryStoreReader queryStoreReader)
+        public GetLiveVacancyQueryHandler(IQueryStoreReader queryStoreReader, ILogger<GetLiveVacancyQueryHandler> logger)
         {
             _queryStoreReader = queryStoreReader;
+            _logger = logger;
         }
 
         public async Task<GetLiveVacancyQueryResponse> Handle(GetLiveVacancyQuery request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation($"Getting live {request.VacancyReference}");
+
             var queryResult = await _queryStoreReader.GetLiveVacancy(request.VacancyReference);
+
+            if (queryResult == null)
+            {
+                _logger.LogError($"Wage for vacancy {request.VacancyReference} is null");
+                throw new InvalidOperationException($"Wage for vacancy {request.VacancyReference} is null");
+            }
+
+            if (queryResult.Wage == null)
+            {
+                _logger.LogError($"Wage for vacancy {request.VacancyReference} is null");
+                throw new InvalidOperationException($"Wage for vacancy {request.VacancyReference} is null");
+            }
+
+            _logger.LogInformation($"Adding wage data to vacancy {request.VacancyReference}");
+            _logger.LogInformation($"Wage type for vacancy {request.VacancyReference} is  {queryResult.Wage.WageType}");
 
             queryResult.AddWageData();
 
