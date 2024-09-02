b0b3836e84936f3296e5e899873168f238dbef18
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2024-09-02 11:37:36 +0100

    Removed NLog references from the Recruit Provider and replaced with Azure Monitor Open Telemetry
    Story:https://skillsfundingagency.atlassian.net/browse/FAI-1351

--- src/Provider/Provider.Web/Program.cs
+++ src/Provider/Provider.Web/Program.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using Esfa.Recruit.Vacancies.Client.Infrastructure.Mongo;
-using Esfa.Recruit.Vacancies.Client.Infrastructure.TableStore;
-using Esfa.Recruit.Vacancies.Client.Ioc;
+﻿using Esfa.Recruit.Vacancies.Client.Ioc;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using NLog.Web;
 
 namespace Esfa.Recruit.Provider.Web
 {
@@ -13,48 +8,13 @@ namespace Esfa.Recruit.Provider.Web
     {
         public static void Main(string[] args)
         {
-            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
-            try
-            {
-                logger.Info("Starting up host");
-                var host = CreateWebHostBuilder(args).Build();
-
-                CheckInfrastructure(host.Services, logger);
-
-                host.Run();
-            }
-            catch (Exception ex)
-            {
-                //NLog: catch setup errors
-                logger.Fatal(ex, "Stopped program because of exception");
-                throw;
-            }
-            finally
-            {
-                NLog.LogManager.Shutdown();
-            }
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .UseUrls("https://localhost:5030")
-                .UseNLog()
                 .ConfigureLogging(b => b.ConfigureRecruitLogging());
-
-        private static void CheckInfrastructure(IServiceProvider serviceProvider, NLog.ILogger logger)
-        {
-            try
-            {
-                var collectionChecker = serviceProvider.GetRequiredService<MongoDbCollectionChecker>();
-                collectionChecker.EnsureCollectionsExist();
-                var storageTableChecker = (QueryStoreTableChecker)serviceProvider.GetService(typeof(QueryStoreTableChecker));
-                storageTableChecker.EnsureQueryStoreTableExist();
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex, "Error checking infrastructure");
-            }
-        }
     }
 }
