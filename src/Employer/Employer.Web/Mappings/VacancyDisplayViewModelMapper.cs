6af2a88118ca451deb84d42823469a649b1c0436
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-09-06 17:46:11 +0100

    FAI-1255 - fix issue with vacancy display view model mapper
    Fix issue with optedtoaddqualifications being null on cloned adverts

--- src/Employer/Employer.Web/Mappings/VacancyDisplayViewModelMapper.cs
+++ src/Employer/Employer.Web/Mappings/VacancyDisplayViewModelMapper.cs
@@ -51,6 +51,20 @@ namespace Esfa.Recruit.Employer.Web.Mappings
                 programme = await _apprenticeshipProgrammeProvider.GetApprenticeshipStandardVacancyPreviewData(standardId);    
             }
 
+            var hasOptedToAddQualifications = false;
+            if (vacancy.HasOptedToAddQualifications == null)
+            {
+                if (vacancy.Qualifications != null && vacancy.Qualifications.Count != 0)
+                {
+                    hasOptedToAddQualifications = true;
+                }
+            }
+            else
+            {
+                hasOptedToAddQualifications = vacancy.HasOptedToAddQualifications.Value;
+            }
+            
+
             var allQualifications = await _vacancyClient.GetCandidateQualificationsAsync();
 
             vm.VacancyId = vacancy.Id;
@@ -86,7 +100,7 @@ namespace Esfa.Recruit.Employer.Web.Mappings
             vm.ProviderReviewFieldIndicators = vacancy.ProviderReviewFieldIndicators;
             vm.Qualifications = vacancy.Qualifications?.Where(c=>c.Weighting == QualificationWeighting.Essential).SortQualifications(allQualifications).AsText(_feature.IsFeatureEnabled(FeatureNames.FaaV2Improvements));
             vm.QualificationsDesired = vacancy.Qualifications?.Where(c=>c.Weighting == QualificationWeighting.Desired).SortQualifications(allQualifications).AsText(_feature.IsFeatureEnabled(FeatureNames.FaaV2Improvements));
-            vm.HasOptedToAddQualifications = !_feature.IsFeatureEnabled(FeatureNames.FaaV2Improvements) ? true : vacancy.HasOptedToAddQualifications;
+            vm.HasOptedToAddQualifications = !_feature.IsFeatureEnabled(FeatureNames.FaaV2Improvements) || hasOptedToAddQualifications;
             vm.ShortDescription = vacancy.ShortDescription;
             vm.Skills = vacancy.Skills ?? Enumerable.Empty<string>();
             vm.ThingsToConsider = vacancy.ThingsToConsider;
