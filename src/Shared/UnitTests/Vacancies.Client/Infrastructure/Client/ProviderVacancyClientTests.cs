0aa3550f8f492bbf0ab3453d21b12582b62c1a45
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-11-27 22:33:31 +0000

    FAI-2063 - Add notification preference setting with DfESignInId

--- src/Shared/UnitTests/Vacancies.Client/Infrastructure/Client/ProviderVacancyClientTests.cs
+++ src/Shared/UnitTests/Vacancies.Client/Infrastructure/Client/ProviderVacancyClientTests.cs
@@ -5,8 +5,10 @@ using AutoFixture.NUnit3;
 using Esfa.Recruit.Vacancies.Client.Application.Commands;
 using Esfa.Recruit.Vacancies.Client.Domain.Entities;
 using Esfa.Recruit.Vacancies.Client.Domain.Messaging;
+using Esfa.Recruit.Vacancies.Client.Domain.Repositories;
 using Esfa.Recruit.Vacancies.Client.Infrastructure.Client;
 using Esfa.Recruit.Vacancies.Client.Infrastructure.QueryStore.Projections.VacancyApplications;
+using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.Testing.AutoFixture;
@@ -56,6 +58,62 @@ namespace Esfa.Recruit.Vacancies.Client.UnitTests.Vacancies.Client.Infrastructur
                 c.ApplicationReviews.Equals(applicationReviewsToUnsuccessful)
             )));
 
+        }
+        
+        [Test, MoqAutoData]
+        public async Task Then_If_Getting_UserNotificationPreferences_And_DfESignInId_Not_Passed_And_Preferences_Dont_Exist_Then_Returned_With_Default(
+            string idamsUserId,
+            [Frozen] Mock<IUserNotificationPreferencesRepository> userNotificationPreferencesRepository,
+            VacancyClient client)
+        {
+            userNotificationPreferencesRepository.Setup(x => x.GetAsync(idamsUserId))
+                .ReturnsAsync((UserNotificationPreferences)null);
+            
+            var actual = await client.GetUserNotificationPreferencesAsync(idamsUserId);
+
+            actual.DfeUserId.Should().BeNull();
+            actual.Id.Should().Be(idamsUserId);
+            actual.NotificationFrequency.Should().BeNull();
+            actual.NotificationScope.Should().BeNull();
+        }
+
+        [Test, MoqAutoData]
+        public async Task Then_If_Getting_UserNotificationPreferences_And_DfESignInId_Passed_And_Preferences_Dont_Exist_Then_Null_Returned_With_Default(
+                string dfESignInId,
+                string idamsUserId,
+                [Frozen] Mock<IUserNotificationPreferencesRepository> userNotificationPreferencesRepository,
+                VacancyClient client)
+        {
+            userNotificationPreferencesRepository.Setup(x => x.GetAsync(idamsUserId))
+                .ReturnsAsync((UserNotificationPreferences)null);
+            userNotificationPreferencesRepository.Setup(x => x.GetByDfeUserId(dfESignInId))
+                .ReturnsAsync((UserNotificationPreferences)null);
+            
+            var actual = await client.GetUserNotificationPreferencesByDfEUserIdAsync(idamsUserId, dfESignInId);
+
+            actual.DfeUserId.Should().Be(dfESignInId);
+            actual.Id.Should().Be(idamsUserId);
+            actual.NotificationFrequency.Should().BeNull();
+            actual.NotificationScope.Should().BeNull();
+        }
+        
+        [Test, MoqAutoData]
+        public async Task Then_If_Getting_UserNotificationPreferences_And_DfESignInId_Passed_And_Preferences_Exist_Then_Returned(
+                string dfESignInId,
+                string idamsUserId,
+                UserNotificationPreferences preference1,
+                UserNotificationPreferences preference2,
+                [Frozen] Mock<IUserNotificationPreferencesRepository> userNotificationPreferencesRepository,
+                VacancyClient client)
+        {
+            userNotificationPreferencesRepository.Setup(x => x.GetAsync(idamsUserId))
+                .ReturnsAsync(preference1);
+            userNotificationPreferencesRepository.Setup(x => x.GetByDfeUserId(dfESignInId))
+                .ReturnsAsync(preference2);
+            
+            var actual = await client.GetUserNotificationPreferencesByDfEUserIdAsync(idamsUserId, dfESignInId);
+
+            actual.Should().BeEquivalentTo(preference2);
         }
     }
 }
