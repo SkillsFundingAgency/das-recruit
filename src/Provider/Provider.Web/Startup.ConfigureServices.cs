9693b6ac6d6340d540411b4ca11af5fc3a26b1fa
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2025-03-04 08:20:15 +0000

    FAI-2298 - Use async and add wait

--- src/Provider/Provider.Web/Startup.ConfigureServices.cs
+++ src/Provider/Provider.Web/Startup.ConfigureServices.cs
@@ -126,7 +126,7 @@ namespace Esfa.Recruit.Provider.Web
                 var serviceProvider = services.BuildServiceProvider();
                 var collectionChecker = (MongoDbCollectionChecker)serviceProvider.GetService(typeof(MongoDbCollectionChecker));
                 collectionChecker?.EnsureCollectionsExist();
-                collectionChecker?.CreateIndexes();
+                collectionChecker?.CreateIndexes().Wait();
                 var storageTableChecker = (QueryStoreTableChecker)serviceProvider.GetService(typeof(QueryStoreTableChecker));
                 storageTableChecker?.EnsureQueryStoreTableExist();
             }
