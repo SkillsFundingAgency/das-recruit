90b8135eba147593971c7e6d2988d81bfb1eede8
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2025-04-01 22:38:13 +0100

    FAI-2392 - Remove Qualification and ApprenticeshipProgramme from IReferenceDataItem

--- src/Shared/Recruit.Vacancies.Client/Infrastructure/ReferenceData/Qualifications/Qualifications.cs
+++ src/Shared/Recruit.Vacancies.Client/Infrastructure/ReferenceData/Qualifications/Qualifications.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 
 namespace Esfa.Recruit.Vacancies.Client.Infrastructure.ReferenceData.Qualifications
 {
-    public class Qualifications : IReferenceDataItem
+    public class Qualifications
     {
-        public string Id { get; set; }
-        public DateTime LastUpdatedDate { get; set; }
         public List<string> QualificationTypes { get; set; }
     }
 }
