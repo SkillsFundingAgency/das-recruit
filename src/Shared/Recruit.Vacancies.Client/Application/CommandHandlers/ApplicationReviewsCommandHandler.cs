86c0a8fb8988a499450b46300fca382648bfe781
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2025-08-21 12:00:34 +0100

    Optimized the call to the outer api while sending the notification email for Shared application

--- src/Shared/Recruit.Vacancies.Client/Application/CommandHandlers/ApplicationReviewsCommandHandler.cs
+++ src/Shared/Recruit.Vacancies.Client/Application/CommandHandlers/ApplicationReviewsCommandHandler.cs
@@ -28,30 +28,16 @@ public class ApplicationReviewPendingUnsuccessfulFeedbackCommandHandler(
     }
 }
 
-public class ApplicationReviewsCommandHandler :
-    IRequestHandler<ApplicationReviewsSharedCommand, Unit>,
-    IRequestHandler<ApplicationReviewsUnsuccessfulCommand, Unit>
+public class ApplicationReviewsCommandHandler(
+    IApplicationReviewRepository applicationReviewRepository,
+    ITimeProvider timeProvider,
+    IOuterApiClient outerApiClient,
+    IVacancyRepository vacancyRepository,
+    IApplicationReviewRepositoryRunner applicationReviewRepositoryRunner)
+    :
+        IRequestHandler<ApplicationReviewsSharedCommand, Unit>,
+        IRequestHandler<ApplicationReviewsUnsuccessfulCommand, Unit>
 {
-    private readonly IApplicationReviewRepository _applicationReviewRepository;
-    private readonly IApplicationReviewRepositoryRunner _applicationReviewRepositoryRunner;
-    private readonly ITimeProvider _timeProvider;
-    private readonly IOuterApiClient _outerApiClient;
-    private readonly IVacancyRepository _vacancyRepository;
-
-    public ApplicationReviewsCommandHandler(
-        IApplicationReviewRepository applicationReviewRepository,
-        ITimeProvider timeProvider, 
-        IOuterApiClient outerApiClient,
-        IVacancyRepository vacancyRepository, 
-        IApplicationReviewRepositoryRunner applicationReviewRepositoryRunner)
-    {
-        _applicationReviewRepository = applicationReviewRepository;
-        _timeProvider = timeProvider;
-        _outerApiClient = outerApiClient;
-        _vacancyRepository = vacancyRepository;
-        _applicationReviewRepositoryRunner = applicationReviewRepositoryRunner;
-    }
-
     public async Task<Unit> Handle(ApplicationReviewsSharedCommand command, CancellationToken cancellationToken)
     {
         await Handle(command.ApplicationReviews.ToList(), command.User, command.Status, command.VacancyId, command.TemporaryStatus, vacancyReference: command.VacancyReference);
@@ -64,30 +50,89 @@ public class ApplicationReviewsCommandHandler :
         return Unit.Value;
     }
 
-    private async Task Handle(List<Guid> applicationReviews, VacancyUser user, ApplicationReviewStatus? status, Guid vacancyId, ApplicationReviewStatus? temporaryStatus, string candidateFeedback = null, long? vacancyReference = null)
+    private async Task Handle(List<Guid> applicationReviews,
+        VacancyUser user,
+        ApplicationReviewStatus? status,
+        Guid vacancyId,
+        ApplicationReviewStatus? temporaryStatus,
+        string candidateFeedback = null,
+        long? vacancyReference = null)
     {
-        await _applicationReviewRepositoryRunner.UpdateApplicationReviewsAsync(applicationReviews, user, _timeProvider.Now, status, temporaryStatus, candidateFeedback, vacancyReference);
+        await applicationReviewRepositoryRunner.UpdateApplicationReviewsAsync(applicationReviews, user, timeProvider.Now, status, temporaryStatus, candidateFeedback, vacancyReference);
 
-        if (status == ApplicationReviewStatus.Unsuccessful)
+        if (status is not (ApplicationReviewStatus.Unsuccessful or ApplicationReviewStatus.Shared))
         {
-            var vacancy = await _vacancyRepository.GetVacancyAsync(vacancyId);
-            var applicationReviewsByReference =
-                await _applicationReviewRepository.GetForVacancyAsync<ApplicationReview>(vacancy.VacancyReference!.Value);
-            
-            await Parallel.ForEachAsync(applicationReviewsByReference.Where(x =>
-                !x.IsWithdrawn && x.Application is { IsFaaV2Application: true } && applicationReviews.Contains(x.Id)).ToList(), new ParallelOptions() , async (applicationReview, token) => 
-                {
-                    await _outerApiClient.Post(new PostApplicationStatusRequest(applicationReview.CandidateId,
-                        applicationReview.Application.ApplicationId, new PostApplicationStatus
-                        {
-                            VacancyReference = applicationReview.VacancyReference,
-                            Status = status.ToString(),
-                            CandidateFeedback = candidateFeedback,
-                            VacancyTitle = vacancy.Title,
-                            VacancyEmployerName = vacancy.EmployerName,
-                            VacancyLocation = vacancy.GetVacancyLocation()
-                        }));   
-                });
+            return; // nothing to do for other statuses
         }
+
+        var vacancy = await vacancyRepository.GetVacancyAsync(vacancyId);
+        var applicationReviewsByReference =
+            await applicationReviewRepository.GetForVacancyAsync<ApplicationReview>(vacancy.VacancyReference!.Value);
+
+        var eligibleReviews = applicationReviewsByReference
+            .Where(x => !x.IsWithdrawn
+                        && x.Application?.IsFaaV2Application == true
+                        && applicationReviews.Contains(x.Id));
+
+        switch (status)
+        {
+            case ApplicationReviewStatus.Unsuccessful:
+                {
+                    await SendUnSuccessfulNotification(eligibleReviews.ToList(), vacancy, status, candidateFeedback);
+                    return;
+                }
+            case ApplicationReviewStatus.Shared:
+                {
+                    await SendSharedNotification(eligibleReviews.ToList(), vacancy);
+                    return;
+                }
+            default:
+                return;
+        }
+    }
+
+    private async Task SendSharedNotification(List<ApplicationReview> reviews, Vacancy vacancy)
+    {
+        var tasks = reviews.Select(applicationReview =>
+        {
+            var requestData = new PostApplicationSharedNotificationApiRequest.PostApplicationSharedNotificationApiRequestData
+            {
+                AdvertTitle = vacancy.Title,
+                ApplicationId = applicationReview.Application!.ApplicationId,
+                FirstName = vacancy.EmployerName,
+                HashAccountId = vacancy.EmployerAccountId,
+                RecipientEmail = vacancy.SubmittedByUser.Email,
+                TrainingProvider = vacancy.TrainingProvider.Name,
+                VacancyId = vacancy.Id,
+                VacancyReference = vacancy.VacancyReference!.Value
+            };
+
+            var request = new PostApplicationSharedNotificationApiRequest(requestData);
+
+            return outerApiClient.Post(request);
+        });
+
+        await Task.WhenAll(tasks);
+    }
+
+    private async Task SendUnSuccessfulNotification(List<ApplicationReview> reviews, Vacancy vacancy, ApplicationReviewStatus? status, string candidateFeedback)
+    {
+        var tasks = reviews.Select(applicationReview =>
+        {
+            var request = new PostApplicationStatusRequest(applicationReview.CandidateId,
+                applicationReview.Application.ApplicationId, new PostApplicationStatus
+                {
+                    VacancyReference = applicationReview.VacancyReference,
+                    Status = status.ToString(),
+                    CandidateFeedback = candidateFeedback,
+                    VacancyTitle = vacancy.Title,
+                    VacancyEmployerName = vacancy.EmployerName,
+                    VacancyLocation = vacancy.GetVacancyLocation()
+                });
+
+            return outerApiClient.Post(request);
+        });
+
+        await Task.WhenAll(tasks);
     }
 }
