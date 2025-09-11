1a6d366e80bf87baf3ee1889d76c14ec8bd36f33
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2025-09-11 13:01:03 +0100

    Cleanup

--- src/Shared/Recruit.Vacancies.Client/Infrastructure/ReferenceData/ApprenticeshipProgrammes/ApprenticeshipProgrammeProvider.cs
+++ src/Shared/Recruit.Vacancies.Client/Infrastructure/ReferenceData/ApprenticeshipProgrammes/ApprenticeshipProgrammeProvider.cs
@@ -2,26 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Esfa.Recruit.Vacancies.Client.Application.Cache;
-using Esfa.Recruit.Vacancies.Client.Application.FeatureToggle;
 using Esfa.Recruit.Vacancies.Client.Application.Providers;
 using Esfa.Recruit.Vacancies.Client.Domain.Entities;
 using Esfa.Recruit.Vacancies.Client.Infrastructure.OuterApi;
 using Esfa.Recruit.Vacancies.Client.Infrastructure.OuterApi.Requests;
 using Esfa.Recruit.Vacancies.Client.Infrastructure.OuterApi.Responses;
-using Microsoft.Extensions.Configuration;
 
 namespace Esfa.Recruit.Vacancies.Client.Infrastructure.ReferenceData.ApprenticeshipProgrammes
 {
-    public class ApprenticeshipProgrammeProvider : IApprenticeshipProgrammeProvider
+    public class ApprenticeshipProgrammeProvider(IOuterApiClient outerApiClient) : IApprenticeshipProgrammeProvider
     {
-        private readonly IOuterApiClient _outerApiClient;
-
-        public ApprenticeshipProgrammeProvider(IOuterApiClient outerApiClient)
-        {
-            _outerApiClient = outerApiClient;
-        }
-
         public async Task<IApprenticeshipProgramme> GetApprenticeshipProgrammeAsync(string programmeId, int? ukprn = null)
         {
             var apprenticeships = await GetApprenticeshipProgrammesAsync(true, ukprn);
@@ -31,7 +21,7 @@ namespace Esfa.Recruit.Vacancies.Client.Infrastructure.ReferenceData.Apprentices
 
         public async Task<ApprenticeshipStandard> GetApprenticeshipStandardVacancyPreviewData(int programmedId)
         {
-            return await _outerApiClient.Get<GetVacancyPreviewApiResponse>(new GetVacancyPreviewApiRequest(programmedId));
+            return await outerApiClient.Get<GetVacancyPreviewApiResponse>(new GetVacancyPreviewApiRequest(programmedId));
         }
 
         public async Task<IEnumerable<IApprenticeshipProgramme>> GetApprenticeshipProgrammesAsync(bool includeExpired = false, int? ukprn = null)
@@ -44,7 +34,7 @@ namespace Esfa.Recruit.Vacancies.Client.Infrastructure.ReferenceData.Apprentices
 
         private async Task<ApprenticeshipProgrammes> GetApprenticeshipProgrammes(int? ukprn)
         {
-            var result = await _outerApiClient.Get<GetTrainingProgrammesResponse>(new GetTrainingProgrammesRequest(ukprn));
+            var result = await outerApiClient.Get<GetTrainingProgrammesResponse>(new GetTrainingProgrammesRequest(ukprn));
             return new ApprenticeshipProgrammes
             {
                 Data = result.TrainingProgrammes.Select(c => (ApprenticeshipProgramme)c).ToList(),
