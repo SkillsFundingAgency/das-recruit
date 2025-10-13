7bbd76ab2a95f47261844509301800a5b4fc8d4b
killij <51908793+killij@users.noreply.github.com>
2025-10-13 13:31:34 +0100

    FAI-2804 Call outer API at correct points for new notifications

--- src/Jobs/Recruit.Vacancies.Jobs/DomainEvents/Handlers/Vacancy/VacancyRejectedHandler.cs
+++ src/Jobs/Recruit.Vacancies.Jobs/DomainEvents/Handlers/Vacancy/VacancyRejectedHandler.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Communication.Types;
+using Esfa.Recruit.Vacancies.Client.Application.Communications;
+using Esfa.Recruit.Vacancies.Client.Application.FeatureToggle;
 using Esfa.Recruit.Vacancies.Client.Domain.Events;
 using Esfa.Recruit.Vacancies.Client.Infrastructure.OuterApi;
 using Esfa.Recruit.Vacancies.Client.Infrastructure.OuterApi.Requests.Events;
+using Esfa.Recruit.Vacancies.Client.Infrastructure.StorageQueue;
 using Microsoft.Extensions.Logging;
 
 namespace Esfa.Recruit.Vacancies.Jobs.DomainEvents.Handlers.Vacancy;
 
 public class VacancyRejectedHandler(
     ILogger<VacancyRejectedHandler> logger,
-    IOuterApiClient outerApiClient)
+    ICommunicationQueueService queue,
+    IOuterApiClient outerApiClient,
+    IFeature feature)
     : DomainEventHandler(logger), IDomainEventHandler<VacancyRejectedEvent>
 {
     public async Task HandleAsync(string eventPayload)
@@ -17,14 +23,36 @@ public class VacancyRejectedHandler(
         var @event = DeserializeEvent<VacancyRejectedEvent>(eventPayload);
         try
         {
-            logger.LogInformation($"Processing {nameof(VacancyRejectedEvent)} for vacancy: {{VacancyId}}", @event.VacancyId);
-            await outerApiClient.Post(new PostVacancyRejectedEventRequest(new PostVacancyRejectedEventData(@event.VacancyId)));
-            logger.LogInformation($"Finished Processing {nameof(VacancyRejectedEvent)} for vacancy: {{VacancyId}}", @event.VacancyId);
+            logger.LogInformation("Processing {EventName} for vacancy: {VacancyId}", nameof(VacancyRejectedEvent), @event.VacancyId);
+
+            if (!feature.IsFeatureEnabled(FeatureNames.NotificationsMigration))
+            {
+                await outerApiClient.Post(new PostEmployerRejectedVacancyEventRequest(new PostEmployerRejectedVacancyEventData(@event.VacancyId)));
+            }
+            else
+            {
+                var communicationRequest = GetRejectedVacancyCommunicationRequest(@event.VacancyReference, @event.ProviderUkprn);
+                await queue.AddMessageAsync(communicationRequest);
+            }
+            
+            logger.LogInformation("Finished Processing {EventName} for vacancy: {VacancyId}", nameof(VacancyRejectedEvent), @event.VacancyId);
         }
         catch (Exception ex)
         {
-            logger.LogError(ex, "Unable to process {eventBody}", @event);
+            logger.LogError(ex, "Unable to process {EventBody}", @event);
             throw;
         }
+    }
+    
+    private static CommunicationRequest GetRejectedVacancyCommunicationRequest(long vacancyReference, long? providerUkprn)
+    {
+        var communicationRequest = new CommunicationRequest(
+            CommunicationConstants.RequestType.VacancyRejectedByEmployer,
+            CommunicationConstants.ParticipantResolverNames.VacancyParticipantsResolverName,
+            CommunicationConstants.ServiceName);
+        communicationRequest.AddEntity(CommunicationConstants.EntityTypes.Vacancy, vacancyReference);
+        communicationRequest.AddEntity(CommunicationConstants.EntityTypes.ApprenticeshipServiceUrl, vacancyReference);
+        communicationRequest.AddEntity(CommunicationConstants.EntityTypes.Provider, providerUkprn);
+        return communicationRequest;
     }
 }
