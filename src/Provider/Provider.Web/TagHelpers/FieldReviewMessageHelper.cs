4d5370ee342f65aec4f2d164671b64d5503189cf
killij <51908793+killij@users.noreply.github.com>
2025-11-25 15:48:26 +0000

    FAI-2878 Prevent any output from the FieldReviewMessageHelper when there isn't an error

--- src/Provider/Provider.Web/TagHelpers/FieldReviewMessageHelper.cs
+++ src/Provider/Provider.Web/TagHelpers/FieldReviewMessageHelper.cs
@@ -2,61 +2,57 @@
 using System.Linq;
 using System.Text.Encodings.Web;
 using Esfa.Recruit.Provider.Web.ViewModels.VacancyPreview;
-using Esfa.Recruit.Shared.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
-namespace Esfa.Recruit.Provider.Web.TagHelpers
+namespace Esfa.Recruit.Provider.Web.TagHelpers;
+
+[HtmlTargetElement("field-review-message")]
+public class FieldReviewMessageHelper : TagHelper
 {
-    [HtmlTargetElement("field-review-message")]
-    public class FieldReviewMessageHelper : TagHelper
+    public string FieldName { get; set; }
+    public VacancyPreviewViewModel Model { get; set; }
+    public override void Process(TagHelperContext tagHelperContext, TagHelperOutput tagHelperOutput)
     {
-        public string FieldName { get; set; }
-        public VacancyPreviewViewModel Model { get; set; }
-        public override void Process(TagHelperContext tagHelperContext, TagHelperOutput tagHelperOutput)
+        var error = Model.Review.FieldIndicators.FirstOrDefault(c =>
+            c.ReviewFieldIdentifier.Equals(FieldName, StringComparison.CurrentCultureIgnoreCase));
+
+        var validationError = string.Empty;
+        if (Model.ValidationErrors.ModelState != null 
+            && Model.ValidationErrors.ModelState.ContainsKey(FieldName) 
+            && Model.ValidationErrors.ModelState[FieldName]!.ValidationState == ModelValidationState.Invalid)
         {
-            var error = Model.Review.FieldIndicators.FirstOrDefault(c =>
-                c.ReviewFieldIdentifier.Equals(FieldName, StringComparison.CurrentCultureIgnoreCase));
-
-
-            var validationError = string.Empty;
-            if (Model.ValidationErrors.ModelState != null 
-                && Model.ValidationErrors.ModelState.ContainsKey(FieldName) 
-                && Model.ValidationErrors.ModelState[FieldName].ValidationState == ModelValidationState.Invalid)
-            {
-                var item = Model.ValidationErrors.ModelState[FieldName];
-                validationError = item.Errors.FirstOrDefault()?.ErrorMessage;
-            }
+            var item = Model.ValidationErrors.ModelState[FieldName];
+            validationError = item.Errors.FirstOrDefault()?.ErrorMessage;
+        }
             
-            if (error == null && string.IsNullOrEmpty(validationError))
-            {
-                return;
-            }
+        if (error == null && string.IsNullOrEmpty(validationError))
+        {
+            tagHelperOutput.SuppressOutput();
+            return;
+        }
             
-            tagHelperOutput.TagName = "span";
-            tagHelperOutput.AddClass("app-summary-list__error-message", HtmlEncoder.Default);
-            if (!string.IsNullOrEmpty(validationError))
-            {
-                tagHelperOutput.Content.AppendHtml(validationError);
-                if (error != null)
-                {
-                    tagHelperOutput.Content.AppendHtml("<br/>");
-                }
-            }
-
+        tagHelperOutput.TagMode = TagMode.StartTagAndEndTag;  
+        tagHelperOutput.TagName = "span";
+        tagHelperOutput.AddClass("app-summary-list__error-message", HtmlEncoder.Default);
+        if (!string.IsNullOrEmpty(validationError))
+        {
+            tagHelperOutput.Content.AppendHtml(validationError);
             if (error != null)
             {
-                tagHelperOutput.Content.AppendHtml(error.ManualQaText);
-                foreach (string autoQaText in error.AutoQaTexts)
-                {
-                    tagHelperOutput.Content.AppendHtml("<br/>");
-                    tagHelperOutput.Content.Append($"{autoQaText}");
-                }    
+                tagHelperOutput.Content.AppendHtml("<br/>");
             }
-            
-            
-            tagHelperOutput.TagMode = TagMode.StartTagAndEndTag;  
+        }
+
+        if (error != null)
+        {
+            tagHelperOutput.Content.AppendHtml(error.ManualQaText);
+            foreach (var autoQaText in error.AutoQaTexts)
+            {
+                tagHelperOutput.Content.AppendHtml("<br/>");
+                tagHelperOutput.Content.Append($"{autoQaText}");
+            }    
         }
     }
 }
