13850e5fffe47ec0d57c1105981f2bebafdd99ec
killij <51908793+killij@users.noreply.github.com>
2026-02-11 14:47:07 +0000

    FAI-2983 Add new request for review vacancies

---
+++ src/Shared/Recruit.Vacancies.Client/Infrastructure/OuterApi/Requests/Vacancy/GetReviewVacanciesByUkprnApiRequest.cs
@@ -0,0 +1,11 @@
+﻿using Esfa.Recruit.Vacancies.Client.Domain.Models;
+
+namespace Esfa.Recruit.Vacancies.Client.Infrastructure.OuterApi.Requests.Vacancy;
+
+public sealed record GetReviewVacanciesByUkprnApiRequest(
+    int Ukprn,
+    string? SearchTerm = null,
+    int Page = 1,
+    int PageSize = 25,
+    VacancySortColumn? SortColumn = VacancySortColumn.CreatedDate,
+    ColumnSortOrder? SortOrder = ColumnSortOrder.Desc) : GetVacanciesByUkprnApiRequestV2("review", Ukprn, SearchTerm, Page, PageSize, SortColumn, SortOrder);
