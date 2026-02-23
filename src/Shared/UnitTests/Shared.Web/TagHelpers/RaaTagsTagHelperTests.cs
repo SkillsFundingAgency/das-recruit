cffa5934bb1aafcc6a74ab7cef775d8384d6e085
Liam Crossley <liam.crossley@education.gov.uk>
2026-02-23 16:06:29 +0000

    FAI-2990 Refactored govuk-status-tags

--- src/Shared/UnitTests/Shared.Web/TagHelpers/RaaTagsTagHelperTests.cs
+++ src/Shared/UnitTests/Shared.Web/TagHelpers/RaaTagsTagHelperTests.cs
@@ -1,4 +1,6 @@
-﻿using Esfa.Recruit.Shared.Web.TagHelpers;
+﻿using System.Linq;
+using Esfa.Recruit.Shared.Web.TagHelpers;
+using Esfa.Recruit.Vacancies.Client.Domain.Entities;
 using NUnit.Framework;
 
 namespace Esfa.Recruit.Vacancies.Client.UnitTests.Shared.Web.TagHelpers;
@@ -58,5 +60,38 @@ public class RaaTagsTagHelperTests: TagHelperTestsBase
 
         // assert
         TagHelperOutput.AsString().Should().Be("""<strong class="govuk-tag--yellow govuk-tag">API submitted</strong>""");
+    }
+}
+
+public class StatusTagHelperTests: TagHelperTestsBase
+{
+    [TestCase(VacancyStatus.Draft, "<strong class=\"govuk-tag govuk-tag--grey\">Draft</strong>")]
+    [TestCase(VacancyStatus.Submitted, "<strong class=\"govuk-tag govuk-tag--blue\">Submitted</strong>")]
+    [TestCase(VacancyStatus.Live, "<strong class=\"govuk-tag govuk-tag--turquoise\">Live</strong>")]
+    public async Task VacancyStatus_Renders_Canonical_Class(VacancyStatus status, string expected)
+    {
+        // Arrange
+        var sut = new StatusTagHelper { VacancyStatusValue = status };
+
+        // Act
+        await sut.ProcessAsync(TagHelperContext, TagHelperOutput);
+
+        // Assert
+        TagHelperOutput.AsString().Should().Be(expected);
+    }
+
+    [TestCase(ApplicationReviewStatus.New, "<strong class=\"govuk-tag govuk-tag--light-blue\">New</strong>")]
+    [TestCase(ApplicationReviewStatus.Unsuccessful, "<strong class=\"govuk-tag govuk-tag--orange\">Unsuccessful</strong>")]
+    [TestCase(ApplicationReviewStatus.Interviewing, "<strong class=\"govuk-tag govuk-tag--purple\">Interviewing</strong>")]
+    public async Task ApplicationStatus_Renders_Canonical_Class(ApplicationReviewStatus status, string expected)
+    {
+        // Arrange
+        var sut = new StatusTagHelper { ApplicationStatusValue = status };
+
+        // Act
+        await sut.ProcessAsync(TagHelperContext, TagHelperOutput);
+
+        // Assert
+        TagHelperOutput.AsString().Should().Be(expected);
     }
 }
