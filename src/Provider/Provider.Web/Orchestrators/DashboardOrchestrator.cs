18f526d60d5ebedb86e2cdaa97e3152daf25e747
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-03-23 12:14:29 +0000

    Changes related to the Dashboard Employer review permissions

--- src/Provider/Provider.Web/Orchestrators/DashboardOrchestrator.cs
+++ src/Provider/Provider.Web/Orchestrators/DashboardOrchestrator.cs
@@ -3,29 +3,19 @@ using Esfa.Recruit.Provider.Web.ViewModels;
 using Esfa.Recruit.Provider.Web.ViewModels.Dashboard;
 using Esfa.Recruit.Shared.Web.ViewModels.Alerts;
 using Esfa.Recruit.Vacancies.Client.Domain.Entities;
-using Esfa.Recruit.Vacancies.Client.Domain.Models;
 using Esfa.Recruit.Vacancies.Client.Infrastructure.Client;
-using Esfa.Recruit.Vacancies.Client.Infrastructure.Services.ProviderRelationship;
 
 namespace Esfa.Recruit.Provider.Web.Orchestrators
 {
-    public class DashboardOrchestrator(
-        IProviderVacancyClient vacancyClient,
-        IRecruitVacancyClient client,
-        IProviderRelationshipsService providerRelationshipsService)
+    public class DashboardOrchestrator(IProviderVacancyClient vacancyClient,
+        IRecruitVacancyClient client)
     {
         public virtual async Task<DashboardViewModel> GetDashboardViewModelAsync(VacancyUser user)
         {
             long ukprn = user.Ukprn ?? 0;
 
             await client.UserSignedInAsync(user, UserType.Provider);
-            var dashboardTask = vacancyClient.GetDashboardSummary(ukprn, user.UserId);
-            var providerTask = providerRelationshipsService.CheckProviderHasPermissions(ukprn, OperationType.RecruitmentRequiresReview);
-            
-            await Task.WhenAll(dashboardTask, providerTask);
-            
-            var dashboard = dashboardTask.Result;
-            bool providerPermissions = providerTask.Result;
+            var dashboard = await vacancyClient.GetDashboardSummary(ukprn, user.UserId);
             var alerts = new AlertsViewModel(new ProviderTransferredVacanciesAlertViewModel
             {
                 LegalEntityNames = dashboard.ProviderTransferredVacanciesAlert.LegalEntityNames,
@@ -40,7 +30,6 @@ namespace Esfa.Recruit.Provider.Web.Orchestrators
             {
                 ProviderDashboardSummary = dashboard,
                 Alerts = alerts,
-                HasEmployerReviewPermission = providerPermissions,
                 Ukprn = ukprn
             };
         }
