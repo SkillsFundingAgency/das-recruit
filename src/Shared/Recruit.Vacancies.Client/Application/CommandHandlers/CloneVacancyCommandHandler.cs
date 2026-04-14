31df3a83df280e7688afa98d2bb5092d04d20c25
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-04-14 14:33:51 +0100

    Removed Vacancy Created Event as this is not handled anywhere

--- src/Shared/Recruit.Vacancies.Client/Application/CommandHandlers/CloneVacancyCommandHandler.cs
+++ src/Shared/Recruit.Vacancies.Client/Application/CommandHandlers/CloneVacancyCommandHandler.cs
@@ -1,64 +1,45 @@
-﻿using Esfa.Recruit.Vacancies.Client.Application.Commands;
-using Esfa.Recruit.Vacancies.Client.Domain.Events;
-using Esfa.Recruit.Vacancies.Client.Domain.Messaging;
-using Esfa.Recruit.Vacancies.Client.Domain.Repositories;
-using MediatR;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
+using Esfa.Recruit.Vacancies.Client.Application.Commands;
 using Esfa.Recruit.Vacancies.Client.Application.Providers;
 using Esfa.Recruit.Vacancies.Client.Domain.Entities;
+using Esfa.Recruit.Vacancies.Client.Domain.Exceptions;
+using Esfa.Recruit.Vacancies.Client.Domain.Repositories;
+using MediatR;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Esfa.Recruit.Vacancies.Client.Domain.Exceptions;
 
 namespace Esfa.Recruit.Vacancies.Client.Application.CommandHandlers
 {
-    public class CloneVacancyCommandHandler: IRequestHandler<CloneVacancyCommand, Unit>
+    public class CloneVacancyCommandHandler(
+        ILogger<CloneVacancyCommandHandler> logger,
+        IVacancyRepository repository,
+        ITimeProvider timeProvider)
+        : IRequestHandler<CloneVacancyCommand, Unit>
     {
-        private readonly ILogger<CloneVacancyCommandHandler> _logger;
-        private readonly IVacancyRepository _repository;
-        private readonly IMessaging _messaging;
-        private readonly ITimeProvider _timeProvider;
-
-        public CloneVacancyCommandHandler(
-            ILogger<CloneVacancyCommandHandler> logger,
-            IVacancyRepository repository, 
-            IMessaging messaging, 
-            ITimeProvider timeProvider)
-        {
-            _logger = logger;
-            _repository = repository;
-            _messaging = messaging;
-            _timeProvider = timeProvider;
-        }
-
         public async Task<Unit> Handle(CloneVacancyCommand message, CancellationToken cancellationToken)
         {
-            _logger.LogInformation("Cloning new vacancy with id: {vacancyId} from vacancy with id: {clonedVacancyId}", message.IdOfVacancyToClone, message.NewVacancyId);
+            logger.LogInformation("Cloning new vacancy with id: {vacancyId} from vacancy with id: {clonedVacancyId}", message.IdOfVacancyToClone, message.NewVacancyId);
 
-            var vacancy = await _repository.GetVacancyAsync(message.IdOfVacancyToClone);
+            var vacancy = await repository.GetVacancyAsync(message.IdOfVacancyToClone);
 
             if (vacancy.Status != VacancyStatus.Submitted && vacancy.Status != VacancyStatus.Live && vacancy.Status != VacancyStatus.Closed && vacancy.Status != VacancyStatus.Review)
             {
-                _logger.LogError($"Unable to clone vacancy {{vacancyId}} due to it having a status of {vacancy.Status}.", message.IdOfVacancyToClone);
+                logger.LogError($"Unable to clone vacancy {{vacancyId}} due to it having a status of {vacancy.Status}.", message.IdOfVacancyToClone);
                 
                 throw new InvalidStateException($"Vacancy is not in correct state to be cloned. Current State: {vacancy.Status}");
             }
 
             var clone = CreateClone(message, vacancy);
 
-            await _repository.CreateAsync(clone);
-
-            await _messaging.PublishEvent(new VacancyCreatedEvent
-            {
-                VacancyId = clone.Id
-            });
+            await repository.CreateAsync(clone);
+           
             return Unit.Value;
         }
 
         private Vacancy CreateClone(CloneVacancyCommand message, Vacancy vacancy)
         {
-            var now = _timeProvider.Now;
+            var now = timeProvider.Now;
             var clone = JsonConvert.DeserializeObject<Vacancy>(JsonConvert.SerializeObject(vacancy));
 
             // Properties to replace
