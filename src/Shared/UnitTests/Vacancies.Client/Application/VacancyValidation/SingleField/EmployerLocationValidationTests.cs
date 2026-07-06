8b73d2d8199800559a926ca0a96ab9bb6a552aa0
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-07-06 17:32:38 +0100

    Add England-only employer location validation & refactor DI
    Refactor controllers and orchestrators to use primary constructors for dependency injection. Add EmployerLocationMustBeInEngland validation, new error code, and rule set flag to enforce all employer locations are in England. Update FluentVacancyValidator and tests to support and verify this validation. Clean up unused usings and improve message formatting.

---
+++ src/Shared/UnitTests/Vacancies.Client/Application/VacancyValidation/SingleField/EmployerLocationValidationTests.cs
@@ -0,0 +1,51 @@
+﻿using Esfa.Recruit.Vacancies.Client.Application.Validation;
+using Esfa.Recruit.Vacancies.Client.Domain.Entities;
+using Xunit;
+
+namespace Esfa.Recruit.Vacancies.Client.UnitTests.Vacancies.Client.Application.VacancyValidation.SingleField;
+
+public class EmployerLocationValidationTests : VacancyValidationTestsBase
+{
+    [Fact]
+    public void NoErrorsWhenLocationIsValid()
+    {
+        var vacancy = new Vacancy
+        {
+            EmployerLocation = new Address
+            {
+                AddressLine1 = "address line 1",
+                AddressLine2 = "address line 2",
+                AddressLine3 = "address line 3",
+                Postcode = "SW1 1AB",
+                Country = "England"
+            }
+        };
+
+        MockLocationsService.Setup(p => p.IsPostcodeInEnglandAsync("SW1 1AB")).ReturnsAsync(true);
+
+        var result = Validator.Validate(vacancy, VacancyRuleSet.EmployerAddress);
+
+        result.HasErrors.Should().BeFalse();
+        result.Errors.Should().HaveCount(0);
+    }
+
+    [Fact]
+    public void ErrorWhenLocationIsOutOfArea()
+    {
+        var vacancy = new Vacancy
+        {
+            EmployerLocation = new Address
+            {
+                Postcode = "AB1 2CD"
+            },
+            EmployerLocationOption = AvailableWhere.OneLocation
+        };
+
+        MockLocationsService.Setup(p => p.IsPostcodeInEnglandAsync("AB1 2CD")).ReturnsAsync(false);
+
+        var result = Validator.Validate(vacancy, VacancyRuleSet.EmployerAddress);
+
+        result.HasErrors.Should().BeTrue();
+        result.Errors.Should().HaveCount(1);
+    }
+}
