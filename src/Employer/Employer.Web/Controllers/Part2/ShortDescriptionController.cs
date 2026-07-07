bb0cfcbd2fbc597b0490e04717765b9ab539d8c3
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-07-07 16:40:31 +0100

    Refactor ShortDescription logic and fix form markup
    Refactored ShortDescriptionController and Orchestrator to use primary constructors and parameter properties, removing explicit fields and constructors. Updated field usages to use injected parameters directly. Simplified mapping definition with an expression-bodied member. Fixed form markup in ShortDescription.cshtml by correcting div placement and button group location. Reformatted ShortDescription validation rule for readability. Made Vacancy.ShortDescription nullable and simplified its getter/setter.

--- src/Employer/Employer.Web/Controllers/Part2/ShortDescriptionController.cs
+++ src/Employer/Employer.Web/Controllers/Part2/ShortDescriptionController.cs
@@ -10,33 +10,26 @@ using Microsoft.AspNetCore.Mvc;
 namespace Esfa.Recruit.Employer.Web.Controllers.Part2
 {
     [Route(RoutePaths.AccountVacancyRoutePath)]
-    public class ShortDescriptionController : Controller
+    public class ShortDescriptionController(ShortDescriptionOrchestrator orchestrator) : Controller
     {
-        private readonly ShortDescriptionOrchestrator _orchestrator;
-
-        public ShortDescriptionController(ShortDescriptionOrchestrator orchestrator)
-        {
-            _orchestrator = orchestrator;
-        }
-        
         [HttpGet("description", Name = RouteNames.ShortDescription_Get)]
         public async Task<IActionResult> ShortDescription(VacancyRouteModel vrm)
         {
-            var vm = await _orchestrator.GetShortDescriptionViewModelAsync(vrm);
+            var vm = await orchestrator.GetShortDescriptionViewModelAsync(vrm);
             return View(vm);
         }
 
         [HttpPost("description", Name = RouteNames.ShortDescription_Post)]
         public async Task<IActionResult> ShortDescription(ShortDescriptionEditModel m)
         {
-            var response = await _orchestrator.PostShortDescriptionEditModelAsync(m, User.ToVacancyUser());
+            var response = await orchestrator.PostShortDescriptionEditModelAsync(m, User.ToVacancyUser());
 
             if (!response.Success)
             {
                 response.AddErrorsToModelState(ModelState);
             }
             
-            var vm = await _orchestrator.GetShortDescriptionViewModelAsync(m);
+            var vm = await orchestrator.GetShortDescriptionViewModelAsync(m);
             if (!ModelState.IsValid)
             {
                 return View(vm);
