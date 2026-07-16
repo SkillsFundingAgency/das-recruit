f6b158f05051381ae72ea027e7a4ce3b97d6d2ce
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-07-16 12:50:48 +0100

    Add outstanding applications banner to Manage Vacancy
    Show a GOV.UK-style banner on Employer and Provider Manage Vacancy pages when there are outstanding (new, not withdrawn) applications for live or closed vacancies. Added logic to view models for counting and messaging, used Humanizer for pluralization, and introduced a new constant for "applicant". Updated views to display the banner and refactored related code for clarity.

--- src/Shared/Recruit.Vacancies.Client/Application/Constants.cs
+++ src/Shared/Recruit.Vacancies.Client/Application/Constants.cs
@@ -3,5 +3,6 @@ namespace Esfa.Recruit.Vacancies.Client.Application
     public static class Constants
     {
         public const string HelpdeskPhoneNumber = "08000 150 600";
+        public const string ApplicantWord = "applicant";
     }
 }
